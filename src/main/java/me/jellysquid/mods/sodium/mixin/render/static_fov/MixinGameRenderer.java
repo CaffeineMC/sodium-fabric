02ff8aed679649a8cae7e44515c834ebbba0a135
eatmyvenom <jacobhagan@outlook.com>
2020-02-03 20:51:12 -0800

    comments

--- src/main/java/me/jellysquid/mods/sodium/mixin/render/static_fov/MixinGameRenderer.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/render/static_fov/MixinGameRenderer.java
@@ -12,7 +12,15 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(GameRenderer.class)
 public class MixinGameRenderer {
     @Inject(method = "getFov", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/GameRenderer;lastMovementFovMultiplier:F"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    /**
+     * @param camera Unused
+     * @param deltaTime Unused
+     * @param changingFov Unused
+     * @param cir used to tell the gameRenderer that the fov is actually our value and to pause calculation 
+     * @param currentFov used as the fov that is already used by the game without any effects
+     */
     private void onRequestFov(Camera camera, float deltaTime, boolean changingFov, CallbackInfoReturnable<Double> cir, double currentFov){
+        // SodiumClientMod.options() actually holds what the user set values are and should be checked before applying changes 
         if(SodiumClientMod.options().quality.staticFov){
             cir.setReturnValue(currentFov);
         }
