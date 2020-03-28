50ffead5d1edd6243566f3647a6cd5e3d97d1af6
Angeline <jellysquid3@users.noreply.github.com>
2020-03-28 15:29:58 -0500

    new: Implement specialized Matrix rotation methods

--- src/main/java/me/jellysquid/mods/sodium/mixin/fast_mojmath/MixinMatrix4f.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/fast_mojmath/MixinMatrix4f.java
@@ -6,68 +6,165 @@ import net.minecraft.util.math.Quaternion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-import static me.jellysquid.mods.sodium.client.util.MathUtil.fma;
-
 @Mixin(Matrix4f.class)
 public class MixinMatrix4f implements ExtendedMatrix {
-
-    @Shadow protected float a30;
-
-    @Shadow protected float a31;
-
-    @Shadow protected float a32;
-
     @Shadow protected float a00;
-
     @Shadow protected float a01;
-
     @Shadow protected float a02;
-
     @Shadow protected float a03;
-
-    @Shadow protected float a33;
-
     @Shadow protected float a10;
-
     @Shadow protected float a11;
-
     @Shadow protected float a12;
-
     @Shadow protected float a13;
-
     @Shadow protected float a20;
-
     @Shadow protected float a21;
-
     @Shadow protected float a22;
-
     @Shadow protected float a23;
+    @Shadow protected float a30;
+    @Shadow protected float a31;
+    @Shadow protected float a32;
+    @Shadow protected float a33;
 
     @Override
     public void rotate(Quaternion quaternion) {
-        // TODO: de-obfuscate this soup
-        float float_1 = quaternion.getB();
-        float float_2 = quaternion.getC();
-        float float_3 = quaternion.getD();
-        float float_4 = quaternion.getA();
-        float float_5 = 2.0F * float_1 * float_1;
-        float float_6 = 2.0F * float_2 * float_2;
-        float float_7 = 2.0F * float_3 * float_3;
-        float ta00 = 1.0F - float_6 - float_7;
-        float ta11 = 1.0F - float_7 - float_5;
-        float ta22 = 1.0F - float_5 - float_6;
-        float float_8 = float_1 * float_2;
-        float float_9 = float_2 * float_3;
-        float float_10 = float_3 * float_1;
-        float float_11 = float_1 * float_4;
-        float float_12 = float_2 * float_4;
-        float float_13 = float_3 * float_4;
-        float ta10 = 2.0F * (float_8 + float_13);
-        float ta01 = 2.0F * (float_8 - float_13);
-        float ta20 = 2.0F * (float_10 - float_12);
-        float ta02 = 2.0F * (float_10 + float_12);
-        float ta21 = 2.0F * (float_9 + float_11);
-        float ta12 = 2.0F * (float_9 - float_11);
+        boolean x = quaternion.getB() != 0.0F;
+        boolean y = quaternion.getC() != 0.0F;
+        boolean z = quaternion.getD() != 0.0F;
+
+        // Try to determine if this is a simple rotation on one axis component only
+        if (x) {
+            if (!y && !z) {
+                this.rotateX(quaternion);
+            } else {
+                this.rotateXYZ(quaternion);
+            }
+        } else if (y) {
+            if (!z) {
+                this.rotateY(quaternion);
+            } else {
+                this.rotateXYZ(quaternion);
+            }
+        } else if (z) {
+            this.rotateZ(quaternion);
+        }
+    }
+
+    private void rotateX(Quaternion quaternion) {
+        float x = quaternion.getB();
+        float w = quaternion.getA();
+
+        float xx = 2.0F * x * x;
+        float ta11 = 1.0F - xx;
+        float ta22 = 1.0F - xx;
+
+        float xw = x * w;
+
+        float ta21 = 2.0F * xw;
+        float ta12 = 2.0F * -xw;
+
+        float a01 = this.a01 * ta11 + this.a02 * ta21;
+        float a02 = this.a01 * ta12 + this.a02 * ta22;
+        float a11 = this.a11 * ta11 + this.a12 * ta21;
+        float a12 = this.a11 * ta12 + this.a12 * ta22;
+        float a21 = this.a21 * ta11 + this.a22 * ta21;
+        float a22 = this.a21 * ta12 + this.a22 * ta22;
+        float a31 = this.a31 * ta11 + this.a32 * ta21;
+        float a32 = this.a31 * ta12 + this.a32 * ta22;
+
+        this.a01 = a01;
+        this.a02 = a02;
+        this.a11 = a11;
+        this.a12 = a12;
+        this.a21 = a21;
+        this.a22 = a22;
+        this.a31 = a31;
+        this.a32 = a32;
+    }
+
+    private void rotateY(Quaternion quaternion) {
+        float y = quaternion.getC();
+        float w = quaternion.getA();
+
+        float yy = 2.0F * y * y;
+        float ta00 = 1.0F - yy;
+        float ta22 = 1.0F - yy;
+        float yw = y * w;
+        float ta20 = 2.0F * -yw;
+        float ta02 = 2.0F * yw;
+
+        float a00 = this.a00 * ta00 + this.a02 * ta20;
+        float a02 = this.a00 * ta02 + this.a02 * ta22;
+        float a10 = this.a10 * ta00 + this.a12 * ta20;
+        float a12 = this.a10 * ta02 + this.a12 * ta22;
+        float a20 = this.a20 * ta00 + this.a22 * ta20;
+        float a22 = this.a20 * ta02 + this.a22 * ta22;
+        float a30 = this.a30 * ta00 + this.a32 * ta20;
+        float a32 = this.a30 * ta02 + this.a32 * ta22;
+
+        this.a00 = a00;
+        this.a02 = a02;
+        this.a10 = a10;
+        this.a12 = a12;
+        this.a20 = a20;
+        this.a22 = a22;
+        this.a30 = a30;
+        this.a32 = a32;
+    }
+
+    private void rotateZ(Quaternion quaternion) {
+        float z = quaternion.getD();
+        float w = quaternion.getA();
+
+        float zz = 2.0F * z * z;
+        float ta00 = 1.0F - zz;
+        float ta11 = 1.0F - zz;
+        float zw = z * w;
+        float ta10 = 2.0F * (zw);
+        float ta01 = 2.0F * (-zw);
+
+        float a00 = this.a00 * ta00 + this.a01 * ta10;
+        float a01 = this.a00 * ta01 + this.a01 * ta11;
+        float a10 = this.a10 * ta00 + this.a11 * ta10;
+        float a11 = this.a10 * ta01 + this.a11 * ta11;
+        float a20 = this.a20 * ta00 + this.a21 * ta10;
+        float a21 = this.a20 * ta01 + this.a21 * ta11;
+        float a30 = this.a30 * ta00 + this.a31 * ta10;
+        float a31 = this.a30 * ta01 + this.a31 * ta11;
+
+        this.a00 = a00;
+        this.a01 = a01;
+        this.a10 = a10;
+        this.a11 = a11;
+        this.a20 = a20;
+        this.a21 = a21;
+        this.a30 = a30;
+        this.a31 = a31;
+    }
+
+    private void rotateXYZ(Quaternion quaternion) {
+        float x = quaternion.getB();
+        float y = quaternion.getC();
+        float z = quaternion.getD();
+        float w = quaternion.getA();
+
+        float xx = 2.0F * x * x;
+        float yy = 2.0F * y * y;
+        float zz = 2.0F * z * z;
+        float ta00 = 1.0F - yy - zz;
+        float ta11 = 1.0F - zz - xx;
+        float ta22 = 1.0F - xx - yy;
+        float xy = x * y;
+        float yz = y * z;
+        float zx = z * x;
+        float xw = x * w;
+        float yw = y * w;
+        float zw = z * w;
+        float ta10 = 2.0F * (xy + zw);
+        float ta01 = 2.0F * (xy - zw);
+        float ta20 = 2.0F * (zx - yw);
+        float ta02 = 2.0F * (zx + yw);
+        float ta21 = 2.0F * (yz + xw);
+        float ta12 = 2.0F * (yz - xw);
 
         float a00 = this.a00 * ta00 + this.a01 * ta10 + this.a02 * ta20;
         float a01 = this.a00 * ta01 + this.a01 * ta11 + this.a02 * ta21;
@@ -98,10 +195,10 @@ public class MixinMatrix4f implements ExtendedMatrix {
 
     @Override
     public void translate(float x, float y, float z) {
-        this.a03 = fma(this.a00, x, fma(this.a01, y, fma(this.a02, z, this.a03)));
-        this.a13 = fma(this.a10, x, fma(this.a11, y, fma(this.a12, z, this.a13)));
-        this.a23 = fma(this.a20, x, fma(this.a21, y, fma(this.a22, z, this.a23)));
-        this.a33 = fma(this.a30, x, fma(this.a31, y, fma(this.a32, z, this.a33)));
+        this.a03 = this.a00 * x + this.a01 * y + this.a02 * z + this.a03;
+        this.a13 = this.a10 * x + this.a11 * y + this.a12 * z + this.a13;
+        this.a23 = this.a20 * x + this.a21 * y + this.a22 * z + this.a23;
+        this.a33 = this.a30 * x + this.a31 * y + this.a32 * z + this.a33;
     }
 
 }
