0a235fe0ec7926f479111319d3f1b6e139d5fe04
Angeline <jellysquid3@users.noreply.github.com>
2020-04-05 20:40:50 -0500

    fix: Use atomic operations in the biome cache

--- src/main/java/me/jellysquid/mods/sodium/client/world/BiomeCache.java
+++ src/main/java/me/jellysquid/mods/sodium/client/world/BiomeCache.java
@@ -4,25 +4,27 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.source.BiomeAccess;
 import net.minecraft.world.biome.source.BiomeAccessType;
 
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
 public class BiomeCache {
     private final BiomeAccessType type;
     private final long seed;
 
-    private Biome[] biomes;
+    private AtomicReferenceArray<Biome> biomes;
 
     public BiomeCache(BiomeAccessType type, long seed) {
         this.type = type;
         this.seed = seed;
-        this.biomes = new Biome[16 * 16 * 16];
+        this.biomes = new AtomicReferenceArray<>(16 * 16 * 16);
     }
 
     public Biome getBiome(BiomeAccess.Storage storage, int x, int y, int z) {
         int idx = ((y & 15) << 8) | ((z & 15) << 4) | (x & 15);
 
-        Biome biome = this.biomes[idx];
+        Biome biome = this.biomes.get(idx);
 
         if (biome == null) {
-            this.biomes[idx] = biome = this.type.getBiome(this.seed, x, y, z, storage);
+            this.biomes.compareAndSet(idx, null, biome = this.type.getBiome(this.seed, x, y, z, storage));
         }
 
         return biome;
