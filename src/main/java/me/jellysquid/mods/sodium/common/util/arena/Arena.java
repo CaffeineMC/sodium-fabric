e0722bc7d3516b59ebbd76d46b20792e48fcdd4d
Angeline <jellysquid3@users.noreply.github.com>
2020-04-17 18:54:31 -0500

    change: Make Arenas thread-safe

--- src/main/java/me/jellysquid/mods/sodium/common/util/arena/Arena.java
+++ src/main/java/me/jellysquid/mods/sodium/common/util/arena/Arena.java
@@ -16,7 +16,9 @@ public class Arena<T extends ReusableObject> {
      * Drops all objects from the arena.
      */
     public void reset() {
-        this.pool.clear();
+        synchronized (this.pool) {
+            this.pool.clear();
+        }
     }
 
     /**
@@ -24,7 +26,11 @@ public class Arena<T extends ReusableObject> {
      * re-usable objects in the arena, they will be used instead of allocating a new object.
      */
     public T allocate() {
-        T obj = this.pool.poll();
+        T obj;
+
+        synchronized (this.pool) {
+            obj = this.pool.poll();
+        }
 
         if (obj == null) {
             obj = this.factory.get();
@@ -45,7 +51,9 @@ public class Arena<T extends ReusableObject> {
      */
     public void release(T obj) {
         if (obj.releaseReference()) {
-            this.pool.offer(obj);
+            synchronized (this.pool) {
+                this.pool.offer(obj);
+            }
         }
     }
 
