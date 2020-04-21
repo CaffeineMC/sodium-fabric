359754c2f9716c583f091a87732d5885ef0ed39c
Angeline <jellysquid3@users.noreply.github.com>
2020-04-20 19:17:25 -0500

    change: Allow color blenders full context to enable them to better optimize

--- src/main/java/me/jellysquid/mods/sodium/client/render/model/quad/blender/FlatVertexColorBlender.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/model/quad/blender/FlatVertexColorBlender.java
@@ -1,5 +1,6 @@
 package me.jellysquid.mods.sodium.client.render.model.quad.blender;
 
+import me.jellysquid.mods.sodium.client.render.model.quad.ModelQuadView;
 import me.jellysquid.mods.sodium.client.util.ColorUtil;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.color.block.BlockColorProvider;
@@ -7,14 +8,22 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
 
 public class FlatVertexColorBlender implements VertexColorBlender {
+    private final int[] cachedRet = new int[4];
+
     @Override
-    public int getColor(BlockColorProvider provider, BlockState state, BlockRenderView world, int color, int colorIndex, float posX, float posZ, BlockPos origin, float brightness) {
+    public int[] getColors(BlockColorProvider provider, BlockState state, BlockRenderView world, ModelQuadView quad, BlockPos origin, int colorIndex, float[] brightness) {
         int biomeColor = provider.getColor(state, world, origin, colorIndex);
 
         float r = ColorUtil.normalize(ColorUtil.unpackColorR(biomeColor));
         float g = ColorUtil.normalize(ColorUtil.unpackColorG(biomeColor));
         float b = ColorUtil.normalize(ColorUtil.unpackColorB(biomeColor));
 
-        return ColorUtil.mulPackedRGB(color, r * brightness,  g * brightness, b * brightness);
+        int[] colors = this.cachedRet;
+
+        for (int i = 0; i < 4; i++) {
+            colors[i] = ColorUtil.mulPackedRGB(quad.getColor(i), r * brightness[i],  g * brightness[i], b * brightness[i]);
+        }
+
+        return colors;
     }
 }
