1119748633992dd78c09565d31c1e7a4ab5b004e
Angeline <jellysquid3@users.noreply.github.com>
2020-04-21 12:59:56 -0500

    change: Remove compact vertex format option
    Causes too many problems -- we can't make guarantees about how Minecraft will
    use values.

--- src/main/java/me/jellysquid/mods/sodium/client/gl/SodiumVertexFormats.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gl/SodiumVertexFormats.java
@@ -11,29 +11,17 @@ public class SodiumVertexFormats {
     /**
      * Standard vertex format with single-precision floating point values. Closely mirrors the vanilla vertex format.
      */
-    public static final GlVertexFormat<ChunkMeshAttribute> CHUNK_MESH_SFP = GlVertexAttribute.builder(ChunkMeshAttribute.class)
+    public static final GlVertexFormat<ChunkMeshAttribute> CHUNK_MESH_VANILLA = GlVertexAttribute.builder(ChunkMeshAttribute.class)
             .add(ChunkMeshAttribute.POSITION, new GlVertexAttribute(GlVertexAttributeFormat.FLOAT, 3, false, 0))
             .add(ChunkMeshAttribute.COLOR, new GlVertexAttribute(GlVertexAttributeFormat.UNSIGNED_BYTE, 4, true, 12))
             .add(ChunkMeshAttribute.TEXTURE, new GlVertexAttribute(GlVertexAttributeFormat.FLOAT, 2, false, 16))
             .add(ChunkMeshAttribute.LIGHT, new GlVertexAttribute(GlVertexAttributeFormat.SHORT, 2, false, 24))
             .build(28);
 
-    /**
-     * Compact vertex format which makes use of normalized unsigned shorts to represent floating point values. This
-     * format normalizes the position attribute of a vertex from [-8,24] to [-1,1] with no precision loss for whole
-     * values.
-     */
-    public static final GlVertexFormat<ChunkMeshAttribute> CHUNK_MESH_HFP = GlVertexAttribute.builder(ChunkMeshAttribute.class)
-            .add(ChunkMeshAttribute.POSITION, new GlVertexAttribute(GlVertexAttributeFormat.UNSIGNED_SHORT, 3, true, 0))
-            .add(ChunkMeshAttribute.COLOR, new GlVertexAttribute(GlVertexAttributeFormat.UNSIGNED_BYTE, 4, true, 8))
-            .add(ChunkMeshAttribute.TEXTURE, new GlVertexAttribute(GlVertexAttributeFormat.UNSIGNED_SHORT, 2, true, 12))
-            .add(ChunkMeshAttribute.LIGHT, new GlVertexAttribute(GlVertexAttributeFormat.SHORT, 2, false, 16))
-            .build(20);
-
     private static final Reference2ObjectMap<GlVertexFormat<?>, ModelQuadEncoder> encoders = new Reference2ObjectOpenHashMap<>();
 
     static {
-        registerEncoder(CHUNK_MESH_SFP, (buffer, position, quad, x, y, z) -> {
+        registerEncoder(CHUNK_MESH_VANILLA, (buffer, position, quad, x, y, z) -> {
             for (int i = 0; i < 4; i++) {
                 buffer.putFloat(position, quad.getX(i) + x);
                 buffer.putFloat(position + 4, quad.getY(i) + y);
@@ -44,20 +32,6 @@ public class SodiumVertexFormats {
                 buffer.putInt(position + 24, quad.getLight(i));
 
                 position += 28;
-            }
-        });
-
-        registerEncoder(CHUNK_MESH_HFP, (buffer, position, quad, x, y, z) -> {
-            for (int i = 0; i < 4; i++) {
-                buffer.putShort(position, normalizeFloat(quad.getX(i) + x));
-                buffer.putShort(position + 2, normalizeFloat(quad.getY(i) + y));
-                buffer.putShort(position + 4, normalizeFloat(quad.getZ(i) + z));
-                buffer.putInt(position + 8, quad.getColor(i));
-                buffer.putShort(position + 12, normalizeTex(quad.getTexU(i)));
-                buffer.putShort(position + 14, normalizeTex(quad.getTexV(i)));
-                buffer.putInt(position + 16, quad.getLight(i));
-
-                position += 20;
             }
         });
     }
@@ -85,13 +59,5 @@ public class SodiumVertexFormats {
         COLOR,
         TEXTURE,
         LIGHT
-    }
-
-    private static short normalizeFloat(float value) {
-        return (short) ((((value + 8.0f) / 32.0f) * 65536.0f) - 1.0f);
-    }
-
-    private static short normalizeTex(float value) {
-        return (short) ((value * 65536.0f) - 1.0f);
     }
 }
