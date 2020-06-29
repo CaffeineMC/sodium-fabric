fbd72a01eadf83e7c528dba356f6b40351d6d18c
JellySquid <jellysquid3@users.noreply.github.com>
2020-06-28 22:56:27 -0500

    change: Use per-pixel fog shading instead of per-vertex shading
    This improves the quality of rendered fog drastically when the far
    plane is very close to the player.

--- src/main/resources/assets/sodium/shaders/chunk_gl20.f.glsl
+++ src/main/resources/assets/sodium/shaders/chunk_gl20.f.glsl
@@ -4,13 +4,33 @@ varying vec4 v_Color; // The interpolated vertex color
 varying vec2 v_TexCoord; // The interpolated block texture coordinates
 varying vec2 v_LightCoord; // The interpolated light map texture coordinates
 
+uniform sampler2D u_BlockTex; // The block texture sampler
+uniform sampler2D u_LightTex; // The light map texture sampler
+
 #ifdef USE_FOG
-varying float v_FogFactor; // Normalized blend ratio for fog color
+varying float v_FragDistance;
 uniform vec4 u_FogColor; // The color of the fog
 #endif
 
-uniform sampler2D u_BlockTex; // The block texture sampler
-uniform sampler2D u_LightTex; // The light map texture sampler
+#ifdef USE_FOG_EXP2
+uniform float u_FogDensity;
+
+// e^(-density * c^2)
+float getFogFactor() {
+    float dist = v_FragDistance * u_FogDensity;
+    return 1.0 / exp2(dist * dist);
+}
+#endif
+
+#ifdef USE_FOG_LINEAR
+uniform float u_FogLength; // FogStart - FogEnd
+uniform float u_FogEnd;
+
+// (end - dist) / (end - start)
+float getFogFactor() {
+    return (u_FogEnd - v_FragDistance) / u_FogLength;
+}
+#endif
 
 void main() {
     // Block texture sample
@@ -23,8 +43,10 @@ void main() {
     vec4 diffuseColor = sampleBlockTex * sampleLightTex * v_Color;
 
 #ifdef USE_FOG
-    // Mix the fog color into the texture color by the fog factor
-    gl_FragColor = vec4(mix(u_FogColor.rgb, diffuseColor.rgb, v_FogFactor), diffuseColor.a);
+    float fogFactor = clamp(getFogFactor(), 0.0, 1.0);
+
+    gl_FragColor = mix(u_FogColor, diffuseColor, fogFactor);
+    gl_FragColor.a = diffuseColor.a;
 #else
     // No fog is being used, so the fragment color is just that of the blended texture color
     gl_FragColor = diffuseColor;
