b359f289d1cb8687446616920e66f01ce1055706
JellySquid <jellysquid3@users.noreply.github.com>
2020-07-07 00:46:27 -0500

    change: Clean up matrix and buffer intrinsics, fix memory safety issues
    Closes #59

--- src/main/java/me/jellysquid/mods/sodium/mixin/buffers/MixinBufferBuilder.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/buffers/MixinBufferBuilder.java
@@ -6,8 +6,11 @@ import me.jellysquid.mods.sodium.client.model.quad.ModelQuadView;
 import me.jellysquid.mods.sodium.client.util.ColorARGB;
 import me.jellysquid.mods.sodium.client.util.Norm3b;
 import me.jellysquid.mods.sodium.client.util.UnsafeUtil;
-import me.jellysquid.mods.sodium.common.util.matrix.MatrixUtil;
-import net.minecraft.client.render.*;
+import me.jellysquid.mods.sodium.client.util.math.MatrixUtil;
+import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.render.FixedColorVertexConsumer;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormats;
 import net.minecraft.client.render.model.BakedQuad;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.util.math.Vector4f;
@@ -15,58 +18,126 @@ import net.minecraft.util.math.Matrix3f;
 import net.minecraft.util.math.Matrix4f;
 import org.lwjgl.system.MemoryUtil;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import sun.misc.Unsafe;
 
 import java.nio.ByteBuffer;
 
 @Mixin(BufferBuilder.class)
-public abstract class MixinBufferBuilder extends FixedColorVertexConsumer implements
-        ParticleVertexConsumer, QuadVertexConsumer, BufferVertexConsumer {
+public abstract class MixinBufferBuilder extends FixedColorVertexConsumer
+        implements ParticleVertexConsumer, QuadVertexConsumer {
     @Shadow
-    private int elementOffset;
-
-    @Shadow
-    public abstract void next();
-
-    @Shadow
-    private boolean field_21594; // canUseVertexPath
-
-    @Shadow
-    private boolean field_21595; // hasOverlay
+    private VertexFormat format;
 
     @Shadow
     private ByteBuffer buffer;
 
     @Shadow
-    private VertexFormat format;
+    private int elementOffset;
 
     @Shadow
     private int vertexCount;
 
     @Shadow
+    private boolean field_21595; // has overlay
+
+    @Shadow
+    private boolean field_21594; // is baked quad format
+
+    @Shadow
     protected abstract void grow(int size);
 
-    /**
-     * @reason Use faster implementation which works directly with Unsafe
-     * @author JellySquid
-     */
-    @Overwrite
-    public void vertex(float x, float y, float z, float r, float g, float b, float a, float u, float v, int overlay, int light, float normX, float normY, float normZ) {
-        if (!this.field_21594) {
-            super.vertex(x, y, z, r, g, b, a, u, v, overlay, light, normX, normY, normZ);
-
+    @Override
+    public void vertexParticle(float x, float y, float z, float u, float v, int color, int light) {
+        if (this.format != VertexFormats.POSITION_TEXTURE_COLOR_LIGHT) {
+            this.vertexParticleFallback(x, y, z, u, v, color, light);
             return;
         }
 
-        this.vertexQuad(x, y, z, ColorARGB.pack(r, g, b, a), u, v, overlay, light, Norm3b.pack(normX, normY, normZ));
+        int size = this.format.getVertexSize();
+
+        this.grow(size);
+
+        if (UnsafeUtil.isAvailable()) {
+            this.vertexParticleUnsafe(x, y, z, u, v, color, light);
+        } else {
+            this.vertexParticleSafe(x, y, z, u, v, color, light);
+        }
+
+        this.elementOffset += size;
+        this.vertexCount++;
+    }
+
+    private void vertexParticleFallback(float x, float y, float z, float u, float v, int color, int light) {
+        this.vertex(x, y, z);
+        this.texture(u, v);
+        this.color(ColorARGB.unpackRed(color), ColorARGB.unpackGreen(color), ColorARGB.unpackBlue(color),
+                ColorARGB.unpackAlpha(color));
+        this.light(light);
+        this.next();
+    }
+
+    private void vertexParticleSafe(float x, float y, float z, float u, float v, int color, int light) {
+        int i = this.elementOffset;
+
+        ByteBuffer buffer = this.buffer;
+        buffer.putFloat(i, x);
+        i += 4;
+
+        buffer.putFloat(i, y);
+        i += 4;
+
+        buffer.putFloat(i, z);
+        i += 4;
+
+        buffer.putFloat(i, u);
+        i += 4;
+
+        buffer.putFloat(i, v);
+        i += 4;
+
+        buffer.putInt(i, color);
+        i += 4;
+
+        buffer.putInt(i, light);
+        i += 4;
+    }
+
+    private void vertexParticleUnsafe(float x, float y, float z, float u, float v, int color, int light) {
+        long i = MemoryUtil.memAddress(this.buffer, this.elementOffset);
+
+        Unsafe unsafe = UnsafeUtil.instance();
+        unsafe.putFloat(i, x);
+        i += 4;
+
+        unsafe.putFloat(i, y);
+        i += 4;
+
+        unsafe.putFloat(i, z);
+        i += 4;
+
+        unsafe.putFloat(i, u);
+        i += 4;
+
+        unsafe.putFloat(i, v);
+        i += 4;
+
+        unsafe.putInt(i, color);
+        i += 4;
+
+        unsafe.putInt(i, light);
+        i += 4;
     }
 
     @Override
     public void vertexQuad(float x, float y, float z, int color, float u, float v, int overlay, int light, int normal) {
         if (this.colorFixed) {
             throw new IllegalStateException();
+        }
+
+        if (!this.field_21594) {
+            this.vertexQuadFallback(x, y, z, color, u, v, overlay, light, normal);
+            return;
         }
 
         int size = this.format.getVertexSize();
@@ -81,6 +152,21 @@ public abstract class MixinBufferBuilder extends FixedColorVertexConsumer implem
 
         this.elementOffset += size;
         this.vertexCount++;
+    }
+
+    private void vertexQuadFallback(float x, float y, float z, int color, float u, float v, int overlay, int light, int normal) {
+        this.vertex(x, y, z);
+        this.color(ColorARGB.unpackRed(color), ColorARGB.unpackGreen(color), ColorARGB.unpackBlue(color),
+                ColorARGB.unpackAlpha(color));
+        this.texture(u, v);
+
+        if (this.field_21595) {
+            this.overlay(overlay);
+        }
+
+        this.light(light);
+        this.normal(Norm3b.unpackX(normal), Norm3b.unpackY(normal), Norm3b.unpackZ(normal));
+        this.next();
     }
 
     @SuppressWarnings("SuspiciousNameCombination")
@@ -148,78 +234,6 @@ public abstract class MixinBufferBuilder extends FixedColorVertexConsumer implem
         i += 4;
 
         buffer.putInt(i, normal);
-    }
-
-    @Override
-    public void vertexParticle(float x, float y, float z, float u, float v, int color, int light) {
-        if (this.format != VertexFormats.POSITION_TEXTURE_COLOR_LIGHT) {
-            throw new IllegalStateException("Invalid vertex format");
-        }
-
-        int size = this.format.getVertexSize();
-
-        this.grow(size);
-
-        if (UnsafeUtil.isAvailable()) {
-            this.vertexParticleUnsafe(x, y, z, u, v, color, light);
-        } else {
-            this.vertexParticleFallback(x, y, z, u, v, color, light);
-        }
-
-        this.elementOffset += size;
-        this.vertexCount++;
-    }
-
-    private void vertexParticleFallback(float x, float y, float z, float u, float v, int color, int light) {
-        int i = this.elementOffset;
-
-        ByteBuffer buffer = this.buffer;
-        buffer.putFloat(i, x);
-        i += 4;
-
-        buffer.putFloat(i, y);
-        i += 4;
-
-        buffer.putFloat(i, z);
-        i += 4;
-
-        buffer.putFloat(i, u);
-        i += 4;
-
-        buffer.putFloat(i, v);
-        i += 4;
-
-        buffer.putInt(i, color);
-        i += 4;
-
-        buffer.putInt(i, light);
-        i += 4;
-    }
-
-    private void vertexParticleUnsafe(float x, float y, float z, float u, float v, int color, int light) {
-        long i = MemoryUtil.memAddress(this.buffer, this.elementOffset);
-
-        Unsafe unsafe = UnsafeUtil.instance();
-        unsafe.putFloat(i, x);
-        i += 4;
-
-        unsafe.putFloat(i, y);
-        i += 4;
-
-        unsafe.putFloat(i, z);
-        i += 4;
-
-        unsafe.putFloat(i, u);
-        i += 4;
-
-        unsafe.putFloat(i, v);
-        i += 4;
-
-        unsafe.putInt(i, color);
-        i += 4;
-
-        unsafe.putInt(i, light);
-        i += 4;
     }
 
     @Override
