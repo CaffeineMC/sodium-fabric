604a8085119785723b8eea6d51f0e054f505df0f
JellySquid <jellysquid3@users.noreply.github.com>
2020-07-08 00:20:30 -0500

    fix: Force a minimum bounding box size for smooth entity lighting
    Closes #60
    Closes #48

--- src/main/java/me/jellysquid/mods/sodium/client/model/light/EntityLighter.java
+++ src/main/java/me/jellysquid/mods/sodium/client/model/light/EntityLighter.java
@@ -7,6 +7,8 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.LightType;
 
 public class EntityLighter {
+    private static final double MIN_BOX_SIZE = 0.001D;
+
     private static final double MAX_LIGHT_VAL = 15.0;
     private static final double MAX_LIGHTMAP_COORD = 240.0D;
 
@@ -17,17 +19,24 @@ public class EntityLighter {
         double x1 = MathHelper.lerp(tickDelta, entity.prevX, entity.getX());
         double y1 = MathHelper.lerp(tickDelta, entity.prevY, entity.getY());
         double z1 = MathHelper.lerp(tickDelta, entity.prevZ, entity.getZ());
-        double x2 = x1 + entity.getWidth();
-        double y2 = y1 + entity.getHeight();
-        double z2 = z1 + entity.getWidth();
+
+        // Bounding boxes with no volume cause issues, ensure they're non-zero
+        // Notably, armor stands in "Marker" mode decide this is a cute thing to do
+        // https://github.com/jellysquid3/sodium-fabric/issues/60
+        double width = Math.max(entity.getWidth(), MIN_BOX_SIZE);
+        double height = Math.max(entity.getHeight(), MIN_BOX_SIZE);
+
+        double x2 = x1 + width;
+        double y2 = y1 + height;
+        double z2 = z1 + width;
 
         // The sampling volume of blocks which could possibly contribute light to this entity
         int bMinX = MathHelper.floor(x1);
         int bMinY = MathHelper.floor(y1);
         int bMinZ = MathHelper.floor(z1);
-        int bMaxX = MathHelper.floor(x2);
-        int bMaxY = MathHelper.floor(y2);
-        int bMaxZ = MathHelper.floor(z2);
+        int bMaxX = MathHelper.ceil(x2);
+        int bMaxY = MathHelper.ceil(y2);
+        int bMaxZ = MathHelper.ceil(z2);
 
         // The maximum amount of light that could be contributed
         double max = 0.0D;
@@ -39,9 +48,15 @@ public class EntityLighter {
         BlockPos.Mutable pos = new BlockPos.Mutable();
 
         // Iterate over every block in the sampling volume
-        for (int bX = bMinX; bX <= bMaxX; bX++) {
-            for (int bY = bMinY; bY <= bMaxY; bY++) {
-                for (int bZ = bMinZ; bZ <= bMaxZ; bZ++) {
+        for (int bX = bMinX; bX < bMaxX; bX++) {
+            double ix1 = Math.max(bX, x1);
+            double ix2 = Math.min(bX + 1, x2);
+
+            for (int bY = bMinY; bY < bMaxY; bY++) {
+                double iy1 = Math.max(bY, y1);
+                double iy2 = Math.min(bY + 1, y2);
+
+                for (int bZ = bMinZ; bZ < bMaxZ; bZ++) {
                     pos.set(bX, bY, bZ);
 
                     BlockState blockState = entity.world.getBlockState(pos);
@@ -52,11 +67,7 @@ public class EntityLighter {
                     }
 
                     // Find the intersecting volume between the entity box and the block's bounding box
-                    double ix1 = Math.max(bX, x1);
-                    double iy1 = Math.max(bY, y1);
                     double iz1 = Math.max(bZ, z1);
-                    double ix2 = Math.min(bX + 1, x2);
-                    double iy2 = Math.min(bY + 1, y2);
                     double iz2 = Math.min(bZ + 1, z2);
 
                     // The amount of light this block can contribute is the volume of the intersecting box
