1fe580e6be0f072dcf28a9325d3afa4e206290f4
JellySquid <jellysquid3@users.noreply.github.com>
2020-07-08 14:49:09 -0500

    fix: Avoid allocating BlockPos objects in BlockOcclusionCache

--- src/main/java/me/jellysquid/mods/sodium/client/render/occlusion/BlockOcclusionCache.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/occlusion/BlockOcclusionCache.java
@@ -1,6 +1,6 @@
 package me.jellysquid.mods.sodium.client.render.occlusion;
 
-import it.unimi.dsi.fastutil.objects.Object2ByteOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2ByteLinkedOpenHashMap;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
@@ -12,44 +12,49 @@ import net.minecraft.world.BlockView;
 public class BlockOcclusionCache {
     private static final byte UNCACHED_VALUE = (byte) 127;
 
-    private final Object2ByteOpenHashMap<CachedOcclusionShapeTest> map;
+    private final Object2ByteLinkedOpenHashMap<CachedOcclusionShapeTest> map;
     private final CachedOcclusionShapeTest cachedTest = new CachedOcclusionShapeTest();
     private final BlockPos.Mutable cpos = new BlockPos.Mutable();
 
     public BlockOcclusionCache() {
-        this.map = new Object2ByteOpenHashMap<>(2048, 0.75F);
+        this.map = new Object2ByteLinkedOpenHashMap<>(2048, 0.5F);
         this.map.defaultReturnValue(UNCACHED_VALUE);
     }
 
     /**
-     * @param state The state of the block in the world
+     * @param selfState The state of the block in the world
      * @param view The world view for this render context
      * @param pos The position of the block
      * @param facing The facing direction of the side to check
      * @return True if the block side facing {@param dir} is not occluded, otherwise false
      */
-    public boolean shouldDrawSide(BlockState state, BlockView view, BlockPos pos, Direction facing) {
-        BlockPos adjPos = this.cpos.set(pos).offset(facing);
+    public boolean shouldDrawSide(BlockState selfState, BlockView view, BlockPos pos, Direction facing) {
+        BlockPos.Mutable adjPos = this.cpos;
+        adjPos.set(pos.getX() + facing.getOffsetX(), pos.getY() + facing.getOffsetY(), pos.getZ() + facing.getOffsetZ());
+
         BlockState adjState = view.getBlockState(adjPos);
 
-        if (state.isSideInvisible(adjState, facing)) {
+        if (selfState.isSideInvisible(adjState, facing)) {
             return false;
-        }
+        } else if (adjState.isOpaque()) {
+            VoxelShape selfShape = selfState.getCullingFace(view, pos, facing);
+            VoxelShape adjShape = adjState.getCullingFace(view, adjPos, facing.getOpposite());
 
-        if (!adjState.isOpaque()) {
+            if (selfShape == VoxelShapes.fullCube() && adjShape == VoxelShapes.fullCube()) {
+                return false;
+            }
+
+            return this.calculate(selfShape, adjShape);
+        } else {
             return true;
         }
+    }
 
-        VoxelShape selfShape = state.getCullingFace(view, pos, facing);
-        VoxelShape adjShape = adjState.getCullingFace(view, adjPos, facing.getOpposite());
-
-        if (selfShape == VoxelShapes.fullCube() && adjShape == VoxelShapes.fullCube()) {
-            return false;
-        }
-
+    private boolean calculate(VoxelShape selfShape, VoxelShape adjShape) {
         CachedOcclusionShapeTest cache = this.cachedTest;
         cache.a = selfShape;
         cache.b = adjShape;
+        cache.updateHash();
 
         byte cached = this.map.getByte(cache);
 
@@ -61,42 +66,53 @@ public class BlockOcclusionCache {
 
         this.map.put(cache.copy(), (byte) (ret ? 1 : 0));
 
+        if (this.map.size() > 2048) {
+            this.map.removeLastByte();
+        }
+
         return ret;
     }
 
     private static final class CachedOcclusionShapeTest {
         private VoxelShape a, b;
+        private int hashCode;
 
         private CachedOcclusionShapeTest() {
 
         }
 
-        private CachedOcclusionShapeTest(VoxelShape a, VoxelShape b) {
+        private CachedOcclusionShapeTest(VoxelShape a, VoxelShape b, int hashCode) {
             this.a = a;
             this.b = b;
+            this.hashCode = hashCode;
+        }
+
+        public void updateHash() {
+            int result = System.identityHashCode(this.a);
+            result = 31 * result + System.identityHashCode(this.b);
+
+            this.hashCode = result;
         }
 
         public CachedOcclusionShapeTest copy() {
-            return new CachedOcclusionShapeTest(this.a, this.b);
+            return new CachedOcclusionShapeTest(this.a, this.b, this.hashCode);
         }
 
         @Override
         public boolean equals(Object o) {
-            if (!(o instanceof CachedOcclusionShapeTest)) {
-                return false;
+            if (o instanceof CachedOcclusionShapeTest) {
+                CachedOcclusionShapeTest that = (CachedOcclusionShapeTest) o;
+
+                return this.a == that.a &&
+                        this.b == that.b;
             }
 
-            CachedOcclusionShapeTest that = (CachedOcclusionShapeTest) o;
-
-            return this.a == that.a && this.b == that.b;
+            return false;
         }
 
         @Override
         public int hashCode() {
-            int result = System.identityHashCode(this.a);
-            result = 31 * result + System.identityHashCode(this.b);
-
-            return result;
+            return this.hashCode;
         }
     }
 }
