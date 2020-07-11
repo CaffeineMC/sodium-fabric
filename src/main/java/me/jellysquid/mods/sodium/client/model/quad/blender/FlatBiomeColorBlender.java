f9c576e1247d0a1cbe14459bb642e93847783568
JellySquid <jellysquid3@users.noreply.github.com>
2020-07-11 18:37:52 -0500

    fix: Use ABGR color encoding for flat color blending

--- src/main/java/me/jellysquid/mods/sodium/client/model/quad/blender/FlatBiomeColorBlender.java
+++ src/main/java/me/jellysquid/mods/sodium/client/model/quad/blender/FlatBiomeColorBlender.java
@@ -1,6 +1,7 @@
 package me.jellysquid.mods.sodium.client.model.quad.blender;
 
 import me.jellysquid.mods.sodium.client.model.quad.ModelQuadView;
+import me.jellysquid.mods.sodium.client.util.color.ColorARGB;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.color.block.BlockColorProvider;
 import net.minecraft.util.math.BlockPos;
@@ -17,7 +18,7 @@ public class FlatBiomeColorBlender implements BiomeColorBlender {
     @Override
     public int[] getColors(BlockColorProvider colorizer, BlockRenderView world, BlockState state, BlockPos origin,
                            ModelQuadView quad) {
-        Arrays.fill(this.cachedRet, colorizer.getColor(state, world, origin, quad.getColorIndex()));
+        Arrays.fill(this.cachedRet, ColorARGB.toABGR(colorizer.getColor(state, world, origin, quad.getColorIndex())));
 
         return this.cachedRet;
     }
