20efff5c175d65cffc53c11aca06c1afe2c42bcb
JellySquid <jellysquid3@users.noreply.github.com>
2020-07-11 21:03:19 -0500

    change: Use vanilla biome blend distance setting instead of our own

--- src/main/java/me/jellysquid/mods/sodium/client/render/pipeline/RenderContextCommon.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/pipeline/RenderContextCommon.java
@@ -1,12 +1,12 @@
 package me.jellysquid.mods.sodium.client.render.pipeline;
 
-import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.model.quad.blender.BiomeColorBlender;
 import me.jellysquid.mods.sodium.client.model.quad.blender.FlatBiomeColorBlender;
 import me.jellysquid.mods.sodium.client.model.quad.blender.SmoothBiomeColorBlender;
+import net.minecraft.client.MinecraftClient;
 
 public class RenderContextCommon {
     public static BiomeColorBlender createBiomeColorBlender() {
-        return SodiumClientMod.options().quality.biomeBlendDistance <= 0 ? new FlatBiomeColorBlender() : new SmoothBiomeColorBlender();
+        return MinecraftClient.getInstance().options.biomeBlendRadius <= 0 ? new FlatBiomeColorBlender() : new SmoothBiomeColorBlender();
     }
 }
