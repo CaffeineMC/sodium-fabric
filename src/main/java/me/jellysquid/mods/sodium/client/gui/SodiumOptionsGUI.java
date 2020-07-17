3a515994f54bb8a88edd6b9bd70888ee8ccc23a2
AMereBagatelle <themerebagatelle@gmail.com>
2020-07-17 19:56:19 -0400

    Adding sodium namespace to all translations

--- src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumOptionsGUI.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumOptionsGUI.java
@@ -79,9 +79,9 @@ public class SodiumOptionsGUI extends Screen {
         this.rebuildGUIPages();
         this.rebuildGUIOptions();
 
-        this.undoButton = new FlatButtonWidget(new Dim2i(this.width - 211, this.height - 30, 65, 20), I18n.translate("options.buttons.undo"), this::undoChanges);
-        this.applyButton = new FlatButtonWidget(new Dim2i(this.width - 142, this.height - 30, 65, 20), I18n.translate("options.buttons.apply"), this::applyChanges);
-        this.closeButton = new FlatButtonWidget(new Dim2i(this.width - 73, this.height - 30, 65, 20), I18n.translate("options.buttons.close"), this::onClose);
+        this.undoButton = new FlatButtonWidget(new Dim2i(this.width - 211, this.height - 30, 65, 20), I18n.translate("sodium.options.buttons.undo"), this::undoChanges);
+        this.applyButton = new FlatButtonWidget(new Dim2i(this.width - 142, this.height - 30, 65, 20), I18n.translate("sodium.options.buttons.apply"), this::applyChanges);
+        this.closeButton = new FlatButtonWidget(new Dim2i(this.width - 73, this.height - 30, 65, 20), I18n.translate("sodium.options.buttons.close"), this::onClose);
 
         this.children.add(this.undoButton);
         this.children.add(this.applyButton);
@@ -198,7 +198,7 @@ public class SodiumOptionsGUI extends Screen {
         OptionImpact impact = option.getImpact();
 
         if (impact != null) {
-            tooltip.add(new TranslatableText("options.tooltips.performance_impact_string", Formatting.GRAY, impact.toDisplayString()));
+            tooltip.add(new TranslatableText("sodium.options.tooltips.performance_impact_string", Formatting.GRAY, impact.toDisplayString()));
         }
 
         int boxHeight = (tooltip.size() * 12) + boxPadding;
--- src/main/java/me/jellysquid/mods/sodium/client/gui/options/control/ControlValueFormatter.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/options/control/ControlValueFormatter.java
@@ -4,19 +4,19 @@ import net.minecraft.client.resource.language.I18n;
 
 public interface ControlValueFormatter {
     static ControlValueFormatter guiScale() {
-        return (v) -> (v == 0) ? I18n.translate("control_value_formatter.gui_scale_auto") : v + "x";
+        return (v) -> (v == 0) ? I18n.translate("sodium.control_value_formatter.gui_scale_auto") : v + "x";
     }
 
     static ControlValueFormatter fpsLimit() {
-        return (v) -> (v == 260) ? I18n.translate("control_value_formatter.fps_limit_unlimited") : v + " FPS";
+        return (v) -> (v == 260) ? I18n.translate("sodium.control_value_formatter.fps_limit_unlimited") : v + " FPS";
     }
 
     static ControlValueFormatter brightness() {
         return (v) -> {
             if (v == 0) {
-                return I18n.translate("control_value_formatter.moody");
+                return I18n.translate("sodium.control_value_formatter.moody");
             } else if (v == 100) {
-                return I18n.translate("control_value_formatter.bright");
+                return I18n.translate("sodium.control_value_formatter.bright");
             } else {
                 return v + "%";
             }
