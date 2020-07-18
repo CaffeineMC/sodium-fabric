d7ff9a9cb265478fedefdfaa1fcc3fa8e95a8438
AMereBagatelle <56981737+AMereBagatelle@users.noreply.github.com>
2020-07-17 23:50:37 -0400

    new: Add translation for Sodium's video settings screen (#100)

--- src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptions.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptions.java
@@ -9,6 +9,7 @@ import me.jellysquid.mods.sodium.client.render.chunk.backends.gl20.GL20ChunkRend
 import me.jellysquid.mods.sodium.client.render.chunk.backends.gl30.GL30ChunkRenderBackend;
 import me.jellysquid.mods.sodium.client.render.chunk.backends.gl43.GL43ChunkRenderBackend;
 import net.minecraft.client.options.GraphicsMode;
+import net.minecraft.client.resource.language.I18n;
 
 import java.io.File;
 import java.io.FileReader;
@@ -97,9 +98,9 @@ public class SodiumGameOptions {
     }
 
     public enum GraphicsQuality implements TextProvider {
-        DEFAULT("Default"),
-        FANCY("Fancy"),
-        FAST("Fast");
+        DEFAULT("generator.default"),
+        FANCY("options.clouds.fancy"),
+        FAST("options.clouds.fast");
 
         private final String name;
 
@@ -109,7 +110,7 @@ public class SodiumGameOptions {
 
         @Override
         public String getLocalizedName() {
-            return this.name;
+            return I18n.translate(this.name);
         }
 
         public boolean isFancy(GraphicsMode graphicsMode) {
--- src/main/java/me/jellysquid/mods/sodium/client/gui/options/control/ControlValueFormatter.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/options/control/ControlValueFormatter.java
@@ -1,20 +1,22 @@
 package me.jellysquid.mods.sodium.client.gui.options.control;
 
+import net.minecraft.client.resource.language.I18n;
+
 public interface ControlValueFormatter {
     static ControlValueFormatter guiScale() {
-        return (v) -> (v == 0) ? "Auto" : v + "x";
+        return (v) -> (v == 0) ? I18n.translate("options.guiScale.auto") : v + "x";
     }
 
     static ControlValueFormatter fpsLimit() {
-        return (v) -> (v == 260) ? "Unlimited" : v + " FPS";
+        return (v) -> (v == 260) ? I18n.translate("options.framerateLimit.max") : v + " FPS";
     }
 
     static ControlValueFormatter brightness() {
         return (v) -> {
             if (v == 0) {
-                return "Moody";
+                return I18n.translate("options.gamma.min");
             } else if (v == 100) {
-                return "Bright";
+                return I18n.translate("options.gamma.max");
             } else {
                 return v + "%";
             }
