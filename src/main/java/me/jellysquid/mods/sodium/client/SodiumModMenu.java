a25182493ba4bc8271a5da9e7789267042d64ccd
LambdAurora <aurora42lambda@gmail.com>
2020-07-18 20:27:29 +0200

    Add ModMenu integration.

---
+++ src/main/java/me/jellysquid/mods/sodium/client/SodiumModMenu.java
@@ -0,0 +1,16 @@
+package me.jellysquid.mods.sodium.client;
+
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
+import io.github.prospector.modmenu.api.ModMenuApi;
+import me.jellysquid.mods.sodium.client.gui.SodiumOptionsGUI;
+
+/**
+ * Represents the ModMenu integration.
+ */
+public class SodiumModMenu implements ModMenuApi
+{
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return SodiumOptionsGUI::new;
+    }
+}
