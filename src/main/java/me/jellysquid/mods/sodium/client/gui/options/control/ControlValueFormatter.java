25cc4aaca7780eb60bbcc680a673381d4fc19d3e
AMereBagatelle <themerebagatelle@gmail.com>
2020-07-17 20:51:34 -0400

    Adding vanilla translation strings and changing translation string format

--- src/main/java/me/jellysquid/mods/sodium/client/gui/options/control/ControlValueFormatter.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/options/control/ControlValueFormatter.java
@@ -4,19 +4,19 @@ import net.minecraft.client.resource.language.I18n;
 
 public interface ControlValueFormatter {
     static ControlValueFormatter guiScale() {
-        return (v) -> (v == 0) ? I18n.translate("sodium.control_value_formatter.gui_scale_auto") : v + "x";
+        return (v) -> (v == 0) ? I18n.translate("options.guiScale.auto") : v + "x";
     }
 
     static ControlValueFormatter fpsLimit() {
-        return (v) -> (v == 260) ? I18n.translate("sodium.control_value_formatter.fps_limit_unlimited") : v + " FPS";
+        return (v) -> (v == 260) ? I18n.translate("options.framerateLimit.max") : v + " FPS";
     }
 
     static ControlValueFormatter brightness() {
         return (v) -> {
             if (v == 0) {
-                return I18n.translate("sodium.control_value_formatter.moody");
+                return I18n.translate("options.gamma.min");
             } else if (v == 100) {
-                return I18n.translate("sodium.control_value_formatter.bright");
+                return I18n.translate("options.gamma.max");
             } else {
                 return v + "%";
             }
