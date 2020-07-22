74d29d014d91eeabeb72e633da8e1d474f70a462
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2020-07-22 17:57:20 -0500

    new: added new UI scroll pane & redesign layout
    This will come in handy when sodium's settings screen has more options/

--- src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumOptionsGUI.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumOptionsGUI.java
@@ -2,12 +2,10 @@ package me.jellysquid.mods.sodium.client.gui;
 
 import me.jellysquid.mods.sodium.client.gui.options.Option;
 import me.jellysquid.mods.sodium.client.gui.options.OptionFlag;
-import me.jellysquid.mods.sodium.client.gui.options.OptionGroup;
 import me.jellysquid.mods.sodium.client.gui.options.OptionPage;
-import me.jellysquid.mods.sodium.client.gui.options.control.Control;
-import me.jellysquid.mods.sodium.client.gui.options.control.ControlElement;
 import me.jellysquid.mods.sodium.client.gui.options.storage.OptionStorage;
 import me.jellysquid.mods.sodium.client.gui.widgets.FlatButtonWidget;
+import me.jellysquid.mods.sodium.client.gui.widgets.TabControlScrollPaneWidget;
 import me.jellysquid.mods.sodium.client.util.Dim2i;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.Drawable;
@@ -16,10 +14,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.VideoOptionsScreen;
 import net.minecraft.client.resource.language.I18n;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.StringRenderable;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Formatting;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.ArrayList;
@@ -31,16 +26,11 @@ import java.util.stream.Stream;
 public class SodiumOptionsGUI extends Screen {
     private final List<OptionPage> pages = new ArrayList<>();
 
-    private final List<ControlElement<?>> controls = new ArrayList<>();
     private final List<Drawable> drawable = new ArrayList<>();
 
     private final Screen prevScreen;
 
-    private OptionPage currentPage;
-
-    private FlatButtonWidget applyButton, closeButton, undoButton;
-    private boolean hasPendingChanges;
-    private ControlElement<?> hoveredElement;
+    private TabControlScrollPaneWidget settingsTabControlScrollPane;
 
     public SodiumOptionsGUI(Screen prevScreen) {
         super(new TranslatableText("Sodium Options"));
@@ -52,46 +42,32 @@ public class SodiumOptionsGUI extends Screen {
         this.pages.add(SodiumGameOptionPages.advanced());
     }
 
-    public void setPage(OptionPage page) {
-        this.currentPage = page;
-
-        this.rebuildGUI();
-    }
-
     @Override
     protected void init() {
         super.init();
 
-        this.rebuildGUI();
+        this.buildGUI();
     }
 
-    private void rebuildGUI() {
-        this.controls.clear();
+    private void buildGUI() {
         this.children.clear();
         this.drawable.clear();
 
-        if (this.currentPage == null) {
-            if (this.pages.isEmpty()) {
-                throw new IllegalStateException("No pages are available?!");
-            }
-
-            // Just use the first page for now
-            this.currentPage = this.pages.get(0);
-        }
-
-        this.rebuildGUIPages();
-        this.rebuildGUIOptions();
-
-        this.undoButton = new FlatButtonWidget(new Dim2i(this.width - 211, this.height - 30, 65, 20),
+        FlatButtonWidget undoButton = new FlatButtonWidget(new Dim2i(this.width - 211, this.height - 30, 65, 20),
                 I18n.translate("sodium.options.buttons.undo"), this::undoChanges);
-        this.applyButton = new FlatButtonWidget(new Dim2i(this.width - 142, this.height - 30, 65, 20),
+        FlatButtonWidget applyButton = new FlatButtonWidget(new Dim2i(this.width - 142, this.height - 30, 65, 20),
                 I18n.translate("sodium.options.buttons.apply"), this::applyChanges);
-        this.closeButton = new FlatButtonWidget(new Dim2i(this.width - 73, this.height - 30, 65, 20),
+        FlatButtonWidget closeButton = new FlatButtonWidget(new Dim2i(this.width - 73, this.height - 30, 65, 20),
                 I18n.translate("sodium.options.buttons.close"), this::onClose);
 
-        this.children.add(this.undoButton);
-        this.children.add(this.applyButton);
-        this.children.add(this.closeButton);
+        this.settingsTabControlScrollPane = new TabControlScrollPaneWidget(new Dim2i(4, 4, this.width-8,
+                this.height - 8 - 32), pages, this.textRenderer, applyButton, undoButton, closeButton);
+
+
+        this.children.add(this.settingsTabControlScrollPane);
+        this.children.add(undoButton);
+        this.children.add(applyButton);
+        this.children.add(closeButton);
 
         for (Element element : this.children) {
             if (element instanceof Drawable) {
@@ -100,132 +76,17 @@ public class SodiumOptionsGUI extends Screen {
         }
     }
 
-    private void rebuildGUIPages() {
-        int x = 10;
-        int y = 6;
-
-        for (OptionPage page : this.pages) {
-            int width = 10 + this.textRenderer.getWidth(page.getName());
-
-            FlatButtonWidget button = new FlatButtonWidget(new Dim2i(x, y, width, 16), page.getName(), () -> this.setPage(page));
-            button.setSelected(this.currentPage == page);
-
-            x += width + 6;
-
-            this.children.add(button);
-        }
-    }
-
-    private void rebuildGUIOptions() {
-        int x = 10;
-        int y = 28;
-
-        for (OptionGroup group : this.currentPage.getGroups()) {
-            // Add each option's control element
-            for (Option<?> option : group.getOptions()) {
-                Control<?> control = option.getControl();
-                ControlElement<?> element = control.createElement(new Dim2i(x, y, 200, 18));
-
-                this.controls.add(element);
-                this.children.add(element);
-
-                // Move down to the next option
-                y += 18;
-            }
-
-            // Add padding beneath each option group
-            y += 4;
-        }
-    }
-
     @Override
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float delta) {
         super.renderBackground(matrixStack);
-
-        this.updateControls();
-
         for (Drawable drawable : this.drawable) {
             drawable.render(matrixStack, mouseX, mouseY, delta);
         }
-
-        if (this.hoveredElement != null) {
-            this.renderOptionTooltip(matrixStack, this.hoveredElement);
-        }
-    }
-
-    private void updateControls() {
-        ControlElement<?> hovered = this.getActiveControls()
-                .filter(ControlElement::isHovered)
-                .findFirst()
-                .orElse(null);
-
-        boolean hasChanges = this.getAllOptions()
-                .anyMatch(Option::hasChanged);
-
-        for (OptionPage page : this.pages) {
-            for (Option<?> option : page.getOptions()) {
-                if (option.hasChanged()) {
-                    hasChanges = true;
-                }
-            }
-        }
-
-        this.applyButton.setEnabled(hasChanges);
-        this.undoButton.setVisible(hasChanges);
-        this.closeButton.setEnabled(!hasChanges);
-
-        this.hasPendingChanges = hasChanges;
-        this.hoveredElement = hovered;
     }
 
     private Stream<Option<?>> getAllOptions() {
         return this.pages.stream()
                 .flatMap(s -> s.getOptions().stream());
-    }
-
-    private Stream<ControlElement<?>> getActiveControls() {
-        return this.controls.stream();
-    }
-
-    private void renderOptionTooltip(MatrixStack matrixStack, ControlElement<?> element) {
-        Dim2i dim = element.getDimensions();
-
-        int textPadding = 3;
-        int boxPadding = 3;
-
-        int boxWidth = 200;
-        int textWidth = boxWidth - (textPadding * 2);
-
-        int boxY = dim.getOriginY();
-        int boxX = dim.getLimitX() + boxPadding;
-
-        Option<?> option = element.getOption();
-
-        StringRenderable title = new LiteralText(option.getName()).formatted(Formatting.GRAY);
-
-        List<StringRenderable> text = this.textRenderer.wrapLines(title, textWidth);
-        text.addAll(this.textRenderer.wrapLines(option.getTooltip(), textWidth));
-
-        int boxHeight = (text.size() * 12) + boxPadding;
-        int boxYLimit = boxY + boxHeight;
-        int boxYCutoff = this.height - 40;
-
-        // If the box is going to be cutoff on the Y-axis, move it back up the difference
-        if (boxYLimit > boxYCutoff) {
-            boxY -= boxYLimit - boxYCutoff;
-        }
-
-        this.fillGradient(matrixStack, boxX, boxY, boxX + boxWidth, boxY + boxHeight, 0xE0000000, 0xE0000000);
-
-        for (int i = 0; i < text.size(); i++) {
-            StringRenderable str = text.get(i);
-
-            if (str.getString().isEmpty()) {
-                continue;
-            }
-
-            this.textRenderer.draw(matrixStack, str, boxX + textPadding, boxY + textPadding + (i * 12), 0xFFFFFFFF);
-        }
     }
 
     private void applyChanges() {
@@ -265,6 +126,16 @@ public class SodiumOptionsGUI extends Screen {
     }
 
     @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double amount){
+        for (Element element : this.children) {
+            if(element.mouseScrolled(mouseX, mouseY, amount)){
+                return true;
+            }
+        }
+        return super.mouseScrolled(mouseX, mouseY, amount);
+    }
+
+    @Override
     public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
         if (keyCode == GLFW.GLFW_KEY_P && (modifiers & GLFW.GLFW_MOD_SHIFT) != 0) {
             MinecraftClient.getInstance().openScreen(new VideoOptionsScreen(this.prevScreen, MinecraftClient.getInstance().options));
@@ -277,7 +148,7 @@ public class SodiumOptionsGUI extends Screen {
 
     @Override
     public boolean shouldCloseOnEsc() {
-        return !this.hasPendingChanges;
+        return !this.settingsTabControlScrollPane.hasPendingChanges();
     }
 
     @Override
