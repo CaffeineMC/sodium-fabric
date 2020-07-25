dcd723748537358c28bb69f808def14bc640d84e
qouteall <qouteall@163.com>
2020-07-25 16:21:00 +0800

    Revert "Add detection for IP"

--- src/main/java/me/jellysquid/mods/sodium/client/SodiumClientMod.java
+++ src/main/java/me/jellysquid/mods/sodium/client/SodiumClientMod.java
@@ -3,7 +3,6 @@ package me.jellysquid.mods.sodium.client;
 import me.jellysquid.mods.sodium.client.gui.SodiumGameOptions;
 import me.jellysquid.mods.sodium.client.util.UnsafeUtil;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,14 +12,9 @@ public class SodiumClientMod implements ClientModInitializer {
     private static SodiumGameOptions CONFIG;
     private static Logger LOGGER;
 
-    private static boolean immersivePortalsPresent = false;
-
     @Override
     public void onInitializeClient() {
-        immersivePortalsPresent = FabricLoader.getInstance().isModLoaded("immersive_portals");
-        if (immersivePortalsPresent) {
-            logger().info("Detected Immersive Portals Mod");
-        }
+
     }
 
     public static SodiumGameOptions options() {
@@ -48,9 +42,5 @@ public class SodiumClientMod implements ClientModInitializer {
 
     public static void onConfigChanged(SodiumGameOptions options) {
         UnsafeUtil.setEnabled(options.advanced.useMemoryIntrinsics);
-    }
-
-    public static boolean isIPPresent() {
-        return immersivePortalsPresent;
     }
 }
