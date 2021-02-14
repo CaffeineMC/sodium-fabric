136e2fd6c56a0c88555a72c2b0702c37692f99d9
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2021-02-14 03:00:46 -0500

    change: Track sprite atlas textures for updating

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/texture_tracking/MixinSpriteAtlasTexture.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/texture_tracking/MixinSpriteAtlasTexture.java
@@ -2,14 +2,14 @@ package me.jellysquid.mods.sodium.mixin.features.texture_tracking;
 
 import me.jellysquid.mods.sodium.client.render.texture.SpriteUtil;
 import net.minecraft.client.texture.Sprite;
-import net.minecraft.client.texture.SpriteAtlasHolder;
+import net.minecraft.client.texture.SpriteAtlasTexture;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(SpriteAtlasHolder.class)
-public class MixinSpriteAtlasHolder {
+@Mixin(SpriteAtlasTexture.class)
+public class MixinSpriteAtlasTexture {
     @Inject(method = "getSprite", at = @At("RETURN"))
     private void preReturnPaintingSprite(CallbackInfoReturnable<Sprite> cir) {
         Sprite sprite = cir.getReturnValue();
