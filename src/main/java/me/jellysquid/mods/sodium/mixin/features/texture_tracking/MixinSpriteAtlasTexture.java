e81935b41b71c652cb99b3bb689eb7287787f0b8
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2021-02-25 16:46:20 -0600

    fix: Hook directly into SpriteAtlasTexture for visiblity tracking (#532)
    Covers a few additional cases such as animated paintings and effect icons. Fixes #248 and #403.

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/texture_tracking/MixinSpriteAtlasTexture.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/texture_tracking/MixinSpriteAtlasTexture.java
@@ -2,14 +2,14 @@ package me.jellysquid.mods.sodium.mixin.features.texture_tracking;
 
 import me.jellysquid.mods.sodium.client.render.texture.SpriteUtil;
 import net.minecraft.client.texture.Sprite;
-import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.client.texture.SpriteAtlasTexture;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(SpriteIdentifier.class)
-public class MixinSpriteIdentifier {
+@Mixin(SpriteAtlasTexture.class)
+public class MixinSpriteAtlasTexture {
     @Inject(method = "getSprite", at = @At("RETURN"))
     private void preReturnSprite(CallbackInfoReturnable<Sprite> cir) {
         Sprite sprite = cir.getReturnValue();
