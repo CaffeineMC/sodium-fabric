3316fa66a62ad6c5b8035cee9f908166befb8b37
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-03-08 10:26:09 +0200

    oops, remove unused imports

--- src/main/java/me/jellysquid/mods/sodium/common/config/SodiumConfig.java
+++ src/main/java/me/jellysquid/mods/sodium/common/config/SodiumConfig.java
@@ -12,8 +12,6 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
