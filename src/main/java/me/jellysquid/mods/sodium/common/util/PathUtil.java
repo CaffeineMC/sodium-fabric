220c417fca9ae23a142661874879a31ee300cab5
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-03-11 12:32:22 +0200

    oops: Remove colons from timestamped file name

--- src/main/java/me/jellysquid/mods/sodium/common/util/PathUtil.java
+++ src/main/java/me/jellysquid/mods/sodium/common/util/PathUtil.java
@@ -3,8 +3,29 @@ package me.jellysquid.mods.sodium.common.util;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Locale;
+
+import static java.time.temporal.ChronoField.*;
 
 public class PathUtil {
+    /**
+     * A variant of {@link DateTimeFormatter#ISO_LOCAL_DATE_TIME} that is safe for file names (IE doesn't contain colons).
+     */
+    private static final DateTimeFormatter ISO_LOCAL_DATE_TIME_SAFE = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive().parseStrict()
+            .append(DateTimeFormatter.ISO_LOCAL_DATE)
+            .appendLiteral('T')
+            .appendValue(HOUR_OF_DAY, 2)
+            .appendLiteral('-')
+            .appendValue(MINUTE_OF_HOUR, 2)
+            .optionalStart()
+            .appendLiteral('-')
+            .appendValue(SECOND_OF_MINUTE, 2)
+            .optionalStart()
+            .appendFraction(NANO_OF_SECOND, 0, 9, true)
+            .toFormatter(Locale.ROOT);
+
     /**
      * Resolves a timestamped sibling of the base path - {@code "{base_dir}/{base_name}_{tag}_{timestamp}.{base_ext}"}.
      * @param base Base path
@@ -15,7 +36,7 @@ public class PathUtil {
         LocalDateTime now = LocalDateTime.now();
         String backupName = base.getFileName().toString();
         backupName = backupName.substring(0, backupName.lastIndexOf('.') - 1);
-        backupName += "_" + tag + "_" + now.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + ".json";
+        backupName += "_" + tag + "_" + now.format(ISO_LOCAL_DATE_TIME_SAFE) + ".json";
         return base.resolveSibling(backupName);
     }
 }
