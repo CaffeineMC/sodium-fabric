73024f627e7b55727aac56164e3129261b95381e
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-03-12 12:47:10 +0200

    change: Remove PathUtil.resolveTimestampedSibling's file extension handling
    Change timestamp format to be more compact

--- src/main/java/me/jellysquid/mods/sodium/common/util/PathUtil.java
+++ src/main/java/me/jellysquid/mods/sodium/common/util/PathUtil.java
@@ -10,33 +10,34 @@ import static java.time.temporal.ChronoField.*;
 
 public class PathUtil {
     /**
-     * A variant of {@link DateTimeFormatter#ISO_LOCAL_DATE_TIME} that is safe for file names (IE doesn't contain colons).
+     * A variant of {@link DateTimeFormatter#ISO_LOCAL_DATE_TIME} that is:
+     * - Safe for file names (IE doesn't contain colons).
+     * - More compact (uses {@link DateTimeFormatter#BASIC_ISO_DATE}'s format for date component and a similar format for time component).
      */
     private static final DateTimeFormatter ISO_LOCAL_DATE_TIME_SAFE = new DateTimeFormatterBuilder()
             .parseCaseInsensitive().parseStrict()
-            .append(DateTimeFormatter.ISO_LOCAL_DATE)
+            .appendValue(YEAR, 4)
+            .appendValue(MONTH_OF_YEAR, 2)
+            .appendValue(DAY_OF_MONTH, 2)
             .appendLiteral('T')
             .appendValue(HOUR_OF_DAY, 2)
-            .appendLiteral('-')
             .appendValue(MINUTE_OF_HOUR, 2)
             .optionalStart()
-            .appendLiteral('-')
             .appendValue(SECOND_OF_MINUTE, 2)
             .optionalStart()
-            .appendFraction(NANO_OF_SECOND, 0, 9, true)
+            .appendLiteral('N')
+            .appendFraction(NANO_OF_SECOND, 0, 9, false)
             .toFormatter(Locale.ROOT);
 
     /**
-     * Resolves a timestamped sibling of the base path - {@code "{base_dir}/{base_name}_{tag}_{timestamp}.{base_ext}"}.
+     * Resolves a timestamped sibling of the base path - {@code "{base}-{tag}-{timestamp}"}.
      * @param base Base path
      * @param tag Tag to add to sibling's name
      * @return Sibling path
      */
     public static Path resolveTimestampedSibling(Path base, String tag) {
-        LocalDateTime now = LocalDateTime.now();
-        String backupName = base.getFileName().toString();
-        backupName = backupName.substring(0, backupName.lastIndexOf('.') - 1);
-        backupName += "_" + tag + "_" + now.format(ISO_LOCAL_DATE_TIME_SAFE) + ".json";
-        return base.resolveSibling(backupName);
+        return base.resolveSibling(base.getFileName().toString()
+                + "-" + tag
+                + "-" + LocalDateTime.now().format(ISO_LOCAL_DATE_TIME_SAFE));
     }
 }
