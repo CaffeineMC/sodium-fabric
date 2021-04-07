0d6af7ef8ebd6863fcd3f2e8d257eaf1c02f543d
Bytzo <bytzo@bytzo.net>
2021-04-07 14:34:14 -0700

    fix: Prevent hidden button widgets from being pressed Previously, button widgets that were not visible could be clicked on, since only the enabled state of the button was considered if action should be taken or not. To fix this issue, button widgets will also consider their own visibility state when taking action after a click.

--- src/main/java/me/jellysquid/mods/sodium/client/gui/widgets/FlatButtonWidget.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/widgets/FlatButtonWidget.java
@@ -46,7 +46,7 @@ public class FlatButtonWidget extends AbstractWidget implements Drawable {
 
     @Override
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        if (!this.enabled) {
+        if (!this.enabled || !this.visible) {
             return false;
         }
 
