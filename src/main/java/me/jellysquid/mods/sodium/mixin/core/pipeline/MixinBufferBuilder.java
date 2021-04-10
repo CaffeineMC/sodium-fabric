7dafc0b6fa974c208e5fa3806915b7d9f2b8488b
comp500 <comp500@users.noreply.github.com>
2021-04-10 03:27:23 +0100

    fix: Allocate an extra vertex in ensureBufferCapacity to match vanilla (#614)
    * fix: Allocate an extra vertex in ensureCapacity to match vanilla
    
    Vanilla rendering code always ensures that there is space for 1 vertex left in
    vertex buffers, in BufferBuilder.next(), but Sodium VertexSinks do this growth
    check before appending vertices, without leaving space that may be necessary in
    code that uses VertexConsumers. This causes issues in places where buffers
    appended to by a Sodium VertexSink is later used by VertexConsumer code,
    especially with Sodium's intrinsics mixins (e.g. MixinGlyphRenderer).
    
    * Add blank line to match style of surrounding code
    
    Co-authored-by: coderbot <coderbot16@gmail.com>

--- src/main/java/me/jellysquid/mods/sodium/mixin/core/pipeline/MixinBufferBuilder.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/core/pipeline/MixinBufferBuilder.java
@@ -43,6 +43,9 @@ public abstract class MixinBufferBuilder implements VertexBufferView, VertexDrai
 
     @Override
     public boolean ensureBufferCapacity(int bytes) {
+        // Ensure that there is always space for 1 more vertex; see BufferBuilder.next()
+        bytes += format.getVertexSize();
+
         if (this.elementOffset + bytes <= this.buffer.capacity()) {
             return false;
         }
