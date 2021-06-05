972c7c141763c84c25981827b186c113d3f6df43
jellysquid3 <jellysquid3@users.noreply.github.com>
2021-06-05 14:42:01 -0500

    new: Add version information to F3, closes #598

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/debug/MixinDebugHud.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/debug/MixinDebugHud.java
@@ -1,14 +1,18 @@
 package me.jellysquid.mods.sodium.mixin.features.debug;
 
+import com.google.common.collect.Lists;
 import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
 import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderBackend;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.hud.DebugHud;
 import net.minecraft.util.Formatting;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.lang.management.ManagementFactory;
@@ -22,25 +26,45 @@ public abstract class MixinDebugHud {
         throw new UnsupportedOperationException();
     }
 
-    @Inject(method = "getRightText", at = @At("RETURN"))
-    private void appendRightText(CallbackInfoReturnable<List<String>> cir) {
-        List<String> strings = cir.getReturnValue();
-
-        for (int i = 0; i < strings.size(); i++) {
-            String str = strings.get(i);
-
-            if (str.startsWith("Allocated:")) {
-                strings.add(i + 1, getNativeMemoryString());
-                break;
-            }
-        }
-
+    @Redirect(method = "getRightText", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayList([Ljava/lang/Object;)Ljava/util/ArrayList;"))
+    private ArrayList<String> redirectRightTextEarly(Object[] elements) {
+        ArrayList<String> strings = Lists.newArrayList((String[]) elements);
+        strings.add("");
+        strings.add("Sodium Renderer");
+        strings.add(Formatting.UNDERLINE + getFormattedVersionText());
         strings.add("");
         strings.addAll(getChunkRendererDebugStrings());
 
         if (SodiumClientMod.options().advanced.disableDriverBlacklist) {
             strings.add(Formatting.RED + "(!!) Driver blacklist ignored");
         }
+
+        for (int i = 0; i < strings.size(); i++) {
+            String str = strings.get(i);
+
+            if (str.startsWith("Allocated:")) {
+                strings.add(i + 1, getNativeMemoryString());
+
+                break;
+            }
+        }
+
+        return strings;
+    }
+
+    private static String getFormattedVersionText() {
+        String version = SodiumClientMod.getVersion();
+        Formatting color;
+
+        if (version.endsWith("-dirty")) {
+            color = Formatting.RED;
+        } else if (version.contains("+rev.")) {
+            color = Formatting.LIGHT_PURPLE;
+        } else {
+            color = Formatting.GREEN;
+        }
+
+        return color + version;
     }
 
     private static List<String> getChunkRendererDebugStrings() {
