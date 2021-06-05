9efd665e25a1cffd53171eece97344fd2b523c9f
Bytzo <bytzo@bytzo.net>
2021-06-05 15:24:24 -0700

    fix: Respect useAdvancedEntityCulling option

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/entity/cull/MixinWorldRenderer.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/entity/cull/MixinWorldRenderer.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -37,7 +38,8 @@ public class MixinWorldRenderer {
     private void stopRenderEntity(Entity entity, double cameraX, double cameraY, double cameraZ, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo callbackInfo, double d, double e, double f) {
         // If the entity about to be rendered has a nametag, has no outline, and
         // is also not visible, just render the entity nametag and nothing else.
-        if (entityRenderDispatcher.getRenderer(entity).hasLabel(entity) && !this.client.hasOutline(entity) && !SodiumWorldRenderer.getInstance().isEntityVisible(entity)) {
+        if (SodiumClientMod.options().advanced.useAdvancedEntityCulling && entityRenderDispatcher.getRenderer(entity).hasLabel(entity) &&
+                !this.client.hasOutline(entity) && !SodiumWorldRenderer.getInstance().isEntityVisible(entity)) {
             matrices.push();
             matrices.translate(d - cameraX, e - cameraY, f - cameraZ);
             entityRenderDispatcher.getRenderer(entity).renderLabelIfPresent(entity, entity.getDisplayName(), matrices, vertexConsumers, entityRenderDispatcher.getLight(entity, tickDelta));;
