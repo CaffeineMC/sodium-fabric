a08b00ebf4a572f3868f4c63aac6956da613686c
jellysquid3 <jellysquid3@users.noreply.github.com>
2021-06-08 17:21:16 -0500

    change: Remove ARB fallbacks for functions before OpenGL 3.2

--- src/main/java/me/jellysquid/mods/sodium/client/gl/func/GlFunctions.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gl/func/GlFunctions.java
@@ -6,18 +6,8 @@ import org.lwjgl.opengl.GLCapabilities;
 public class GlFunctions {
     private static final GLCapabilities capabilities = GL.getCapabilities();
 
-    public static final GlVertexArrayFunctions VERTEX_ARRAY = GlVertexArrayFunctions.load(capabilities);
-    public static final GlBufferCopyFunctions BUFFER_COPY = GlBufferCopyFunctions.load(capabilities);
     public static final GlIndirectMultiDrawFunctions INDIRECT_DRAW = GlIndirectMultiDrawFunctions.load(capabilities);
     public static final GlInstancedArrayFunctions INSTANCED_ARRAY = GlInstancedArrayFunctions.load(capabilities);
-
-    public static boolean isVertexArraySupported() {
-        return VERTEX_ARRAY != GlVertexArrayFunctions.UNSUPPORTED;
-    }
-
-    public static boolean isBufferCopySupported() {
-        return BUFFER_COPY != GlBufferCopyFunctions.UNSUPPORTED;
-    }
 
     public static boolean isIndirectMultiDrawSupported() {
         return INDIRECT_DRAW != GlIndirectMultiDrawFunctions.UNSUPPORTED;
