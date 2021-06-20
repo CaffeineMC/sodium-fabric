6e334af59cb95fb6f4e18d716e7f18401e3f759b
jellysquid3 <jellysquid3@users.noreply.github.com>
2021-06-20 18:26:53 -0500

    new: Early shader import system

--- src/main/resources/assets/sodium/shaders/blocks/block_layer_solid.v.glsl
+++ src/main/resources/assets/sodium/shaders/blocks/block_layer_solid.v.glsl
@@ -1,24 +1,6 @@
 #version 150 core
 
-in vec3 a_Pos; // The position of the vertex
-in vec4 a_Color; // The color of the vertex
-in vec2 a_TexCoord; // The block texture coordinate of the vertex
-in vec2 a_LightCoord; // The light map texture coordinate of the vertex
-
-out vec4 v_Color;
-out vec2 v_TexCoord;
-out vec2 v_LightCoord;
-
-#ifdef USE_FOG
-out float v_FragDistance;
-#endif
-
-uniform mat4 u_ModelViewProjectionMatrix;
-uniform vec3 u_ModelScale;
-uniform vec2 u_TextureScale;
-
-// The model translation for this draw call.
-in vec4 d_ModelOffset;
+#import <sodium:blocks/base.v.glsl>
 
 void main() {
     // Translates the vertex position around the position of the camera
