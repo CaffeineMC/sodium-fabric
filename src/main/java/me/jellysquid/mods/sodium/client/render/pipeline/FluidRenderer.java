8f3ddb6614cab42169531be48014b9c1cb6c7fbb
Lorenzo Rai <lorenzo.rai@codingrays.dev>
2021-06-29 18:42:43 +0200

    Fixed occlusion test behaving incorrectly for non full square shapes.

--- src/main/java/me/jellysquid/mods/sodium/client/render/pipeline/FluidRenderer.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/pipeline/FluidRenderer.java
@@ -77,7 +77,7 @@ public class FluidRenderer {
             BlockPos pos = this.scratchPos.set(x, y, z);
             BlockState blockState = world.getBlockState(pos);
             VoxelShape shape = blockState.getCullingShape(world, pos);
-            if (VoxelShapes.isSideCovered(shape, VoxelShapes.fullCube(), dir)) {
+            if (VoxelShapes.isSideCovered(VoxelShapes.fullCube(), shape, dir.getOpposite())) {
                 return false; // Fluid is in waterlogged block that self occludes
             }
         }
