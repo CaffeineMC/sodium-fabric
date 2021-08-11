10f1ce9b9e20992ff1c836b482e7aa789a753fbd
Michael Peacock <you@example.com>
2021-08-11 06:37:17 +0100

    used correct base to start from

--- src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptions.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptions.java
@@ -35,6 +35,7 @@ public class SodiumGameOptions {
     public static class QualitySettings {
         public GraphicsQuality cloudQuality = GraphicsQuality.DEFAULT;
         public GraphicsQuality weatherQuality = GraphicsQuality.DEFAULT;
+        public GraphicsQuality leavesQuality = GraphicsQuality.DEFAULT;
 
         public boolean enableVignette = true;
         public boolean enableClouds = true;
--- src/main/java/me/jellysquid/mods/sodium/mixin/features/render_layer/MixinRenderLayers.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/render_layer/MixinRenderLayers.java
@@ -1,7 +1,11 @@
 package me.jellysquid.mods.sodium.mixin.features.render_layer;
 
 import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
+import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderLayers;
 import net.minecraft.fluid.Fluid;
@@ -9,6 +13,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 
@@ -28,5 +35,12 @@ public class MixinRenderLayers {
         // Replace the backing collection types with something a bit faster, since this is a hot spot in chunk rendering.
         BLOCKS = new Reference2ReferenceOpenHashMap<>(BLOCKS);
         FLUIDS = new Reference2ReferenceOpenHashMap<>(FLUIDS);
+    }
+    @Inject(method = "getBlockLayer(Lnet/minecraft/block/BlockState;)Lnet/minecraft/client/render/RenderLayer;", at = @At(value = "RETURN"), cancellable = true)
+    private static void redirectGetFancyLeaves(BlockState state, CallbackInfoReturnable<RenderLayer> cir) {
+        if (state.getBlock() instanceof LeavesBlock) {
+            boolean fancyLeaves = SodiumClientMod.options().quality.leavesQuality.isFancy(MinecraftClient.getInstance().options.graphicsMode);
+            cir.setReturnValue(fancyLeaves ? RenderLayer.getCutoutMipped() : RenderLayer.getSolid());
+        }
     }
 }
