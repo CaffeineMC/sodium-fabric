0b559e364742fa0aa81adf6f874b8962307543b9
Michael Peacock <you@example.com>
2021-08-30 03:08:06 +0100

    added extra file handling

--- src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptions.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptions.java
@@ -3,19 +3,20 @@ package me.jellysquid.mods.sodium.client.gui;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
 import me.jellysquid.mods.sodium.client.gui.options.TextProvider;
 import net.minecraft.client.option.GraphicsMode;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Modifier;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.StandardOpenOption;
 
 public class SodiumGameOptions {
     public final QualitySettings quality = new QualitySettings();
@@ -99,7 +100,7 @@ public class SodiumGameOptions {
         SodiumGameOptions config;
 
         if (Files.exists(path)) {
-            try (FileReader reader = new FileReader(path.toFile())) {
+            try (JsonReader reader = new JsonReader(new FileReader(path.toFile()))) {
                 config = GSON.fromJson(reader, SodiumGameOptions.class);
             } catch (IOException e) {
                 throw new RuntimeException("Could not parse config", e);
@@ -125,12 +126,14 @@ public class SodiumGameOptions {
 
     public void writeChanges() throws IOException {
         Path dir = this.configPath.getParent();
+        File file = new File(String.valueOf(this.configPath.resolveSibling(this.configPath.getFileName())));
 
         if (!Files.exists(dir)) {
             Files.createDirectories(dir);
         } else if (!Files.isDirectory(dir)) {
             throw new IOException("Not a directory: " + dir);
         }
+        if (!file.canWrite()){throw new IOException("Sodium Config is read-only.");}
 
         // Use a temporary location next to the config's final destination
         Path tempPath = this.configPath.resolveSibling(this.configPath.getFileName() + ".tmp");
