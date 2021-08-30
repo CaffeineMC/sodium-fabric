095d2537b669734a6183e0b2936e1fd226424149
jellysquid3 <jellysquid3@users.noreply.github.com>
2021-08-30 14:36:23 -0500

    new: Handle config corruption with user prompt

--- src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptions.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptions.java
@@ -4,6 +4,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import me.jellysquid.mods.sodium.client.gui.options.TextProvider;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.option.GraphicsMode;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
@@ -15,14 +16,25 @@ import java.lang.reflect.Modifier;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.StandardOpenOption;
 
 public class SodiumGameOptions {
+    private static final String DEFAULT_FILE_NAME = "sodium-options.json";
+
     public final QualitySettings quality = new QualitySettings();
     public final AdvancedSettings advanced = new AdvancedSettings();
     public final NotificationSettings notifications = new NotificationSettings();
 
+    private boolean readOnly;
+
     private Path configPath;
+
+    public static SodiumGameOptions defaults() {
+        var options = new SodiumGameOptions();
+        options.configPath = getConfigPath(DEFAULT_FILE_NAME);
+        options.sanitize();
+
+        return options;
+    }
 
     public static class AdvancedSettings {
         public ArenaMemoryAllocator arenaMemoryAllocator = null;
@@ -95,7 +107,12 @@ public class SodiumGameOptions {
             .excludeFieldsWithModifiers(Modifier.PRIVATE)
             .create();
 
-    public static SodiumGameOptions load(Path path) {
+    public static SodiumGameOptions load() {
+        return load(DEFAULT_FILE_NAME);
+    }
+
+    public static SodiumGameOptions load(String name) {
+        Path path = getConfigPath(name);
         SodiumGameOptions config;
 
         if (Files.exists(path)) {
@@ -109,10 +126,7 @@ public class SodiumGameOptions {
         }
 
         config.configPath = path;
-
-        if (config.advanced.arenaMemoryAllocator == null) {
-            config.advanced.arenaMemoryAllocator = ArenaMemoryAllocator.ASYNC;
-        }
+        config.sanitize();
 
         try {
             config.writeChanges();
@@ -123,7 +137,23 @@ public class SodiumGameOptions {
         return config;
     }
 
+    private void sanitize() {
+        if (this.advanced.arenaMemoryAllocator == null) {
+            this.advanced.arenaMemoryAllocator = ArenaMemoryAllocator.ASYNC;
+        }
+    }
+
+    private static Path getConfigPath(String name) {
+        return FabricLoader.getInstance()
+                .getConfigDir()
+                .resolve(name);
+    }
+
     public void writeChanges() throws IOException {
+        if (this.isReadOnly()) {
+            throw new IllegalStateException("Config file is read-only");
+        }
+
         Path dir = this.configPath.getParent();
 
         if (!Files.exists(dir)) {
@@ -140,5 +170,17 @@ public class SodiumGameOptions {
 
         // Atomically replace the old config file (if it exists) with the temporary file
         Files.move(tempPath, this.configPath, StandardCopyOption.ATOMIC_MOVE, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    public boolean isReadOnly() {
+        return this.readOnly;
+    }
+
+    public void setReadOnly() {
+        this.readOnly = true;
+    }
+
+    public String getFileName() {
+        return this.configPath.getFileName().toString();
     }
 }
