624b9ee30f3a22b35a270ac1587fd775929f6ad6
jellysquid3 <jellysquid3@users.noreply.github.com>
2021-08-30 01:04:54 -0500

    new: Avoid double lookup in font storage access

---
+++ src/main/java/me/jellysquid/mods/sodium/client/util/font/FontManagerExtended.java
@@ -0,0 +1,5 @@
+package me.jellysquid.mods.sodium.client.util.font;
+
+public interface FontManagerExtended {
+    void rebuild();
+}
---
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/gui/font/MixinFontManager.java
@@ -0,0 +1,71 @@
+package me.jellysquid.mods.sodium.mixin.features.gui.font;
+
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import me.jellysquid.mods.sodium.client.util.font.FontManagerExtended;
+import net.minecraft.client.font.Font;
+import net.minecraft.client.font.FontManager;
+import net.minecraft.client.font.FontStorage;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.profiler.Profiler;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.Map;
+
+@Mixin(FontManager.class)
+public class MixinFontManager implements FontManagerExtended {
+    @Shadow
+    @Final
+    private Map<Identifier, FontStorage> fontStorages;
+
+    @Shadow
+    private Map<Identifier, Identifier> idOverrides;
+
+    @Shadow
+    @Final
+    private FontStorage missingStorage;
+
+    private Map<Identifier, FontStorage> fontStorageOverrides = new Object2ObjectOpenHashMap<>();
+
+    /**
+     * @author JellySquid
+     * @reason Avoid double lookup
+     */
+    @Overwrite
+    public TextRenderer createTextRenderer() {
+        return new TextRenderer(this::getFontStorageOverride);
+    }
+
+    @Override
+    public void rebuild() {
+        this.fontStorageOverrides = new Object2ObjectOpenHashMap<>(this.fontStorages);
+
+        for (Map.Entry<Identifier, Identifier> entry : this.idOverrides.entrySet()) {
+            this.fontStorageOverrides.put(entry.getKey(), this.getFontStorageOverride(entry.getValue()));
+        }
+    }
+
+    private FontStorage getFontStorageOverride(Identifier id) {
+        return this.fontStorageOverrides.getOrDefault(id, this.missingStorage);
+    }
+
+    @Mixin(targets = "net/minecraft/client/font/FontManager$1")
+    private static class MixinReloadListener {
+        @Shadow(aliases = "field_18216", remap = false)
+        @Final
+        private FontManager fontManager;
+
+        @Inject(method = "apply", at = @At("RETURN"))
+        private void postApply(Map<Identifier, List<Font>> map, ResourceManager resourceManager, Profiler profiler, CallbackInfo ci) {
+            ((FontManagerExtended) this.fontManager).rebuild();
+        }
+    }
+}
