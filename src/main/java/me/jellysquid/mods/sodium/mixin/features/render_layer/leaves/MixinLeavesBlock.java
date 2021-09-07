f22618b56a23f1fa4a15ea5db85aebc939220dee
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-09-07 10:57:32 +0300

    MixinLeavesBlock: simplify condition

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/render_layer/leaves/MixinLeavesBlock.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/render_layer/leaves/MixinLeavesBlock.java
@@ -1,14 +1,11 @@
 package me.jellysquid.mods.sodium.mixin.features.render_layer.leaves;
 
 import me.jellysquid.mods.sodium.client.SodiumClientMod;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LeavesBlock;
 import net.minecraft.block.Material;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.GraphicsMode;
 import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
 
@@ -21,9 +18,10 @@ public class MixinLeavesBlock extends Block {
 
     @Override
     public boolean isSideInvisible(BlockState state, BlockState stateFrom, Direction direction) {
-        if (!(SodiumClientMod.options().quality.leavesQuality.isFancy(MinecraftClient.getInstance().options.graphicsMode))) {
+        if (SodiumClientMod.options().quality.leavesQuality.isFancy(MinecraftClient.getInstance().options.graphicsMode)) {
+            return super.isSideInvisible(state, stateFrom, direction);
+        } else {
             return stateFrom.getBlock() instanceof LeavesBlock || super.isSideInvisible(state, stateFrom, direction);
         }
-        return super.isSideInvisible(state, stateFrom, direction);
     }
 }
