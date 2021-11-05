b403bdd5f6c0c80f27261637835452c828b7e358
altrisi <altrisi.trillosierra@gmail.com>
2021-11-05 20:05:14 +0100

    Migrate to CaffeineConfig

--- src/main/java/me/jellysquid/mods/sodium/mixin/SodiumMixinPlugin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/SodiumMixinPlugin.java
@@ -1,96 +1,50 @@
 package me.jellysquid.mods.sodium.mixin;
 
-import me.jellysquid.mods.sodium.common.config.Option;
-import me.jellysquid.mods.sodium.common.config.SodiumConfig;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.objectweb.asm.tree.ClassNode;
-import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
-import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import net.caffeinemc.caffeineconfig.AbstractCaffeineConfigMixinPlugin;
+import net.caffeinemc.caffeineconfig.CaffeineConfig;
+import net.fabricmc.loader.api.FabricLoader;
 
-import java.io.File;
-import java.util.List;
-import java.util.Set;
-
-@SuppressWarnings("unused")
-public class SodiumMixinPlugin implements IMixinConfigPlugin {
+public class SodiumMixinPlugin extends AbstractCaffeineConfigMixinPlugin {
     private static final String MIXIN_PACKAGE_ROOT = "me.jellysquid.mods.sodium.mixin.";
 
-    private final Logger logger = LogManager.getLogger("Sodium");
-    private SodiumConfig config;
-
     @Override
-    public void onLoad(String mixinPackage) {
-        try {
-            this.config = SodiumConfig.load(new File("./config/sodium-mixins.properties"));
-        } catch (Exception e) {
-            throw new RuntimeException("Could not load configuration file for Sodium", e);
-        }
+    protected CaffeineConfig createConfig() {
+        return CaffeineConfig.builder("Sodium")
+                .addMixinOption("core", true) // TODO: Don't actually allow the user to disable 
 
-        this.logger.info("Loaded configuration file for Sodium: {} options available, {} override(s) found",
-                this.config.getOptionCount(), this.config.getOptionOverrideCount());
+                .addMixinOption("features.block", true)
+                .addMixinOption("features.buffer_builder", true)
+                .addMixinOption("features.buffer_builder.fast_advance", true)
+                .addMixinOption("features.buffer_builder.fast_sort", true)
+                .addMixinOption("features.buffer_builder.intrinsics", true)
+                .addMixinOption("features.chunk_rendering", true)
+                .addMixinOption("features.debug", true)
+                .addMixinOption("features.entity", true)
+                .addMixinOption("features.entity.fast_render", true)
+                .addMixinOption("features.entity.smooth_lighting", true)
+                .addMixinOption("features.gui", true)
+                .addMixinOption("features.gui.fast_loading_screen", true)
+                .addMixinOption("features.gui.font", true)
+                .addMixinOption("features.item", true)
+                .addMixinOption("features.matrix_stack", true)
+                .addMixinOption("features.model", true)
+                .addMixinOption("features.options", true)
+                .addMixinOption("features.particle", true)
+                .addMixinOption("features.particle.cull", true)
+                .addMixinOption("features.particle.fast_render", true)
+                .addMixinOption("features.render_layer", true)
+                .addMixinOption("features.render_layer.leaves", true)
+                .addMixinOption("features.sky", true)
+                .addMixinOption("features.texture_tracking", true)
+                .addMixinOption("features.texture_updates", true)
+                .addMixinOption("features.world_ticking", true)
+                .addMixinOption("features.fast_biome_colors", true)
+                .addMixinOption("features.shader", true)
+                .build(FabricLoader.getInstance().getConfigDir().resolve("sodium.properties"));
     }
 
     @Override
-    public String getRefMapperConfig() {
-        return null;
-    }
-
-    @Override
-    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        if (!mixinClassName.startsWith(MIXIN_PACKAGE_ROOT)) {
-            this.logger.error("Expected mixin '{}' to start with package root '{}', treating as foreign and " +
-                    "disabling!", mixinClassName, MIXIN_PACKAGE_ROOT);
-
-            return false;
-        }
-
-        String mixin = mixinClassName.substring(MIXIN_PACKAGE_ROOT.length());
-        Option option = this.config.getEffectiveOptionForMixin(mixin);
-
-        if (option == null) {
-            this.logger.error("No rules matched mixin '{}', treating as foreign and disabling!", mixin);
-
-            return false;
-        }
-
-        if (option.isOverridden()) {
-            String source = "[unknown]";
-
-            if (option.isUserDefined()) {
-                source = "user configuration";
-            } else if (option.isModDefined()) {
-                source = "mods [" + String.join(", ", option.getDefiningMods()) + "]";
-            }
-
-            if (option.isEnabled()) {
-                this.logger.warn("Force-enabling mixin '{}' as rule '{}' (added by {}) enables it", mixin,
-                        option.getName(), source);
-            } else {
-                this.logger.warn("Force-disabling mixin '{}' as rule '{}' (added by {}) disables it and children", mixin,
-                        option.getName(), source);
-            }
-        }
-
-        return option.isEnabled();
-    }
-    @Override
-    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
-
-    }
-
-    @Override
-    public List<String> getMixins() {
-        return null;
-    }
-
-    @Override
-    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
-    }
-
-    @Override
-    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
-
+    protected String mixinPackageRoot() {
+        return MIXIN_PACKAGE_ROOT;
     }
 }
