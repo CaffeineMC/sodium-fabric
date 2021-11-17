3a104d9d04766dca863934c1f0766e9b441611e7
Morgan Lewis <meslewis@gmail.com>
2021-11-17 16:13:12 -0600

    Add support for compute shader type

--- src/main/java/me/jellysquid/mods/sodium/client/gl/shader/ShaderType.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gl/shader/ShaderType.java
@@ -1,13 +1,15 @@
 package me.jellysquid.mods.sodium.client.gl.shader;
 
 import org.lwjgl.opengl.GL20C;
+import org.lwjgl.opengl.GL43C;
 
 /**
  * An enumeration over the supported OpenGL shader types.
  */
 public enum ShaderType {
     VERTEX(GL20C.GL_VERTEX_SHADER),
-    FRAGMENT(GL20C.GL_FRAGMENT_SHADER);
+    FRAGMENT(GL20C.GL_FRAGMENT_SHADER),
+    COMPUTE(GL43C.GL_COMPUTE_SHADER);
 
     public final int id;
 
