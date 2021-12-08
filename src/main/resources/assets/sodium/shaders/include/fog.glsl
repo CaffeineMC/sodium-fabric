0c3846c41d55601fb1a6f15f86dcc671b26afc51
jellysquid3 <jellysquid3@users.noreply.github.com>
2021-12-08 17:21:46 -0600

    change: Use cylindrical fog from MC 1.18.1

--- src/main/resources/assets/sodium/shaders/include/fog.glsl
+++ src/main/resources/assets/sodium/shaders/include/fog.glsl
@@ -6,8 +6,16 @@ vec4 _linearFog(vec4 fragColor, float fragDistance, vec4 fogColor, float fogStar
 
     return result;
 }
+float _cylindrical_distance(vec3 pos) {
+    float distXZ = length(vec3(pos.x, 0.0, pos.z));
+    float distY = length(vec3(0.0, pos.y, 0.0));
+    return max(distXZ, distY);
+}
 #else
 vec4 _linearFog(vec4 fragColor, float fragDistance, vec4 fogColor, float fogStart, float fogEnd) {
     return fragColor;
+}
+float _cylindrical_distance(vec4 pos) {
+    return length(pos);
 }
 #endif
