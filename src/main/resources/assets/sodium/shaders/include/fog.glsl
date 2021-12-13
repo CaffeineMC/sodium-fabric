17dd727f56b617fbe0500e7137ae999f8e70db12
dopadream <slyshidiscord@gmail.com>
2021-12-12 21:59:17 -0600

    stupid

--- src/main/resources/assets/sodium/shaders/include/fog.glsl
+++ src/main/resources/assets/sodium/shaders/include/fog.glsl
@@ -1,13 +1,13 @@
 #ifdef USE_FOG
 vec4 _linearFog(vec4 fragColor, float fragDistance, vec4 fogColor, float fogStart, float fogEnd) {
     vec4 result = mix(fogColor, fragColor,
-    smoothstep(fogEnd, fogStart, fragDistance));
+    smoothstep(fogEnd + 10, fogStart, fragDistance));
     result.a = fragColor.a;
 
     return result;
 }
 float _cylindrical_distance(vec3 pos) {
-    float distXZ = length(vec3(pos.x, 0.0, pos.z));
+    float distXZ = length(vec3(pos.x, 1.0, pos.z));
     float distY = length(vec3(0.0, pos.y, 0.0));
     return max(distXZ, distY);
 }
