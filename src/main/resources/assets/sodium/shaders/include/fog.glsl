1330198a9a99c768b7bc34df5b2baab1e15ec92f
IMS212 <ims@ims21.tech>
2021-12-12 23:25:59 -0800

    Fix fog calculation finally

--- src/main/resources/assets/sodium/shaders/include/fog.glsl
+++ src/main/resources/assets/sodium/shaders/include/fog.glsl
@@ -1,22 +1,11 @@
-#ifdef USE_FOG
 vec4 _linearFog(vec4 fragColor, float fragDistance, vec4 fogColor, float fogStart, float fogEnd) {
-    if (fragDistance <= fogStart) {
-        return fragColor;
-    }
+    #ifdef USE_FOG
+    vec4 result = mix(fogColor, fragColor,
+    smoothstep(fogEnd, fogStart, fragDistance));
+    result.a = fragColor.a;
 
-    float fogValue = fragDistance < fogEnd ? smoothstep(fogStart, fogEnd, fragDistance) : 1.0;
-    return vec4(mix(fragColor.rgb, fogColor.rgb, fogValue * fogColor.a), fragColor.a);
-}
-float _cylindrical_distance(vec3 pos) {
-    float distXZ = length(vec3(pos.x, 0.0, pos.z));
-    float distY = length(vec3(0.0, pos.y, 0.0));
-    return max(distXZ, distY);
-}
-#else
-vec4 _linearFog(vec4 fragColor, float fragDistance, vec4 fogColor, float fogStart, float fogEnd) {
+    return result;
+    #else
     return fragColor;
+    #endif
 }
-float _cylindrical_distance(vec4 pos) {
-    return length(pos);
-}
-#endif
