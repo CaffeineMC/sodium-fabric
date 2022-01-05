ab2556731ca89fafb366f49075fb981dbb45542a
jellysquid3 <jellysquid3@users.noreply.github.com>
2022-01-04 18:53:04 -0600

    fix: Use correct alpha cutoff values in chunk rendering

--- src/main/java/me/jellysquid/mods/sodium/render/chunk/passes/DefaultRenderPasses.java
+++ src/main/java/me/jellysquid/mods/sodium/render/chunk/passes/DefaultRenderPasses.java
@@ -5,8 +5,8 @@ import me.jellysquid.mods.sodium.opengl.types.TranslucencyMode;
 
 public class DefaultRenderPasses {
     public static final ChunkRenderPass SOLID = new ChunkRenderPass(RenderPipeline.defaults(), true, 0.0f);
-    public static final ChunkRenderPass CUTOUT_MIPPED = new ChunkRenderPass(RenderPipeline.defaults(), true, 0.1f);
-    public static final ChunkRenderPass CUTOUT = new ChunkRenderPass(RenderPipeline.defaults(), false, 0.5f);
+    public static final ChunkRenderPass CUTOUT_MIPPED = new ChunkRenderPass(RenderPipeline.defaults(), true, 0.5f);
+    public static final ChunkRenderPass CUTOUT = new ChunkRenderPass(RenderPipeline.defaults(), false, 0.1f);
     public static final ChunkRenderPass TRANSLUCENT = new ChunkRenderPass(RenderPipeline.builder()
             .setTranslucencyMode(TranslucencyMode.ENABLED)
             .build(), true, 0.0f);
