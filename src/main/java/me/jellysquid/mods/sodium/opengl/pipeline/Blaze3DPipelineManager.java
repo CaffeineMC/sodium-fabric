001f7a20e750276f256022b3285370097ee58168
jellysquid3 <jellysquid3@users.noreply.github.com>
2022-01-09 16:26:27 -0600

    fix: Reset depth/mask state to defaults after rendering

--- src/main/java/me/jellysquid/mods/sodium/opengl/pipeline/Blaze3DPipelineManager.java
+++ src/main/java/me/jellysquid/mods/sodium/opengl/pipeline/Blaze3DPipelineManager.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import me.jellysquid.mods.sodium.opengl.sampler.Sampler;
 import me.jellysquid.mods.sodium.opengl.types.CullingMode;
+import me.jellysquid.mods.sodium.opengl.types.DepthFunc;
 import me.jellysquid.mods.sodium.opengl.types.RenderState;
 import net.minecraft.client.render.BufferRenderer;
 import org.lwjgl.opengl.GL45C;
@@ -28,6 +29,8 @@ public class Blaze3DPipelineManager implements PipelineManager {
         } finally {
             this.state.restore();
         }
+
+        unsetRenderState(pipeline.getState());
     }
 
     private static void setRenderState(RenderState renderState) {
@@ -46,14 +49,38 @@ public class Blaze3DPipelineManager implements PipelineManager {
             RenderSystem.defaultBlendFunc();
         }
 
-        RenderSystem.depthMask(renderState.depthMask);
-        RenderSystem.colorMask(renderState.colorMask.red, renderState.colorMask.green, renderState.colorMask.blue, renderState.colorMask.alpha);
+        if (!renderState.writeMask.depth()) {
+            RenderSystem.depthMask(false);
+        }
 
-        if (renderState.depthFunc != null) {
+        if (!renderState.writeMask.color()) {
+            RenderSystem.colorMask(false, false, false, false);
+        }
+
+        if (renderState.depthFunc != DepthFunc.ALWAYS) {
             RenderSystem.enableDepthTest();
             RenderSystem.depthFunc(renderState.depthFunc.id);
         } else {
             RenderSystem.disableDepthTest();
+        }
+    }
+
+
+    private static void unsetRenderState(RenderState renderState) {
+        // VANILLA BUG: Some code paths rely on the depth function being GL_LEQUAL by default
+        // Not setting this causes graphical corruption when certain effects are being rendered (i.e. items with glint)
+        // If we changed the depth function, we need to be sure to restore it
+        if (renderState.depthFunc != DepthFunc.ALWAYS) {
+            RenderSystem.depthFunc(GL45C.GL_LEQUAL);
+        }
+
+        // VANILLA BUG: Depth and color masks must be reset to their default value of true for each
+        if (!renderState.writeMask.depth()) {
+            RenderSystem.depthMask(true);
+        }
+
+        if (!renderState.writeMask.color()) {
+            RenderSystem.colorMask(true, true, true, true);
         }
     }
 
