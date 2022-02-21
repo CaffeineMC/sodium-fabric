bae26571fe5a41669235edb9d80bdd62c0b0119b
jellysquid3 <jellysquid3@users.noreply.github.com>
2022-02-21 14:15:43 -0600

    fix: Reset color modulator to all-white when using the light map
    This fixes a lot of issues where things don't render or
    have the wrong colors when going through our code path.

--- src/main/java/me/jellysquid/mods/sodium/interop/vanilla/mixin/MultiPhaseParametersExtended.java
+++ src/main/java/me/jellysquid/mods/sodium/interop/vanilla/mixin/MultiPhaseParametersExtended.java
@@ -3,6 +3,7 @@ package me.jellysquid.mods.sodium.interop.vanilla.mixin;
 import me.jellysquid.mods.sodium.opengl.shader.Program;
 import me.jellysquid.mods.sodium.opengl.types.RenderState;
 import me.jellysquid.mods.sodium.render.immediate.VanillaShaderInterface;
+import org.joml.Vector4f;
 
 public interface MultiPhaseParametersExtended {
     RenderState createRenderState();
@@ -10,4 +11,6 @@ public interface MultiPhaseParametersExtended {
     Program<VanillaShaderInterface> createProgram();
 
     ShaderTexture[] createShaderTextures();
+
+    Vector4f getDefaultColorModulator();
 }
