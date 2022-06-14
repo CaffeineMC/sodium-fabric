039aec84ac6349d9d9a850c7fbd5a73b99d781b1
burgerguy <ryan.gehling@yahoo.com>
2022-06-14 12:49:10 -0400

    change: get rid of unneeded float -> double cast

--- src/main/java/net/caffeinemc/sodium/mixin/features/entity/fast_render/MixinModelPart.java
+++ src/main/java/net/caffeinemc/sodium/mixin/features/entity/fast_render/MixinModelPart.java
@@ -13,6 +13,7 @@ import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Matrix4f;
 import net.minecraft.util.math.Vec3f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -105,11 +106,13 @@ public class MixinModelPart {
     @SuppressWarnings("DuplicatedCode")
     @Overwrite
     public void rotate(MatrixStack matrices) {
-        matrices.translate(this.pivotX * NORM, this.pivotY * NORM, this.pivotZ * NORM);
-
         MatrixStack.Entry currentStackEntry = matrices.peek();
 
-        Matrix4fExtended modelMat = MatrixUtil.getExtendedMatrix(currentStackEntry.getPositionMatrix());
+        Matrix4f modelMat = currentStackEntry.getPositionMatrix();
+
+        modelMat.multiplyByTranslation(this.pivotX * NORM, this.pivotY * NORM, this.pivotZ * NORM);
+
+        Matrix4fExtended modelMatEx = MatrixUtil.getExtendedMatrix(modelMat);
 
         float sx = MathHelper.sin(this.pitch);
         float cx = MathHelper.cos(this.pitch);
@@ -131,55 +134,55 @@ public class MixinModelPart {
 
         // multiply components (that don't result in an equivalent value) individually. pray for autovectorization.
         // if JOML's FMA mode is enabled, it will use FMA, otherwise it will do the typical floating point operation
-        float newModel00 = Math.fma(modelMat.getA00(), rot00, Math.fma(modelMat.getA01(), rot10, modelMat.getA02() * rot20));
-        float newModel01 = Math.fma(modelMat.getA00(), rot01, Math.fma(modelMat.getA01(), rot11, modelMat.getA02() * rot21));
-        float newModel02 = Math.fma(modelMat.getA00(), rot02, Math.fma(modelMat.getA01(), rot12, modelMat.getA02() * rot22));
-        float newModel10 = Math.fma(modelMat.getA10(), rot00, Math.fma(modelMat.getA11(), rot10, modelMat.getA12() * rot20));
-        float newModel11 = Math.fma(modelMat.getA10(), rot01, Math.fma(modelMat.getA11(), rot11, modelMat.getA12() * rot21));
-        float newModel12 = Math.fma(modelMat.getA10(), rot02, Math.fma(modelMat.getA11(), rot12, modelMat.getA12() * rot22));
-        float newModel20 = Math.fma(modelMat.getA20(), rot00, Math.fma(modelMat.getA21(), rot10, modelMat.getA22() * rot20));
-        float newModel21 = Math.fma(modelMat.getA20(), rot01, Math.fma(modelMat.getA21(), rot11, modelMat.getA22() * rot21));
-        float newModel22 = Math.fma(modelMat.getA20(), rot02, Math.fma(modelMat.getA21(), rot12, modelMat.getA22() * rot22));
-        float newModel30 = Math.fma(modelMat.getA30(), rot00, Math.fma(modelMat.getA31(), rot10, modelMat.getA32() * rot20));
-        float newModel31 = Math.fma(modelMat.getA30(), rot01, Math.fma(modelMat.getA31(), rot11, modelMat.getA32() * rot21));
-        float newModel32 = Math.fma(modelMat.getA30(), rot02, Math.fma(modelMat.getA31(), rot12, modelMat.getA32() * rot22));
+        float newModel00 = Math.fma(modelMatEx.getA00(), rot00, Math.fma(modelMatEx.getA01(), rot10, modelMatEx.getA02() * rot20));
+        float newModel01 = Math.fma(modelMatEx.getA00(), rot01, Math.fma(modelMatEx.getA01(), rot11, modelMatEx.getA02() * rot21));
+        float newModel02 = Math.fma(modelMatEx.getA00(), rot02, Math.fma(modelMatEx.getA01(), rot12, modelMatEx.getA02() * rot22));
+        float newModel10 = Math.fma(modelMatEx.getA10(), rot00, Math.fma(modelMatEx.getA11(), rot10, modelMatEx.getA12() * rot20));
+        float newModel11 = Math.fma(modelMatEx.getA10(), rot01, Math.fma(modelMatEx.getA11(), rot11, modelMatEx.getA12() * rot21));
+        float newModel12 = Math.fma(modelMatEx.getA10(), rot02, Math.fma(modelMatEx.getA11(), rot12, modelMatEx.getA12() * rot22));
+        float newModel20 = Math.fma(modelMatEx.getA20(), rot00, Math.fma(modelMatEx.getA21(), rot10, modelMatEx.getA22() * rot20));
+        float newModel21 = Math.fma(modelMatEx.getA20(), rot01, Math.fma(modelMatEx.getA21(), rot11, modelMatEx.getA22() * rot21));
+        float newModel22 = Math.fma(modelMatEx.getA20(), rot02, Math.fma(modelMatEx.getA21(), rot12, modelMatEx.getA22() * rot22));
+        float newModel30 = Math.fma(modelMatEx.getA30(), rot00, Math.fma(modelMatEx.getA31(), rot10, modelMatEx.getA32() * rot20));
+        float newModel31 = Math.fma(modelMatEx.getA30(), rot01, Math.fma(modelMatEx.getA31(), rot11, modelMatEx.getA32() * rot21));
+        float newModel32 = Math.fma(modelMatEx.getA30(), rot02, Math.fma(modelMatEx.getA31(), rot12, modelMatEx.getA32() * rot22));
 
-        modelMat.setA00(newModel00);
-        modelMat.setA01(newModel01);
-        modelMat.setA02(newModel02);
-        modelMat.setA10(newModel10);
-        modelMat.setA11(newModel11);
-        modelMat.setA12(newModel12);
-        modelMat.setA20(newModel20);
-        modelMat.setA21(newModel21);
-        modelMat.setA22(newModel22);
-        modelMat.setA30(newModel30);
-        modelMat.setA31(newModel31);
-        modelMat.setA32(newModel32);
+        modelMatEx.setA00(newModel00);
+        modelMatEx.setA01(newModel01);
+        modelMatEx.setA02(newModel02);
+        modelMatEx.setA10(newModel10);
+        modelMatEx.setA11(newModel11);
+        modelMatEx.setA12(newModel12);
+        modelMatEx.setA20(newModel20);
+        modelMatEx.setA21(newModel21);
+        modelMatEx.setA22(newModel22);
+        modelMatEx.setA30(newModel30);
+        modelMatEx.setA31(newModel31);
+        modelMatEx.setA32(newModel32);
 
-        Matrix3fExtended normalMat = MatrixUtil.getExtendedMatrix(currentStackEntry.getNormalMatrix());
+        Matrix3fExtended normalMatEx = MatrixUtil.getExtendedMatrix(currentStackEntry.getNormalMatrix());
 
         // multiply all components and pray for autovectorization
         // if JOML's FMA mode is enabled, it will use FMA, otherwise it will do the typical floating point operation
-        float newNormal00 = Math.fma(normalMat.getA00(), rot00, Math.fma(normalMat.getA01(), rot10, normalMat.getA02() * rot20));
-        float newNormal01 = Math.fma(normalMat.getA00(), rot01, Math.fma(normalMat.getA01(), rot11, normalMat.getA02() * rot21));
-        float newNormal02 = Math.fma(normalMat.getA00(), rot02, Math.fma(normalMat.getA01(), rot12, normalMat.getA02() * rot22));
-        float newNormal10 = Math.fma(normalMat.getA10(), rot00, Math.fma(normalMat.getA11(), rot10, normalMat.getA12() * rot20));
-        float newNormal11 = Math.fma(normalMat.getA10(), rot01, Math.fma(normalMat.getA11(), rot11, normalMat.getA12() * rot21));
-        float newNormal12 = Math.fma(normalMat.getA10(), rot02, Math.fma(normalMat.getA11(), rot12, normalMat.getA12() * rot22));
-        float newNormal20 = Math.fma(normalMat.getA20(), rot00, Math.fma(normalMat.getA21(), rot10, normalMat.getA22() * rot20));
-        float newNormal21 = Math.fma(normalMat.getA20(), rot01, Math.fma(normalMat.getA21(), rot11, normalMat.getA22() * rot21));
-        float newNormal22 = Math.fma(normalMat.getA20(), rot02, Math.fma(normalMat.getA21(), rot12, normalMat.getA22() * rot22));
+        float newNormal00 = Math.fma(normalMatEx.getA00(), rot00, Math.fma(normalMatEx.getA01(), rot10, normalMatEx.getA02() * rot20));
+        float newNormal01 = Math.fma(normalMatEx.getA00(), rot01, Math.fma(normalMatEx.getA01(), rot11, normalMatEx.getA02() * rot21));
+        float newNormal02 = Math.fma(normalMatEx.getA00(), rot02, Math.fma(normalMatEx.getA01(), rot12, normalMatEx.getA02() * rot22));
+        float newNormal10 = Math.fma(normalMatEx.getA10(), rot00, Math.fma(normalMatEx.getA11(), rot10, normalMatEx.getA12() * rot20));
+        float newNormal11 = Math.fma(normalMatEx.getA10(), rot01, Math.fma(normalMatEx.getA11(), rot11, normalMatEx.getA12() * rot21));
+        float newNormal12 = Math.fma(normalMatEx.getA10(), rot02, Math.fma(normalMatEx.getA11(), rot12, normalMatEx.getA12() * rot22));
+        float newNormal20 = Math.fma(normalMatEx.getA20(), rot00, Math.fma(normalMatEx.getA21(), rot10, normalMatEx.getA22() * rot20));
+        float newNormal21 = Math.fma(normalMatEx.getA20(), rot01, Math.fma(normalMatEx.getA21(), rot11, normalMatEx.getA22() * rot21));
+        float newNormal22 = Math.fma(normalMatEx.getA20(), rot02, Math.fma(normalMatEx.getA21(), rot12, normalMatEx.getA22() * rot22));
 
-        normalMat.setA00(newNormal00);
-        normalMat.setA01(newNormal01);
-        normalMat.setA02(newNormal02);
-        normalMat.setA10(newNormal10);
-        normalMat.setA11(newNormal11);
-        normalMat.setA12(newNormal12);
-        normalMat.setA20(newNormal20);
-        normalMat.setA21(newNormal21);
-        normalMat.setA22(newNormal22);
+        normalMatEx.setA00(newNormal00);
+        normalMatEx.setA01(newNormal01);
+        normalMatEx.setA02(newNormal02);
+        normalMatEx.setA10(newNormal10);
+        normalMatEx.setA11(newNormal11);
+        normalMatEx.setA12(newNormal12);
+        normalMatEx.setA20(newNormal20);
+        normalMatEx.setA21(newNormal21);
+        normalMatEx.setA22(newNormal22);
 
         if (this.xScale != 1.0F || this.yScale != 1.0F || this.zScale != 1.0F) {
             matrices.scale(this.xScale, this.yScale, this.zScale);
