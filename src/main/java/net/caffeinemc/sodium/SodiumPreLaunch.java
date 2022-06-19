9a5ac16fc677f78125f9b010ec02ac9752bba6bb
burgerguy <ryan.gehling@yahoo.com>
2022-06-18 22:21:24 -0400

    add: Command-line argument to load renderdoc (sodium.load_renderdoc)

--- src/main/java/net/caffeinemc/sodium/SodiumPreLaunch.java
+++ src/main/java/net/caffeinemc/sodium/SodiumPreLaunch.java
@@ -16,8 +16,19 @@ public class SodiumPreLaunch implements PreLaunchEntrypoint {
 
     @Override
     public void onPreLaunch() {
-        System.loadLibrary("renderdoc");
+        tryLoadRenderdoc();
         checkJemalloc();
+    }
+
+    private static void tryLoadRenderdoc() {
+        if (System.getProperty("sodium.load_renderdoc") != null) {
+            LOGGER.info("Loading renderdoc...");
+            try {
+                System.loadLibrary("renderdoc");
+            } catch (Throwable t) {
+                LOGGER.warn("Unable to load renderdoc (is it in your path?)", t);
+            }
+        }
     }
 
     private static void checkJemalloc() {
@@ -25,7 +36,7 @@ public class SodiumPreLaunch implements PreLaunchEntrypoint {
         // Using the system allocator prevents memory leaks and other problems
         // See changelog here: https://github.com/jemalloc/jemalloc/releases/tag/5.2.1
         if (Platform.get() == Platform.WINDOWS && isVersionWithinRange(JEmalloc.JEMALLOC_VERSION, "5.0.0", "5.2.0")) {
-            LOGGER.info("Trying to switch memory allocators to work around memory leaks present with Jemalloc 5.0.0 through 5.2.0 on Windows");
+            LOGGER.info("Switching memory allocators to work around memory leaks present with Jemalloc 5.0.0 through 5.2.0 on Windows...");
 
             if (!Objects.equals(Configuration.MEMORY_ALLOCATOR.get(), "system")) {
                 Configuration.MEMORY_ALLOCATOR.set("system");
