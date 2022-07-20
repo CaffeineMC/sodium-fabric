9e791f592128e789f5ebfa6a050ea47a759125ac
burgerguy <ryan.gehling@yahoo.com>
2022-07-20 01:43:01 -0400

    change: use JOML FMA with Matrix3f (whoops)

--- src/main/java/net/caffeinemc/sodium/mixin/core/matrix/MixinMatrix3f.java
+++ src/main/java/net/caffeinemc/sodium/mixin/core/matrix/MixinMatrix3f.java
@@ -8,6 +8,7 @@ import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec3i;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.joml.Math;
 
 @Mixin(Matrix3f.class)
 public class MixinMatrix3f implements Matrix3fExtended {
