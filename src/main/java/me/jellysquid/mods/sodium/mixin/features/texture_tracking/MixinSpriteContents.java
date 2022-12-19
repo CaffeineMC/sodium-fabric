c975e8e0f6e2b3adbe7adc8215606b8ed432af65
IMS212 <ims@irisshaders.dev>
2022-12-19 14:49:13 -0800

    fix: Attempted fix for a memory leak caused during the 1.19.3 update

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/texture_tracking/MixinSpriteContents.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/texture_tracking/MixinSpriteContents.java
@@ -2,15 +2,28 @@ package me.jellysquid.mods.sodium.mixin.features.texture_tracking;
 
 import me.jellysquid.mods.sodium.client.render.texture.SpriteExtended;
 import net.minecraft.client.texture.SpriteContents;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(SpriteContents.class)
 public abstract class MixinSpriteContents implements SpriteExtended {
+    @Shadow
+    @Final
+    @Nullable
+    private SpriteContents.Animation animation;
+
     private boolean active;
 
     @Override
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    @Override
+    public boolean hasAnimation() {
+        return this.animation != null;
     }
 
     @Override
