635795e4c78deddf7e677469da841b4c2a3678d8
jellysquid3 <jellysquid3@users.noreply.github.com>
2023-01-09 21:44:40 -0600

    Add visibility test benchmarks

--- native/harness/benches/raster.rs
+++ native/harness/benches/raster.rs
@@ -1,8 +1,8 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion, Bencher};
-use rasterizer::{Rasterizer, WriteOnlyPixelFunction, AllExecutionsFunction, BoxFace};
+use rasterizer::{Rasterizer, WriteOnlyPixelFunction, AllExecutionsFunction, BoxFace, ReadOnlyPixelFunction, EarlyExitFunction};
 use ultraviolet::{Mat4, Vec3};
 
-fn raster_cube(bencher: &mut Bencher, width: usize, height: usize) {
+fn draw_cube(bencher: &mut Bencher, width: usize, height: usize) {
     let camera_target = Vec3::new(0.0, 0.0, 0.0);
     let camera_position = Vec3::new(3.0, 3.0, 3.0);
     let camera = Camera::create(camera_target, camera_position, width, height);
@@ -15,17 +15,44 @@ fn raster_cube(bencher: &mut Bencher, width: usize, height: usize) {
         rasterizer.draw_aabb::<WriteOnlyPixelFunction, AllExecutionsFunction>(
             &Vec3::new(-1.0, -1.0, -1.0),
             &Vec3::new(1.0, 1.0, 1.0),
-            BoxFace::ALL);    
+            BoxFace::ALL)
+    });
+
+    black_box(rasterizer.pixels());
+}
+
+fn test_cube(bencher: &mut Bencher, width: usize, height: usize) {
+    let camera_target = Vec3::new(0.0, 0.0, 0.0);
+    let camera_position = Vec3::new(3.0, 3.0, 3.0);
+    let camera = Camera::create(camera_target, camera_position, width, height);
+
+    let mut rasterizer = Rasterizer::create(width, height);
+    rasterizer.clear();
+    rasterizer.set_camera(camera.position, camera.proj_matrix * camera.view_matrix);
+
+    rasterizer.draw_aabb::<WriteOnlyPixelFunction, AllExecutionsFunction>(
+        &Vec3::new(-1.0, -1.0, -1.0),
+        &Vec3::new(1.0, 1.0, 1.0),
+        BoxFace::ALL);
+
+    bencher.iter(|| {
+        rasterizer.draw_aabb::<ReadOnlyPixelFunction, EarlyExitFunction>(
+            &Vec3::new(-1.0, -1.0, -1.0),
+            &Vec3::new(1.0, 1.0, 1.0),
+            BoxFace::ALL)
     });
 
     black_box(rasterizer.pixels());
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("cube  200x200 ", |b| raster_cube(b, 200, 200));
-    c.bench_function("cube  400x400 ", |b| raster_cube(b, 400, 400));
-    c.bench_function("cube  800x800 ", |b| raster_cube(b, 800, 800));
-    c.bench_function("cube 1600x1600", |b| raster_cube(b, 1600, 1600));
+    c.bench_function("draw_cube_200px", |b| draw_cube(b, 200, 200));
+    c.bench_function("draw_cube_400px", |b| draw_cube(b, 400, 400));
+    c.bench_function("draw_cube_800px", |b| draw_cube(b, 800, 800));
+
+    c.bench_function("test_cube_200px", |b| test_cube(b, 200, 200));
+    c.bench_function("test_cube_400px", |b| test_cube(b, 400, 400));
+    c.bench_function("test_cube_800px", |b| test_cube(b, 800, 800));
 }
 
 criterion_group!(benches, criterion_benchmark);
