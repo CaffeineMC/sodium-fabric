25209a56791a68d84febca6642174e0dab3af7b2
jellysquid3 <jellysquid3@users.noreply.github.com>
2023-01-22 22:47:47 -0600

    Avoid trampling locals in MixinCuboid

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/entity/fast_render/MixinCuboid.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/entity/fast_render/MixinCuboid.java
@@ -3,6 +3,7 @@ package me.jellysquid.mods.sodium.mixin.features.entity.fast_render;
 import me.jellysquid.mods.sodium.client.model.ModelCuboidAccessor;
 import me.jellysquid.mods.sodium.client.render.ModelCuboid;
 import net.minecraft.client.model.ModelPart;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinCuboid implements ModelCuboidAccessor {
     private ModelCuboid sodium$cuboid;
 
-    @Inject(method = "<init>", at = @At("RETURN"))
+    // Inject at the start of the function, so we don't capture modified locals
+    @Inject(method = "<init>", at = @At(value = "FIELD", opcode = Opcodes.PUTFIELD, target = "Lnet/minecraft/client/model/ModelPart$Cuboid;sides:[Lnet/minecraft/client/model/ModelPart$Quad;", ordinal = 0))
     private void onInit(int u, int v, float x, float y, float z, float sizeX, float sizeY, float sizeZ, float extraX, float extraY, float extraZ, boolean mirror, float textureWidth, float textureHeight, CallbackInfo ci) {
         this.sodium$cuboid = new ModelCuboid(u, v, x, y, z, sizeX, sizeY, sizeZ, extraX, extraY, extraZ, mirror, textureWidth, textureHeight);
     }
