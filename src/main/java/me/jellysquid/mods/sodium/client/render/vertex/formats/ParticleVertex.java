d0da760e7e4805ff745789f4576ecf1e9ec3bcf2
jellysquid3 <jellysquid3@users.noreply.github.com>
2023-02-07 18:58:26 -0600

    Use correct vertex format for particles

--- src/main/java/me/jellysquid/mods/sodium/client/render/vertex/formats/ParticleVertex.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/vertex/formats/ParticleVertex.java
@@ -6,17 +6,17 @@ import net.minecraft.client.render.VertexFormats;
 import org.lwjgl.system.MemoryUtil;
 
 public final class ParticleVertex {
-    public static final VertexFormatDescription FORMAT = VertexFormatRegistry.get(VertexFormats.POSITION_COLOR_TEXTURE_LIGHT);
+    public static final VertexFormatDescription FORMAT = VertexFormatRegistry.get(VertexFormats.POSITION_TEXTURE_COLOR_LIGHT);
 
     public static final int STRIDE = 28;
 
     private static final int OFFSET_POSITION = 0;
-    private static final int OFFSET_COLOR = 12;
-    private static final int OFFSET_TEXTURE = 16;
+    private static final int OFFSET_TEXTURE = 12;
+    private static final int OFFSET_COLOR = 20;
     private static final int OFFSET_LIGHT = 24;
 
     public static void write(long ptr,
-                             float x, float y, float z, float u, float v, int color, int light) {
+                             float x, float y, float z, int color, float u, float v, int light) {
         MemoryUtil.memPutFloat(ptr + OFFSET_POSITION + 0, x);
         MemoryUtil.memPutFloat(ptr + OFFSET_POSITION + 4, y);
         MemoryUtil.memPutFloat(ptr + OFFSET_POSITION + 8, z);
