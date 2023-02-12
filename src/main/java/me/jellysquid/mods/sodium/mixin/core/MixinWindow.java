22c0c89216184ca3897a34ffc80190a0357b4a88
jellysquid3 <jellysquid3@users.noreply.github.com>
2023-02-12 13:13:17 -0600

    Add option to create the OpenGL context with error checking disabled

---
+++ src/main/java/me/jellysquid/mods/sodium/mixin/core/MixinWindow.java
@@ -0,0 +1,19 @@
+package me.jellysquid.mods.sodium.mixin.core;
+
+import me.jellysquid.mods.sodium.client.SodiumClientMod;
+import net.minecraft.client.util.Window;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Window.class)
+public class MixinWindow {
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwDefaultWindowHints()V", shift = At.Shift.AFTER))
+    public void setAdditionalWindowHints(CallbackInfo ci) {
+        if (SodiumClientMod.options().performance.useNoErrorGLContext) {
+            GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_NO_ERROR, GLFW.GLFW_TRUE);
+        }
+    }
+}
