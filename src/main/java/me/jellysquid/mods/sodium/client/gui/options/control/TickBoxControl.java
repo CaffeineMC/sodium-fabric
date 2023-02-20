349bd1fd4d777472ef0bdb938b3cc6bcea50e849
isXander <xandersmith2008@gmail.com>
2023-02-20 13:31:58 +0000

    fix keyboard navigation

--- src/main/java/me/jellysquid/mods/sodium/client/gui/options/control/TickBoxControl.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/options/control/TickBoxControl.java
@@ -2,6 +2,7 @@ package me.jellysquid.mods.sodium.client.gui.options.control;
 
 import me.jellysquid.mods.sodium.client.gui.options.Option;
 import me.jellysquid.mods.sodium.client.util.Dim2i;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.math.Rect2i;
 import net.minecraft.client.util.math.MatrixStack;
 
@@ -66,13 +67,31 @@ public class TickBoxControl implements Control<Boolean> {
         @Override
         public boolean mouseClicked(double mouseX, double mouseY, int button) {
             if (this.option.isAvailable() && button == 0 && this.dim.containsCursor(mouseX, mouseY)) {
-                this.option.setValue(!this.option.getValue());
+                toggleControl();
                 this.playClickSound();
 
                 return true;
             }
 
             return false;
+        }
+
+        @Override
+        public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+            if (!isFocused()) return false;
+
+            if (keyCode == InputUtil.GLFW_KEY_ENTER) {
+                toggleControl();
+                this.playClickSound();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void toggleControl() {
+            this.option.setValue(!this.option.getValue());
         }
 
         protected void drawRectOutline(int x, int y, int w, int h, int color) {
