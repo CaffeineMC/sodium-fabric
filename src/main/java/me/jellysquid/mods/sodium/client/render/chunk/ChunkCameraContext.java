90ccaf6ca6748ba58f3f051c7a7dba2c5f324aaf
burgerindividual <--global>
2023-06-20 17:53:10 -0400

    Account for negative positions, and reduce precision loss for delta calculation

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/ChunkCameraContext.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/ChunkCameraContext.java
@@ -1,10 +1,10 @@
 package me.jellysquid.mods.sodium.client.render.chunk;
 
 public class ChunkCameraContext {
-    // 32-bit floats have a 23-bit mantissa. We want to reduce that to 14 bits to avoid seams along chunk/region
-    // boundaries. To do this, we have to add and subtract a number with (23 - 14) bits to effectively shift
+    // 32-bit floats have a 23-bit mantissa. We want to reduce that to 16 bits to avoid seams along chunk/region
+    // boundaries. To do this, we have to add and subtract a number with (23 - 16) bits to effectively shift
     // the bits out of the number.
-    private static final float PRECISION_MODIFIER = 0x1p9f; // 512.0f
+    private static final float PRECISION_MODIFIER = 0x1p7f;
 
     public final int blockX, blockY, blockZ;
     public final float deltaX, deltaY, deltaZ;
@@ -15,9 +15,17 @@ public class ChunkCameraContext {
         this.blockY = (int) y;
         this.blockZ = (int) z;
 
-        this.deltaX = ((float) (x - this.blockX) + PRECISION_MODIFIER) - PRECISION_MODIFIER;
-        this.deltaY = ((float) (y - this.blockY) + PRECISION_MODIFIER) - PRECISION_MODIFIER;
-        this.deltaZ = ((float) (z - this.blockZ) + PRECISION_MODIFIER) - PRECISION_MODIFIER;
+        float deltaXFullPrecision = (float) (x - this.blockX);
+        float deltaYFullPrecision = (float) (y - this.blockY);
+        float deltaZFullPrecision = (float) (z - this.blockZ);
+
+        float deltaXModifier = Math.copySign(PRECISION_MODIFIER, deltaXFullPrecision);
+        float deltaYModifier = Math.copySign(PRECISION_MODIFIER, deltaYFullPrecision);
+        float deltaZModifier = Math.copySign(PRECISION_MODIFIER, deltaZFullPrecision);
+
+        this.deltaX = (deltaXFullPrecision + deltaXModifier) - deltaXModifier;
+        this.deltaY = (deltaYFullPrecision + deltaYModifier) - deltaYModifier;
+        this.deltaZ = (deltaZFullPrecision + deltaZModifier) - deltaZModifier;
 
         this.posX = x;
         this.posY = y;
