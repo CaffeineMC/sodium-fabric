8ae68cfa82949a09d53b6ea066ebdba40d5674d4
jellysquid3 <jellysquid@pm.me>
2023-06-24 00:01:55 -0500

    New driver probe and workarounds for NVIDIA drivers
    The NVIDIA drivers now check the command line arguments
    for the presence of a sub-string containing ".main", and
    forcefully enables their "Threaded Optimizations" hack which
    destroys performance and causes crashes with Sodium.
    
    Unfortunately, NVIDIA has implemented this in the most
    insane way possible. Rather than using application-specific
    profiles, they have instead hardcoded the detection
    logic into their driver, and have done so in a manner
    that makes it impossible to use NVAPI to override it.
    
    So, in summary, this workaround does two things:
    
    a) We invoke GetCommandLineW and directly modify the pointer
    of string data it gives us, in order to strip the command line
    arguments of anything identifying.
    
    b) We invoke SetEnvironmentVariableW to set SHIM_MCCOMPAT=0x800000001,
    which hints to the NVIDIA driver that it should use the
    high-performance GPU. This is required, because as mentioned,
    we are breaking the driver's ability to detect Minecraft, which
    means it won't try to run it on the high-performance GPU any longer.
    
    Both of these modifications are applied just before context
    creation, and any changes are restored right after it. This ensures
    other applications which are trying to detect Minecraft are not
    affected.
    
    It is worth noting that this problem *only* affected the Vanilla
    launcher, since the Fabric Installer chose to include a reference
    to the Minecraft entry point on purpose so that the NVIDIA drivers
    could continue detecting it. As far as we know, Prism Launcher does
    not do this, hence it is not affected by the issue.
    
    As a side effect of this work, we have also improved the
    graphics adapter detection code to use OSHI. This is much simpler,
    and allows us to enumerate all graphics adapters more reliably. It
    also doesn't require a sub-process to be launched, which should
    hopefully reduce startup times and some jank.
    
    We have also implemented some minor optimizations which avoid
    the calls into glGetInteger/glGetUniformLocation, as we noticed
    these were particularly hot in profiling on NVIDIA GPUs.
    
    See #1813 for more information.

--- src/main/java/me/jellysquid/mods/sodium/client/util/workarounds/DriverWorkarounds.java
+++ src/main/java/me/jellysquid/mods/sodium/client/util/workarounds/DriverWorkarounds.java
@@ -1,60 +1,23 @@
 package me.jellysquid.mods.sodium.client.util.workarounds;
 
-import net.minecraft.util.Util;
+import me.jellysquid.mods.sodium.client.util.workarounds.platform.NVIDIAWorkarounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.EnumSet;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
-
 public class DriverWorkarounds {
-    private static final Logger LOGGER = LoggerFactory.getLogger("Sodium");
+    private static final Logger LOGGER = LoggerFactory.getLogger("Sodium-DriverWorkarounds");
 
-    private static final AtomicReference<Set<Reference>> ACTIVE_WORKAROUNDS = new AtomicReference<>(EnumSet.noneOf(Reference.class));
+    public static void beforeContextCreation() {
+        LOGGER.info("Checking for any workarounds that need to be applied prior to context creation...");
 
-    public static void init() {
-        LOGGER.info("Launching a new process to probe the system configuration!");
-
-        DriverProbeResult result;
-
-        try {
-            result = DriverProbeLauncher.launchProbe();
-        } catch (Throwable t) {
-            LOGGER.error("Failed to launch driver probe", t);
-            return;
+        if (Workarounds.isWorkaroundEnabled(Workarounds.Reference.NVIDIA_BAD_DRIVER_SETTINGS)) {
+            NVIDIAWorkarounds.install();
         }
-
-        LOGGER.info("OpenGL Vendor: {}", result.vendor);
-        LOGGER.info("OpenGL Renderer: {}", result.renderer);
-        LOGGER.info("OpenGL Version: {}", result.version);
-
-        var workarounds = updateWorkarounds(result);
-
-        if (!workarounds.isEmpty()) {
-            LOGGER.warn("One or more workarounds were enabled to prevent crashes or other issues on your system. You may need to update your graphics drivers.");
-        }
-
-        ACTIVE_WORKAROUNDS.set(workarounds);
     }
 
-    private static Set<Reference> updateWorkarounds(DriverProbeResult probe) {
-        var workarounds = EnumSet.noneOf(Reference.class);
-        var operatingSystem = Util.getOperatingSystem();
-
-        if (operatingSystem == Util.OperatingSystem.WINDOWS && probe.vendor.contains("NVIDIA")) {
-            workarounds.add(Reference.ISSUE_1486);
-            LOGGER.warn("Enabling workaround for NVIDIA graphics drivers on Windows (issue #1486)");
+    public static void afterContextCreation() {
+        if (Workarounds.isWorkaroundEnabled(Workarounds.Reference.NVIDIA_BAD_DRIVER_SETTINGS)) {
+            NVIDIAWorkarounds.uninstall();
         }
-
-        return workarounds;
-    }
-
-    public static boolean isWorkaroundEnabled(Reference id) {
-        return ACTIVE_WORKAROUNDS.get().contains(id);
-    }
-
-    public enum Reference {
-        ISSUE_1486
     }
 }
---
+++ src/main/java/me/jellysquid/mods/sodium/client/util/workarounds/Workarounds.java
@@ -0,0 +1,50 @@
+package me.jellysquid.mods.sodium.client.util.workarounds;
+
+import net.minecraft.util.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
+public class Workarounds {
+    private static final Logger LOGGER = LoggerFactory.getLogger("Sodium");
+
+    private static final AtomicReference<Set<Reference>> ACTIVE_WORKAROUNDS = new AtomicReference<>(EnumSet.noneOf(Reference.class));
+
+    public static void init() {
+        var graphicsVendors = GraphicsAdapterProbe.findAdapters();
+        var workarounds = findNecessaryWorkarounds(graphicsVendors);
+
+        if (!workarounds.isEmpty()) {
+            LOGGER.warn("One or more workarounds were enabled to prevent crashes or other issues on your system: [{}]", workarounds.stream()
+                    .map(Enum::name)
+                    .collect(Collectors.joining(", ")));
+            LOGGER.warn("This might indicate that you need to update your graphics drivers.");
+        }
+
+        ACTIVE_WORKAROUNDS.set(workarounds);
+    }
+
+    private static Set<Reference> findNecessaryWorkarounds(List<GraphicsAdapterProbe.Result> graphicsAdapters) {
+        var workarounds = EnumSet.noneOf(Reference.class);
+        var operatingSystem = Util.getOperatingSystem();
+
+        if (operatingSystem == Util.OperatingSystem.WINDOWS && graphicsAdapters.stream()
+                .anyMatch(adapter -> adapter.vendor() == GraphicsAdapterProbe.Vendor.NVIDIA)) {
+            workarounds.add(Reference.NVIDIA_BAD_DRIVER_SETTINGS);
+        }
+
+        return Collections.unmodifiableSet(workarounds);
+    }
+
+    public static boolean isWorkaroundEnabled(Reference id) {
+        return ACTIVE_WORKAROUNDS.get()
+                .contains(id);
+    }
+
+    public enum Reference {
+        NVIDIA_BAD_DRIVER_SETTINGS
+    }
+}
