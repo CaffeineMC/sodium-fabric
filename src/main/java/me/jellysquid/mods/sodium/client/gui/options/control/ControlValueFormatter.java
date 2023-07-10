26e35fcbe59173fd900c89b3ddb288c41654a1f7
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2023-07-10 19:26:34 +0800

    fix: Use GUI render layer when drawing rectangles for options

--- src/main/java/me/jellysquid/mods/sodium/client/gui/options/control/ControlValueFormatter.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/options/control/ControlValueFormatter.java
@@ -4,48 +4,48 @@ import net.minecraft.text.Text;
 
 public interface ControlValueFormatter {
     static ControlValueFormatter guiScale() {
-        return (v) -> (v == 0) ? Text.translatable("options.guiScale.auto").getString() : v + "x";
+        return (v) -> (v == 0) ? Text.translatable("options.guiScale.auto") : Text.literal(v + "x");
     }
 
     static ControlValueFormatter fpsLimit() {
-        return (v) -> (v == 260) ? Text.translatable("options.framerateLimit.max").getString() : Text.translatable("options.framerate", v).getString();
+        return (v) -> (v == 260) ? Text.translatable("options.framerateLimit.max") : Text.translatable("options.framerate", v);
     }
 
     static ControlValueFormatter brightness() {
         return (v) -> {
             if (v == 0) {
-                return Text.translatable("options.gamma.min").getString();
+                return Text.translatable("options.gamma.min");
             } else if (v == 100) {
-                return Text.translatable("options.gamma.max").getString();
+                return Text.translatable("options.gamma.max");
             } else {
-                return v + "%";
+                return Text.literal(v + "%");
             }
         };
     }
 
     static ControlValueFormatter biomeBlend() {
-        return (v) -> (v == 0) ? Text.translatable("gui.none").getString() : Text.translatable("sodium.options.biome_blend.value", v).getString();
+        return (v) -> (v == 0) ? Text.translatable("gui.none") : Text.translatable("sodium.options.biome_blend.value", v);
     }
 
-    String format(int value);
+    Text format(int value);
 
     static ControlValueFormatter translateVariable(String key) {
-        return (v) -> Text.translatable(key, v).getString();
+        return (v) -> Text.translatable(key, v);
     }
 
     static ControlValueFormatter percentage() {
-        return (v) -> v + "%";
+        return (v) -> Text.literal(v + "%");
     }
 
     static ControlValueFormatter multiplier() {
-        return (v) -> v + "x";
+        return (v) -> Text.literal(v + "%");
     }
 
     static ControlValueFormatter quantityOrDisabled(String name, String disableText) {
-        return (v) -> v == 0 ? disableText : v + " " + name;
+        return (v) -> Text.literal(v == 0 ? disableText : v + " " + name);
     }
 
     static ControlValueFormatter number() {
-        return String::valueOf;
+        return (v) -> Text.literal(String.valueOf(v));
     }
 }
