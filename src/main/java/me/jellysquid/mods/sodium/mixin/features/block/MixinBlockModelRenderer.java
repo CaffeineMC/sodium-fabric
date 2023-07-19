962d443d09dbca227d09506d2714b488d857d4b6
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-07-19 20:47:09 -0300

    Update MixinBlockModelRenderer.java

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/block/MixinBlockModelRenderer.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/block/MixinBlockModelRenderer.java
@@ -1,12 +1,11 @@
 package me.jellysquid.mods.sodium.mixin.features.block;
 
-import me.jellysquid.mods.sodium.client.model.quad.ModelQuadView;
-import me.jellysquid.mods.sodium.client.render.vertex.formats.ModelVertex;
-import me.jellysquid.mods.sodium.client.render.vertex.VertexBufferWriter;
+import me.jellysquid.mods.sodium.client.model.quad.BakedQuadView;
+import me.jellysquid.mods.sodium.client.render.immediate.model.BakedModelEncoder;
 import me.jellysquid.mods.sodium.client.render.texture.SpriteUtil;
-import me.jellysquid.mods.sodium.client.util.Norm3b;
-import me.jellysquid.mods.sodium.client.util.color.ColorABGR;
 import me.jellysquid.mods.sodium.common.util.DirectionUtil;
+import net.caffeinemc.mods.sodium.api.util.ColorABGR;
+import net.caffeinemc.mods.sodium.api.vertex.buffer.VertexBufferWriter;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.block.BlockModelRenderer;
@@ -17,12 +16,9 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.LocalRandom;
 import net.minecraft.util.math.random.Random;
-import net.minecraft.util.math.random.Xoroshiro128PlusPlusRandom;
-import org.joml.Matrix3f;
-import org.joml.Matrix4f;
-import org.lwjgl.system.MemoryStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
 
 import java.util.List;
 
@@ -36,9 +32,9 @@ public class MixinBlockModelRenderer {
      */
     @Overwrite
     public void render(MatrixStack.Entry entry, VertexConsumer vertexConsumer, BlockState blockState, BakedModel bakedModel, float red, float green, float blue, int light, int overlay) {
-        Random random = this.random;
-
         var writer = VertexBufferWriter.of(vertexConsumer);
+
+        Random random = this.random;
 
         // Clamp color ranges
         red = MathHelper.clamp(red, 0.0F, 1.0F);
@@ -52,7 +48,7 @@ public class MixinBlockModelRenderer {
             List<BakedQuad> quads = bakedModel.getQuads(blockState, direction, random);
 
             if (!quads.isEmpty()) {
-                renderQuad(entry, writer, defaultColor, quads, light, overlay);
+                renderQuads(entry, writer, defaultColor, quads, light, overlay);
             }
         }
 
@@ -60,19 +56,20 @@ public class MixinBlockModelRenderer {
         List<BakedQuad> quads = bakedModel.getQuads(blockState, null, random);
 
         if (!quads.isEmpty()) {
-            renderQuad(entry, writer, defaultColor, quads, light, overlay);
+            renderQuads(entry, writer, defaultColor, quads, light, overlay);
         }
     }
 
+    @Unique
     @SuppressWarnings("ForLoopReplaceableByForEach")
-    private static void renderQuad(MatrixStack.Entry matrices, VertexBufferWriter writer, int defaultColor, List<BakedQuad> list, int light, int overlay) {
-        for (int j = 0; j < list.size(); j++) {
-            BakedQuad bakedQuad = list.get(j);
-            int color = bakedQuad.hasColor() ? defaultColor : 0xFFFFFFFF;
+    private static void renderQuads(MatrixStack.Entry matrices, VertexBufferWriter writer, int defaultColor, List<BakedQuad> quads, int light, int overlay) {
+        for (int i = 0; i < quads.size(); i++) {
+            BakedQuad bakedQuad = quads.get(i);
+            BakedQuadView quad = (BakedQuadView) bakedQuad;
 
-            ModelQuadView quad = ((ModelQuadView) bakedQuad);
+            int color = quad.hasColor() ? defaultColor : 0xFFFFFFFF;
 
-            ModelVertex.writeQuadVertices(writer, matrices, quad, light, overlay, color);
+            BakedModelEncoder.writeQuadVertices(writer, matrices, quad, color, light, overlay);
 
             SpriteUtil.markSpriteActive(quad.getSprite());
         }
