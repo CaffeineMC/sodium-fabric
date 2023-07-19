6c1ab714756477b3e7772ce6c1d40b4f20e25532
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-07-19 19:30:25 -0300

    Add material system for consolidating draw calls

--- src/main/java/me/jellysquid/mods/sodium/client/gl/compile/ChunkBuildContext.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gl/compile/ChunkBuildContext.java
@@ -1,8 +1,7 @@
 package me.jellysquid.mods.sodium.client.gl.compile;
 
-import me.jellysquid.mods.sodium.client.render.vertex.type.ChunkVertexType;
+import me.jellysquid.mods.sodium.client.render.chunk.vertex.format.ChunkVertexType;
 import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildBuffers;
-import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPassManager;
 import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderCache;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.world.World;
@@ -11,8 +10,8 @@ public class ChunkBuildContext {
     public final ChunkBuildBuffers buffers;
     public final BlockRenderCache cache;
 
-    public ChunkBuildContext(World world, ChunkVertexType vertexType, BlockRenderPassManager renderPassManager) {
-        this.buffers = new ChunkBuildBuffers(vertexType, renderPassManager);
+    public ChunkBuildContext(World world, ChunkVertexType vertexType) {
+        this.buffers = new ChunkBuildBuffers(vertexType);
         this.cache = new BlockRenderCache(MinecraftClient.getInstance(), world);
     }
 
