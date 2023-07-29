8cb0fd0d9a542f91cac86e2ff9972380daf303e1
Nohaii <88097529+Noryea@users.noreply.github.com>
2023-07-30 02:24:11 +0800

    Fix light updates sometimes not marking chunks as ready (#1922)

--- src/main/java/me/jellysquid/mods/sodium/mixin/core/world/map/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/core/world/map/ClientPlayNetworkHandlerMixin.java
@@ -4,17 +4,13 @@ import me.jellysquid.mods.sodium.client.render.chunk.map.ChunkStatus;
 import me.jellysquid.mods.sodium.client.render.chunk.map.ChunkTrackerHolder;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.network.packet.s2c.play.LightData;
 import net.minecraft.network.packet.s2c.play.UnloadChunkS2CPacket;
-import net.minecraft.world.LightType;
-import net.minecraft.world.chunk.light.LightingProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.BitSet;
-import java.util.Iterator;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
@@ -22,16 +18,12 @@ public class ClientPlayNetworkHandlerMixin {
     private ClientWorld world;
 
     @Inject(
-            method = "updateLighting",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/chunk/light/LightingProvider;enqueueSectionData(Lnet/minecraft/world/LightType;Lnet/minecraft/util/math/ChunkSectionPos;Lnet/minecraft/world/chunk/ChunkNibbleArray;)V",
-                    shift = At.Shift.AFTER
-            )
+            method = "readLightData",
+            at = @At("RETURN")
     )
-    private void onLightDataReceived(int chunkX, int chunkZ, LightingProvider provider, LightType type, BitSet inited, BitSet uninited, Iterator<byte[]> nibbles, CallbackInfo ci) {
+    private void onLightDataReceived(int x, int z, LightData data, CallbackInfo ci) {
         ChunkTrackerHolder.get(this.world)
-                .onChunkStatusAdded(chunkX, chunkZ, ChunkStatus.FLAG_HAS_LIGHT_DATA);
+                .onChunkStatusAdded(x, z, ChunkStatus.FLAG_HAS_LIGHT_DATA);
     }
 
     @Inject(method = "onUnloadChunk", at = @At("RETURN"))
