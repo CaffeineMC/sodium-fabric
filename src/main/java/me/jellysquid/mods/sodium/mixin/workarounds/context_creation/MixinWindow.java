602442ac10738eb50d57a571bafdb3766d08cb86
jellysquid3 <jellysquid3@users.noreply.github.com>
2023-08-04 15:52:54 -0500

    Log information about the render device on startup
    This may be different from what was detected during early graphics adapter probing, since we can't know *which* device will be used.

--- src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/context_creation/MixinWindow.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/context_creation/MixinWindow.java
@@ -2,15 +2,27 @@ package me.jellysquid.mods.sodium.mixin.workarounds.context_creation;
 
 import me.jellysquid.mods.sodium.client.util.workarounds.Workarounds;
 import me.jellysquid.mods.sodium.client.util.workarounds.driver.nvidia.NvidiaWorkarounds;
+import net.minecraft.client.WindowEventHandler;
+import net.minecraft.client.WindowSettings;
+import net.minecraft.client.util.MonitorTracker;
 import net.minecraft.client.util.Window;
 import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL11C;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 @Mixin(Window.class)
 public class MixinWindow {
+    @Unique
+    private static final Logger sodium$LOGGER = LoggerFactory.getLogger("Sodium-EarlySetup");
+
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J"))
     private long wrapGlfwCreateWindow(int width, int height, CharSequence title, long monitor, long share) {
         if (Workarounds.isWorkaroundEnabled(Workarounds.Reference.NVIDIA_THREADED_OPTIMIZATIONS)) {
@@ -24,5 +36,12 @@ public class MixinWindow {
                 NvidiaWorkarounds.uninstall();
             }
         }
+    }
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void postWindowCreated(WindowEventHandler eventHandler, MonitorTracker monitorTracker, WindowSettings settings, String videoMode, String title, CallbackInfo ci) {
+        sodium$LOGGER.info("OpenGL Vendor: {}", GL11C.glGetString(GL11C.GL_VENDOR));
+        sodium$LOGGER.info("OpenGL Renderer: {}", GL11C.glGetString(GL11C.GL_RENDERER));
+        sodium$LOGGER.info("OpenGL Version: {}", GL11C.glGetString(GL11C.GL_VERSION));
     }
 }
