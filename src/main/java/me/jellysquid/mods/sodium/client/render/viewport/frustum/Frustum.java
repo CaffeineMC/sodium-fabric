3ea315a4140aea192bcbe71ba1677c83b1857dcf
jellysquid3 <jellysquid3@users.noreply.github.com>
2023-08-12 18:29:38 -0500

    Allow frustum implementation of viewport to be changed

---
+++ src/main/java/me/jellysquid/mods/sodium/client/render/viewport/frustum/Frustum.java
@@ -0,0 +1,5 @@
+package me.jellysquid.mods.sodium.client.render.viewport.frustum;
+
+public interface Frustum {
+    boolean testAab(float minX, float minY, float minZ, float maxX, float maxY, float maxZ);
+}
--- src/main/java/me/jellysquid/mods/sodium/mixin/core/render/frustum/FrustumMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/core/render/frustum/FrustumMixin.java
@@ -1,9 +1,11 @@
 package me.jellysquid.mods.sodium.mixin.core.render.frustum;
 
+import me.jellysquid.mods.sodium.client.render.viewport.frustum.SimpleFrustum;
 import me.jellysquid.mods.sodium.client.render.viewport.Viewport;
 import me.jellysquid.mods.sodium.client.render.viewport.ViewportProvider;
 import net.minecraft.client.render.Frustum;
 import org.joml.FrustumIntersection;
+import org.joml.Vector3d;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -25,6 +27,6 @@ public class FrustumMixin implements ViewportProvider {
 
     @Override
     public Viewport sodium$createViewport() {
-        return new Viewport(this.frustumIntersection, this.x, this.y, this.z);
+        return new Viewport(new SimpleFrustum(this.frustumIntersection), new Vector3d(this.x, this.y, this.z));
     }
 }
