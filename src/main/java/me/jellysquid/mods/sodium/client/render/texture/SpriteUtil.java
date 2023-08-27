2b7b8038b51d586229283268f6f1e9badc22a928
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-08-27 13:20:24 +0200

    Guard against marking null sprites as active. Fixes #2042

--- src/main/java/me/jellysquid/mods/sodium/client/render/texture/SpriteUtil.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/texture/SpriteUtil.java
@@ -1,9 +1,16 @@
 package me.jellysquid.mods.sodium.client.render.texture;
 
 import net.minecraft.client.texture.Sprite;
+import org.jetbrains.annotations.Nullable;
 
 public class SpriteUtil {
-    public static void markSpriteActive(Sprite sprite) {
+    public static void markSpriteActive(@Nullable Sprite sprite) {
+        if (sprite == null) {
+            // Can happen in some cases, for example if a mod passes a BakedQuad with a null sprite
+            // to a VertexConsumer that does not have a texture element.
+            return;
+        }
+
         ((SpriteContentsExtended) sprite.getContents()).sodium$setActive(true);
     }
 
