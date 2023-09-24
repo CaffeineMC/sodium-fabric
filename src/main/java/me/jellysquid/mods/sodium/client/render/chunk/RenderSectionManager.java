e2bcb9e3dbc0aaf10072c983344aedf0ef7285c0
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-09-24 16:26:14 -0400

    Fix updates submitted during initial builds not being processed

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/RenderSectionManager.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/RenderSectionManager.java
@@ -30,6 +30,7 @@ import me.jellysquid.mods.sodium.client.render.texture.SpriteUtil;
 import me.jellysquid.mods.sodium.client.render.viewport.CameraTransform;
 import me.jellysquid.mods.sodium.client.render.viewport.Viewport;
 import me.jellysquid.mods.sodium.client.util.MathUtil;
+import me.jellysquid.mods.sodium.client.util.task.CancellationToken;
 import me.jellysquid.mods.sodium.client.world.WorldSlice;
 import me.jellysquid.mods.sodium.client.world.cloned.ChunkRenderContext;
 import me.jellysquid.mods.sodium.client.world.cloned.ClonedChunkSectionCache;
@@ -438,7 +439,7 @@ public class RenderSectionManager {
 
         RenderSection section = this.sectionByPosition.get(ChunkSectionPos.asLong(x, y, z));
 
-        if (section != null && section.isBuilt()) {
+        if (section != null) {
             ChunkUpdateType pendingUpdate;
 
             if (allowImportantRebuilds() && (important || this.shouldPrioritizeRebuild(section))) {
@@ -451,6 +452,12 @@ public class RenderSectionManager {
                 section.setPendingUpdate(pendingUpdate);
 
                 this.needsUpdate = true;
+
+                CancellationToken token = section.getBuildCancellationToken();
+                if(token != null) {
+                    token.setCancelled();
+                    section.setBuildCancellationToken(null); // to allow queuing it again
+                }
             }
         }
     }
