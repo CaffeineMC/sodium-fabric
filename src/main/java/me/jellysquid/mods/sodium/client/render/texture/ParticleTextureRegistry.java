2b84a1ee7d42a44580cd5cf1e676f09e9d2e266c
wahfl2 <59855656+wahfl2@users.noreply.github.com>
2023-09-26 23:23:02 -0500

    Begin particle registry impl

---
+++ src/main/java/me/jellysquid/mods/sodium/client/render/texture/ParticleTextureRegistry.java
@@ -0,0 +1,35 @@
+package me.jellysquid.mods.sodium.client.render.texture;
+
+import it.unimi.dsi.fastutil.floats.FloatFloatImmutablePair;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class ParticleTextureRegistry {
+    private final Object2IntOpenHashMap<FloatFloatImmutablePair> uvToIndex = new Object2IntOpenHashMap<>();
+
+    private ArrayList<FloatFloatImmutablePair> toAdd = new ArrayList<>();
+
+    private int currentIndex = 0;
+
+    public int get(float u, float v) {
+        FloatFloatImmutablePair key = new FloatFloatImmutablePair(u, v);
+        return uvToIndex.computeIfAbsent(key, pairKey -> {
+            toAdd.add((FloatFloatImmutablePair) pairKey);
+            return currentIndex++;
+        });
+    }
+
+    public boolean isDirty() {
+        return !toAdd.isEmpty();
+    }
+
+    public List<FloatFloatImmutablePair> flushUpdates() {
+        var ret = this.toAdd;
+        this.toAdd = new ArrayList<>();
+        return ret;
+    }
+}
