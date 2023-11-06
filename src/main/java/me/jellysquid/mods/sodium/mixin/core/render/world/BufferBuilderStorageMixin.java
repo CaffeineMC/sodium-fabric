86b69d259692e1ca22bee0e9f34c35e59a41afc8
IMS212 <ims@irisshaders.dev>
2023-11-06 11:11:24 -0800

    Update to 23w44a

--- src/main/java/me/jellysquid/mods/sodium/mixin/core/render/world/BufferBuilderStorageMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/core/render/world/BufferBuilderStorageMixin.java
@@ -1,16 +1,16 @@
 package me.jellysquid.mods.sodium.mixin.core.render.world;
 
-import me.jellysquid.mods.sodium.client.render.chunk.NonStoringSectionPack;
-import net.minecraft.class_8901;
+import me.jellysquid.mods.sodium.client.render.chunk.NonStoringBuilderPool;
 import net.minecraft.client.render.BufferBuilderStorage;
+import net.minecraft.client.render.chunk.BlockBufferBuilderPool;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BufferBuilderStorage.class)
 public class BufferBuilderStorageMixin {
-    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/class_8901;method_54643(I)Lnet/minecraft/class_8901;"))
-    private class_8901 sodium$doNotAllocateChunks(int i) {
-        return new NonStoringSectionPack();
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/chunk/BlockBufferBuilderPool;allocate(I)Lnet/minecraft/client/render/chunk/BlockBufferBuilderPool;"))
+    private BlockBufferBuilderPool sodium$doNotAllocateChunks(int i) {
+        return new NonStoringBuilderPool();
     }
 }
