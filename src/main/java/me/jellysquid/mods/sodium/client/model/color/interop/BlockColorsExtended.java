FiniteReality/embeddium

Fall back to non-vertex-specific coloring if mods override color providers

src/main/java/me/jellysquid/mods/sodium/client/model/color/interop/BlockColorsExtended.java
@@ -1,6 +1,7 @@
 package me.jellysquid.mods.sodium.client.model.color.interop;
 
 import it.unimi.dsi.fastutil.objects.Reference2ReferenceMap;
+import it.unimi.dsi.fastutil.objects.ReferenceSet;
 import net.minecraft.block.Block;
 import net.minecraft.client.color.block.BlockColorProvider;
 import net.minecraft.client.color.block.BlockColors;
@@ -10,5 +11,11 @@ public interface BlockColorsExtended {
         return ((BlockColorsExtended) blockColors).sodium$getProviders();
     }
 
+    static ReferenceSet<Block> getOverridenVanillaBlocks(BlockColors blockColors) {
+        return ((BlockColorsExtended) blockColors).embeddium$getOverridenVanillaBlocks();
+    }
+
     Reference2ReferenceMap<Block, BlockColorProvider> sodium$getProviders();
+
+    ReferenceSet<Block> embeddium$getOverridenVanillaBlocks();
 }

src/main/java/me/jellysquid/mods/sodium/mixin/core/model/colors/BlockColorsMixin.java
@@ -1,12 +1,12 @@
 package me.jellysquid.mods.sodium.mixin.core.model.colors;
 
-import it.unimi.dsi.fastutil.objects.Reference2ReferenceMap;
-import it.unimi.dsi.fastutil.objects.Reference2ReferenceMaps;
-import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
+import it.unimi.dsi.fastutil.objects.*;
+import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.model.color.interop.BlockColorsExtended;
 import net.minecraft.block.Block;
 import net.minecraft.client.color.block.BlockColorProvider;
 import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.registry.Registries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,12 +19,22 @@ public class BlockColorsMixin implements BlockColorsExtended {
     @Unique
     private final Reference2ReferenceMap<Block, BlockColorProvider> blocksToColor = new Reference2ReferenceOpenHashMap<>();
 
+    @Unique
+    private final ReferenceSet<Block> overridenBlocks = new ReferenceOpenHashSet<>();
+
+
+
     @Inject(method = "registerColorProvider", at = @At("HEAD"))
     private void preRegisterColorProvider(BlockColorProvider provider, Block[] blocks, CallbackInfo ci) {
         // Happens with Quark. Why??
         if(provider != null) {
             for (Block block : blocks) {
-                this.blocksToColor.put(block, provider);
+                // There will be one provider already registered for vanilla blocks, if we are replacing it,
+                // it means a mod is using custom logic and we need to disable per-vertex coloring
+                if (this.blocksToColor.put(block, provider) != null) {
+                    this.overridenBlocks.add(block);
+                    SodiumClientMod.logger().info("Block {} had its color provider replaced and will not use per-vertex coloring", Registries.BLOCK.getId(block));
+                }
             }
         }
     }
@@ -32,5 +42,10 @@ public class BlockColorsMixin implements BlockColorsExtended {
     @Override
     public Reference2ReferenceMap<Block, BlockColorProvider> sodium$getProviders() {
         return Reference2ReferenceMaps.unmodifiable(this.blocksToColor);
+    }
+
+    @Override
+    public ReferenceSet<Block> embeddium$getOverridenVanillaBlocks() {
+        return ReferenceSets.unmodifiable(this.overridenBlocks);
     }
 }

CaffeineMC/sodium-fabric

src/main/java/me/jellysquid/mods/sodium/client/model/color/interop/BlockColorsExtended.java
@@ -1,6 +1,7 @@
 package me.jellysquid.mods.sodium.client.model.color.interop;
 
 import it.unimi.dsi.fastutil.objects.Reference2ReferenceMap;
+import it.unimi.dsi.fastutil.objects.ReferenceSet;
 import net.minecraft.block.Block;
 import net.minecraft.client.color.block.BlockColorProvider;
 import net.minecraft.client.color.block.BlockColors;
@@ -10,5 +11,11 @@ public interface BlockColorsExtended {
         return ((BlockColorsExtended) blockColors).sodium$getProviders();
     }
 
+    static ReferenceSet<Block> getOverridenVanillaBlocks(BlockColors blockColors) {
+        return ((BlockColorsExtended) blockColors).sodium$getOverridenVanillaBlocks();
+    }
+
     Reference2ReferenceMap<Block, BlockColorProvider> sodium$getProviders();
+
+    ReferenceSet<Block> sodium$getOverridenVanillaBlocks();
 }

src/main/java/me/jellysquid/mods/sodium/mixin/core/model/colors/BlockColorsMixin.java
@@ -1,12 +1,12 @@
 package me.jellysquid.mods.sodium.mixin.core.model.colors;
 
-import it.unimi.dsi.fastutil.objects.Reference2ReferenceMap;
-import it.unimi.dsi.fastutil.objects.Reference2ReferenceMaps;
-import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
+import it.unimi.dsi.fastutil.objects.*;
+import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.model.color.interop.BlockColorsExtended;
 import net.minecraft.block.Block;
 import net.minecraft.client.color.block.BlockColorProvider;
 import net.minecraft.client.color.block.BlockColors;
+import net.minecraft.registry.Registries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,15 +19,30 @@ public class BlockColorsMixin implements BlockColorsExtended {
     @Unique
     private final Reference2ReferenceMap<Block, BlockColorProvider> blocksToColor = new Reference2ReferenceOpenHashMap<>();
 
+    @Unique
+    private final ReferenceSet<Block> overridenBlocks = new ReferenceOpenHashSet<>();
+
+
+
     @Inject(method = "registerColorProvider", at = @At("HEAD"))
     private void preRegisterColorProvider(BlockColorProvider provider, Block[] blocks, CallbackInfo ci) {
         for (Block block : blocks) {
-            this.blocksToColor.put(block, provider);
+            // There will be one provider already registered for vanilla blocks, if we are replacing it,
+            // it means a mod is using custom logic and we need to disable per-vertex coloring
+            if (this.blocksToColor.put(block, provider) != null) {
+                this.overridenBlocks.add(block);
+                SodiumClientMod.logger().info("Block {} had its color provider replaced and will not use per-vertex coloring", Registries.BLOCK.getId(block));
+            }
         }
     }
 
     @Override
     public Reference2ReferenceMap<Block, BlockColorProvider> sodium$getProviders() {
         return Reference2ReferenceMaps.unmodifiable(this.blocksToColor);
+    }
+
+    @Override
+    public ReferenceSet<Block> sodium$getOverridenVanillaBlocks() {
+        return ReferenceSets.unmodifiable(this.overridenBlocks);
     }
 }
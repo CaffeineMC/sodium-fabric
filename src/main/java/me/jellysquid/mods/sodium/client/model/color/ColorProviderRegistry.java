1116afb20e764d32e393c4a07ac0fafea9b1a83c
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-11-08 10:51:02 -0500

    Fall back to non-vertex-specific coloring if mods override color providers

--- src/main/java/me/jellysquid/mods/sodium/client/model/color/ColorProviderRegistry.java
+++ src/main/java/me/jellysquid/mods/sodium/client/model/color/ColorProviderRegistry.java
@@ -2,6 +2,7 @@ package me.jellysquid.mods.sodium.client.model.color;
 
 import it.unimi.dsi.fastutil.objects.Reference2ReferenceMap;
 import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
+import it.unimi.dsi.fastutil.objects.ReferenceSet;
 import me.jellysquid.mods.sodium.client.model.color.interop.BlockColorsExtended;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -17,12 +18,16 @@ public class ColorProviderRegistry {
     private final Reference2ReferenceMap<Block, ColorProvider<BlockState>> blocks = new Reference2ReferenceOpenHashMap<>();
     private final Reference2ReferenceMap<Fluid, ColorProvider<FluidState>> fluids = new Reference2ReferenceOpenHashMap<>();
 
+    private final ReferenceSet<Block> overridenBlocks;
+
     public ColorProviderRegistry(BlockColors blockColors) {
         var providers = BlockColorsExtended.getProviders(blockColors);
 
         for (var entry : providers.reference2ReferenceEntrySet()) {
             this.blocks.put(entry.getKey(), DefaultColorProviders.adapt(entry.getValue()));
         }
+
+        this.overridenBlocks = BlockColorsExtended.getOverridenVanillaBlocks(blockColors);
 
         this.installOverrides();
     }
@@ -46,6 +51,9 @@ public class ColorProviderRegistry {
 
     private void registerBlocks(ColorProvider<BlockState> resolver, Block... blocks) {
         for (var block : blocks) {
+            // Do not register our resolver if the block is overriden
+            if (this.overridenBlocks.contains(block))
+                continue;
             this.blocks.put(block, resolver);
         }
     }
