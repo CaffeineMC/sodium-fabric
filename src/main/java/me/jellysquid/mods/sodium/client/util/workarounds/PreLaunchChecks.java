0d343afc74ffcdfd370abf94155e05f87e8b89be
JellySquid <jellysquid@pm.me>
2023-11-12 13:20:13 -0600

    Try to detect RivaTuner and crash the game early
    Fixes #2048

--- src/main/java/me/jellysquid/mods/sodium/client/util/workarounds/PreLaunchChecks.java
+++ src/main/java/me/jellysquid/mods/sodium/client/util/workarounds/PreLaunchChecks.java
@@ -11,25 +11,19 @@ public class PreLaunchChecks {
     private static final Logger LOGGER = LoggerFactory.getLogger("Sodium-PrelaunchChecks");
 
     public static void checkDrivers() {
-        boolean check = Boolean.parseBoolean(System.getProperty("sodium.driver.check", "true"));
-
-        if (!check) {
-            return;
-        }
-
-        if (isBrokenIntelGen7GraphicsDriver()) {
+        if (Boolean.parseBoolean(System.getProperty("sodium.checks.win32.intelGen7", "true")) && isBrokenIntelGen7GraphicsDriver()) {
             LOGGER.error("------------------------------------------------------------------------------------------------------------");
             LOGGER.error("READ ME! You appear to be using an Intel graphics card with unsupported drivers!");
             LOGGER.error("  * Certain graphics cards (such as the Intel HD 2500/4000) currently ship with broken graphics drivers " +
                     "through Windows Update.");
             LOGGER.error("  * You need to update your graphics drivers to fix this problem. More instructions can be found here:");
-            LOGGER.error("     https://github.com/CaffeineMC/sodium-fabric/issues/899");
+            LOGGER.error("    https://github.com/CaffeineMC/sodium-fabric/issues/899");
             LOGGER.error("  * HINT: You cannot use Windows Update or the Intel Graphics Control Panel to install the updated graphics " +
                     "driver, as they incorrectly report that the driver is 'already up to date'.");
 
             LOGGER.error("  * HINT: If you believe this is an error, then you can force the game to start anyways by adding the " +
                     "following JVM argument.");
-            LOGGER.error("      -Dsodium.driver.check=false");
+            LOGGER.error("      -Dsodium.checks.win32.intelGen7" + "=false");
             LOGGER.error("  * NOTE: We will not provide support for any issues caused by using this option. You are on your own!");
             LOGGER.error("------------------------------------------------------------------------------------------------------------");
 
