a3e5825fccd53bb69705f1d705d9ad82f1a80bcc
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-11-12 15:54:21 -0500

    Remove deprecated constructor

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/shader/ChunkShaderOptions.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/shader/ChunkShaderOptions.java
@@ -2,19 +2,9 @@ package me.jellysquid.mods.sodium.client.render.chunk.shader;
 
 import me.jellysquid.mods.sodium.client.gl.shader.ShaderConstants;
 import me.jellysquid.mods.sodium.client.render.chunk.terrain.TerrainRenderPass;
-import me.jellysquid.mods.sodium.client.render.chunk.vertex.format.ChunkMeshFormats;
 import me.jellysquid.mods.sodium.client.render.chunk.vertex.format.ChunkVertexType;
 
 public record ChunkShaderOptions(ChunkFogMode fog, TerrainRenderPass pass, ChunkVertexType type) {
-    /**
-     * @deprecated Only kept for Iris/Oculus compatibility, do not use
-     */
-    @Deprecated
-    @SuppressWarnings("unused")
-    public ChunkShaderOptions(ChunkFogMode fog, TerrainRenderPass pass) {
-        this(fog, pass, ChunkMeshFormats.COMPACT);
-    }
-
     public ShaderConstants constants() {
         ShaderConstants.Builder constants = ShaderConstants.builder();
         constants.addAll(this.fog.getDefines());
