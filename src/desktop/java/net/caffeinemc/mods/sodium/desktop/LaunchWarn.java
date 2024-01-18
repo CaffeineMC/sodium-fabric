9f4b3a6206a39478b0142358901d6e4b65e953a8
Fury_Phoenix <64714532+Phoenix-Starlight@users.noreply.github.com>
2024-01-18 09:08:32 -0800

    Forcibly constrain text, add taskbar presence and icon

--- src/desktop/java/net/caffeinemc/mods/sodium/desktop/LaunchWarn.java
+++ src/desktop/java/net/caffeinemc/mods/sodium/desktop/LaunchWarn.java
@@ -1,6 +1,7 @@
 package net.caffeinemc.mods.sodium.desktop;
 
 import javax.swing.ImageIcon;
+import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -15,13 +16,17 @@ import java.net.URI;
  * and modified to fit Sodium. See Iris' license for more information.
  */
 public class LaunchWarn {
+    private static final String html_message = "<html><body><p style='width:600px;'>You have tried to launch Sodium (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install Fabric Loader for Minecraft, and place this file in your mods directory instead." +
+    "\nIf this is your first time installing mods for Fabric Loader, click \"Help\" for a guide on how to do this.</p></body></html>";
+    private static final String html_fallback = "<html><body><p style='width:600px;'>You have tried to launch Sodium (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install Fabric Loader for Minecraft, and place this file in your mods directory instead." +
+    "\nIf this is your first time installing mods for Fabric Loader, open \"https://github.com/CaffeineMC/sodium-fabric/wiki/Installation\" for a guide on how to do this.</p></body></html>";
+    private static final String fallback = "You have tried to launch Sodium (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install Fabric Loader for Minecraft, and place this file in your mods directory instead." +
+    "\nIf this is your first time installing mods for Fabric Loader, open \"https://github.com/CaffeineMC/sodium-fabric/wiki/Installation\" for a guide on how to do this.";
+
     public static void main(String[] args) {
-        String message = "<html><body><p style='width:600px;'>You have tried to launch Sodium (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install Fabric Loader for Minecraft, and place this file in your mods directory instead." +
-        "\nIf this is your first time installing mods for Fabric Loader, click \"Help\" for a guide on how to do this.</p></body></html>";
-        String fallback = "<html><body><p style='width:600px;'>You have tried to launch Sodium (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install Fabric Loader for Minecraft, and place this file in your mods directory instead." +
-        "\nIf this is your first time installing mods for Fabric Loader, open \"https://github.com/CaffeineMC/sodium-fabric/wiki/Installation\" for a guide on how to do this.</p></body></html>";
+
         if (GraphicsEnvironment.isHeadless()) {
-            System.err.println(fallback);
+            System.err.println(LaunchWarn.fallback);
         } else {
             try {
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -32,7 +37,7 @@ public class LaunchWarn {
             ImageIcon icon = new ImageIcon(LaunchWarn.class.getResource("/assets/sodium/icon.png"), "Sodium");
 
             if (Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-                int option = JOptionPane.showOptionDialog(null, message, "Sodium", JOptionPane.YES_NO_OPTION,
+                int option = LaunchWarn.createOptionPane(LaunchWarn.html_message, "Sodium", JOptionPane.YES_NO_OPTION,
                         JOptionPane.INFORMATION_MESSAGE, icon, new Object[] { "Help", "Cancel" }, JOptionPane.YES_OPTION);
 
                 if (option == JOptionPane.YES_OPTION) {
@@ -44,10 +49,39 @@ public class LaunchWarn {
                 }
             } else {
                 // Fallback for Linux, etc users with no "default" browser
-                JOptionPane.showMessageDialog(null, fallback, "Sodium", JOptionPane.INFORMATION_MESSAGE, icon);
+                LaunchWarn.createOptionPane(LaunchWarn.html_fallback, "Sodium", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, icon, null, null);
             }
         }
 
         System.exit(0);
+    }
+
+    private static int createOptionPane(String msg,
+                                        String title,
+                                        int messageType,
+                                        int optionType,
+                                        ImageIcon icon,
+                                        Object[] options,
+                                        Object initialValue) {
+        JOptionPane pane = new JOptionPane(msg, messageType, optionType, icon, options, initialValue);
+        JDialog dialog = pane.createDialog(title);
+        dialog.setIconImage(icon.getImage());
+        dialog.setVisible(true);
+        Object selectedValue = pane.getValue();
+        if(selectedValue == null)
+            return JOptionPane.CLOSED_OPTION;
+        //If there is not an array of option buttons:
+        if(options == null) {
+            if(selectedValue instanceof Integer)
+                return ((Integer)selectedValue).intValue();
+            return JOptionPane.CLOSED_OPTION;
+        }
+        //If there is an array of option buttons:
+        for(int counter = 0, maxCounter = options.length;
+            counter < maxCounter; counter++) {
+            if(options[counter].equals(selectedValue))
+            return counter;
+        }
+        return JOptionPane.CLOSED_OPTION;
     }
 }
