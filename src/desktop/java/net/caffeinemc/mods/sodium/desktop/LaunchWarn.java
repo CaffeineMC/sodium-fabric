53c4cf10a85200a42290a4d194bc63a780c43b6d
Fury_Phoenix <64714532+Phoenix-Starlight@users.noreply.github.com>
2024-01-19 17:10:23 -0800

    CAPITALIZE CONSTANTS, hopefully fix HTML formatted message

--- src/desktop/java/net/caffeinemc/mods/sodium/desktop/LaunchWarn.java
+++ src/desktop/java/net/caffeinemc/mods/sodium/desktop/LaunchWarn.java
@@ -16,17 +16,17 @@ import java.net.URI;
  * and modified to fit Sodium. See Iris' license for more information.
  */
 public class LaunchWarn {
-    private static final String html_message = "<html><body><p style='width:600px;'>You have tried to launch Sodium (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install Fabric Loader for Minecraft, and place this file in your mods directory instead." +
-    "\nIf this is your first time installing mods for Fabric Loader, click \"Help\" for a guide on how to do this.</p></body></html>";
-    private static final String html_fallback = "<html><body><p style='width:600px;'>You have tried to launch Sodium (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install Fabric Loader for Minecraft, and place this file in your mods directory instead." +
-    "\nIf this is your first time installing mods for Fabric Loader, open \"https://github.com/CaffeineMC/sodium-fabric/wiki/Installation\" for a guide on how to do this.</p></body></html>";
-    private static final String fallback = "You have tried to launch Sodium (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install Fabric Loader for Minecraft, and place this file in your mods directory instead." +
+    private static final String HTML_MESSAGE = "<html><body><p style='width:600px;'>You have tried to launch Sodium (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install Fabric Loader for Minecraft, and place this file in your mods directory instead." +
+    "<br>If this is your first time installing mods for Fabric Loader, click \"Help\" for a guide on how to do this.</p></body></html>";
+    private static final String HTML_FALLBACK = "<html><body><p style='width:600px;'>You have tried to launch Sodium (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install Fabric Loader for Minecraft, and place this file in your mods directory instead." +
+    "<br>If this is your first time installing mods for Fabric Loader, open \"https://github.com/CaffeineMC/sodium-fabric/wiki/Installation\" for a guide on how to do this.</p></body></html>";
+    private static final String FALLBACK = "You have tried to launch Sodium (a Minecraft mod) directly, but it is not an executable program or mod installer. You must install Fabric Loader for Minecraft, and place this file in your mods directory instead." +
     "\nIf this is your first time installing mods for Fabric Loader, open \"https://github.com/CaffeineMC/sodium-fabric/wiki/Installation\" for a guide on how to do this.";
 
     public static void main(String[] args) {
 
         if (GraphicsEnvironment.isHeadless()) {
-            System.err.println(LaunchWarn.fallback);
+            System.err.println(LaunchWarn.FALLBACK);
         } else {
             try {
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -37,7 +37,7 @@ public class LaunchWarn {
             ImageIcon icon = new ImageIcon(LaunchWarn.class.getResource("/assets/sodium/icon.png"), "Sodium");
 
             if (Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-                int option = LaunchWarn.createOptionPane(LaunchWarn.html_message, "Sodium", JOptionPane.YES_NO_OPTION,
+                int option = LaunchWarn.createOptionPane(LaunchWarn.HTML_MESSAGE, "Sodium", JOptionPane.YES_NO_OPTION,
                         JOptionPane.INFORMATION_MESSAGE, icon, new Object[] { "Help", "Cancel" }, JOptionPane.YES_OPTION);
 
                 if (option == JOptionPane.YES_OPTION) {
@@ -49,7 +49,7 @@ public class LaunchWarn {
                 }
             } else {
                 // Fallback for Linux, etc users with no "default" browser
-                LaunchWarn.createOptionPane(LaunchWarn.html_fallback, "Sodium", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, icon, null, null);
+                LaunchWarn.createOptionPane(LaunchWarn.HTML_FALLBACK, "Sodium", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, icon, null, null);
             }
         }
 
