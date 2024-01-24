04a39792cd0a861b0762650fe796c21630201d5a
douira <douira100@gmail.com>
2024-01-24 23:20:39 +0100

    remove unused field in sort type enum

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/translucent_sorting/SortType.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/translucent_sorting/SortType.java
@@ -8,17 +8,17 @@ public enum SortType {
     /**
      * The section is fully empty, no index buffer is needed.
      */
-    EMPTY_SECTION(false, false),
+    EMPTY_SECTION(false),
 
     /**
      * The section has no translucent geometry, no index buffer is needed.
      */
-    NO_TRANSLUCENT(false, false),
+    NO_TRANSLUCENT(false),
 
     /**
      * No sorting is required and the sort order doesn't matter.
      */
-    NONE(false, false),
+    NONE(false),
 
     /**
      * There is only one sort order. No active sorting is required, but an initial
@@ -29,28 +29,24 @@ public enum SortType {
      * changes, remove this note and adjust StaticTranslucentData and anything that
      * reads from it to handle UNASSIGNED quads.
      */
-    STATIC_NORMAL_RELATIVE(true, false),
+    STATIC_NORMAL_RELATIVE(false),
 
     /**
      * There is only one sort order and not active sorting is required, but
      * determining the static sort order involves doing a toplogical sort of the
      * quads.
      */
-    STATIC_TOPO(true, true),
+    STATIC_TOPO(true),
 
     /**
      * There are multiple sort orders. Sorting is required every time GFNI triggers
      * this section.
      */
-    DYNAMIC(true, true);
+    DYNAMIC(true);
 
-    public final boolean needsIndexBuffer;
     public final boolean needsDirectionMixing;
 
-    private SortType(
-            boolean needsIndexBuffer,
-            boolean needsDirectionMixing) {
-        this.needsIndexBuffer = needsIndexBuffer;
+    private SortType(boolean needsDirectionMixing) {
         this.needsDirectionMixing = needsDirectionMixing;
     }
 }
