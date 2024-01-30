01aae0b230e337133136932c4ece0ab3e7347a2e
Julienraptor01 <julienraptor01@gmail.com>
2024-01-31 00:34:53 +0100

    Fix Value 'clicked' is always 'true'
    i saw it and then when IDEA finished loading, it highlighted it :)

--- src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumOptionsGUI.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumOptionsGUI.java
@@ -395,10 +395,9 @@ public class SodiumOptionsGUI extends Screen implements ScreenPromptable {
 
         if (!clicked) {
             this.setFocused(null);
-            return true;
         }
 
-        return clicked;
+        return true;
     }
 
     @Override
