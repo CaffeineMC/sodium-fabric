6ea0b63f2843ce57dc1086d79fd21dc708e06d11
Lolothepro <68275908+Lolothepro@users.noreply.github.com>
2024-03-09 16:08:51 +0100

    bump Fabric API

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ object Constants {
     // https://fabricmc.net/develop/
     const val MINECRAFT_VERSION: String = "1.20.4"
     const val FABRIC_LOADER_VERSION: String = "0.15.6"
-    const val FABRIC_API_VERSION: String = "0.96.0+1.20.4"
+    const val FABRIC_API_VERSION: String = "0.96.4+1.20.4"
 
     // https://semver.org/
     const val MOD_VERSION: String = "0.6.0"
