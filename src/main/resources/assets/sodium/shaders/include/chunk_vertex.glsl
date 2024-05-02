e944f9cf9c28921d75956bfd30cf0a8c4517a00f
JellySquid <jellysquid@pm.me>
2024-05-01 21:30:49 -0500

    Use more efficient unpacking in vertex shader

--- src/main/resources/assets/sodium/shaders/include/chunk_vertex.glsl
+++ src/main/resources/assets/sodium/shaders/include/chunk_vertex.glsl
@@ -17,48 +17,53 @@ uint _draw_id;
 uint _material_params;
 
 #ifdef USE_VERTEX_COMPRESSION
-const uint POSITION_BITS        = 20u;
-const uint POSITION_MAX_COORD   = 1u << POSITION_BITS;
-const uint POSITION_MAX_VALUE   = POSITION_MAX_COORD - 1u;
-
-const uint TEXTURE_BITS         = 15u;
-const uint TEXTURE_MAX_COORD    = 1u << TEXTURE_BITS;
-const uint TEXTURE_MAX_VALUE    = TEXTURE_MAX_COORD - 1u;
+const float POSITION_MAX_COORD   = 1 << 20;
+const float TEXTURE_MAX_COORD    = 1 << 15;
+const float LIGHT_MAX_COORD      = 1 << 8;
 
 const float VERTEX_SCALE = 32.0 / POSITION_MAX_COORD;
 const float VERTEX_OFFSET = -8.0;
 
 // The amount of inset the texture coordinates from the edges of the texture, to avoid texture bleeding
-const float TEXTURE_FUZZ_AMOUNT = 1.0 / 64.0;
-const float TEXTURE_GROW_FACTOR = (1.0 - TEXTURE_FUZZ_AMOUNT) / TEXTURE_MAX_COORD;
+const float TEXTURE_BIAS_VALUE = (1.0 - (1.0 / 64.0)) / TEXTURE_MAX_COORD;
 
-in uint a_PositionHi;
-in uint a_PositionLo;
-in vec4 a_Color;
-in uvec2 a_TexCoord;
-in uvec4 a_LightAndData;
+in vec3 a_PositionHi;       // 3x Unsigned 10-bit integer
+in vec3 a_PositionLo;       // ...
+in vec4 a_Color;            // 4x Unsigned 8-bit integer (normalized)
+in vec2 a_TexCoord;         // 2x Signed 16-bit integer
+in uvec4 a_LightAndData;    // 4x Unsigned 8-bit integer
 
-uvec3 _deinterleave_u20x3(uint packed_hi, uint packed_lo) {
-    uvec3 hi = (uvec3(packed_hi) >> uvec3(0u, 10u, 20u)) & 0x3FFu;
-    uvec3 lo = (uvec3(packed_lo) >> uvec3(0u, 10u, 20u)) & 0x3FFu;
+vec3 _decode_position(vec3 hi, vec3 lo) {
+    // The 2.10.10.10 vertex formats do not support being interpreted as integer data within the shader.
+    // Because of this, we need to emulate the bitwise ops with floating-point arithmetic. There is probably no
+    // performance penalty to doing this (other than making things uglier) since GPUs typically have the same
+    // throughput for Fp32 mul/add and Int32 shl/or operations.
 
-    return (hi << 10u) | lo;
+    vec3 interleaved = (hi * (1 << 10)) + lo; // (hi << 10) | lo
+    vec3 normalized = (interleaved * VERTEX_SCALE) + VERTEX_OFFSET;
+
+    return normalized;
 }
 
-vec2 _get_texcoord() {
-    return vec2(a_TexCoord & TEXTURE_MAX_VALUE) / float(TEXTURE_MAX_COORD);
+vec2 _decode_texcoord(vec2 value) {
+    // Magnitude is within range (0, 32768)
+    // Sign bit encodes bias direction
+    vec2 texcoord = abs(value) / TEXTURE_MAX_COORD;
+    vec2 bias = sign(value) * TEXTURE_BIAS_VALUE;
+
+    return texcoord - bias;
 }
 
-vec2 _get_texcoord_bias() {
-    return mix(vec2(-TEXTURE_GROW_FACTOR), vec2(TEXTURE_GROW_FACTOR), bvec2(a_TexCoord >> TEXTURE_BITS));
+vec2 _decode_light(uvec2 value) {
+    return vec2(value) / LIGHT_MAX_COORD;
 }
 
 void _vert_init() {
-    _vert_position = ((_deinterleave_u20x3(a_PositionHi, a_PositionLo) * VERTEX_SCALE) + VERTEX_OFFSET);
+    _vert_position = _decode_position(a_PositionHi, a_PositionLo);
     _vert_color = a_Color;
-    _vert_tex_diffuse_coord = _get_texcoord() + _get_texcoord_bias();
+    _vert_tex_diffuse_coord = _decode_texcoord(a_TexCoord);
 
-    _vert_tex_light_coord = vec2(a_LightAndData.xy) / vec2(256.0);
+    _vert_tex_light_coord = _decode_light(a_LightAndData.xy);
 
     _material_params = a_LightAndData[2];
     _draw_id = a_LightAndData[3];
