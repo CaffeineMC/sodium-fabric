CaffeineMC/sodium-fabric

commit bc38d5638d391d3dfd1a928f6c7981116a7510f7

Fix fluid rendering occlusion check
Thanks cortex and moul

common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/compile/pipeline/DefaultFluidRenderer.java
@@ -71,14 +71,12 @@ public class DefaultFluidRenderer {
     }
 
     private boolean isFluidOccluded(BlockAndTintGetter world, int x, int y, int z, Direction dir, Fluid fluid) {
-        BlockPos pos = this.scratchPos.set(x, y, z);
-        BlockState blockState = world.getBlockState(pos);
-        BlockPos adjPos = this.scratchPos.set(x + dir.getStepX(), y + dir.getStepY(), z + dir.getStepZ());
-
-        if (blockState.canOcclude()) {
-            return world.getFluidState(adjPos).getType().isSame(fluid) || blockState.isFaceSturdy(world, pos, dir, SupportType.FULL);
+        var adjPos = this.scratchPos.set(x + dir.getStepX(), y + dir.getStepY(), z + dir.getStepZ());
+        BlockState blockState = world.getBlockState(adjPos);
+        if (blockState.getFluidState().getType().isSame(fluid)) {
+            return true;
         }
-        return world.getFluidState(adjPos).getType().isSame(fluid);
+        return blockState.isFaceSturdy(world, adjPos, dir.getOpposite(), SupportType.FULL);
     }
 
     private boolean isSideExposed(BlockAndTintGetter world, int x, int y, int z, Direction dir, float height) {
@@ -89,10 +87,7 @@ public class DefaultFluidRenderer {
             VoxelShape shape = blockState.getOcclusionShape(world, pos);
 
             // Hoist these checks to avoid allocating the shape below
-            if (shape == Shapes.block()) {
-                // The top face always be inset, so if the shape above is a full cube it can't possibly occlude
-                return dir == Direction.UP;
-            } else if (shape.isEmpty()) {
+            if (shape.isEmpty()) {
                 return true;
             }
 

commit c3f0524e0e4dbb7dda24cdd5f8aaf7c98a617579

Bugfix: fluid-rendered would occlude water behing translucent blocks
This bug was introduced in bc38d5638d391d3dfd1a928f6c7981116a7510f7

common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/compile/pipeline/DefaultFluidRenderer.java
@@ -76,7 +76,7 @@ public class DefaultFluidRenderer {
         if (blockState.getFluidState().getType().isSame(fluid)) {
             return true;
         }
-        return blockState.isFaceSturdy(world, adjPos, dir.getOpposite(), SupportType.FULL);
+        return blockState.canOcclude() && blockState.isFaceSturdy(world, adjPos, dir.getOpposite(), SupportType.FULL);
     }
 
     private boolean isSideExposed(BlockAndTintGetter world, int x, int y, int z, Direction dir, float height) {