ca89a5441a9e0876b616044efbbeaca92ee9d2f0
IMS212 <ims@irisshaders.dev>
2024-06-06 21:03:37 -0700

    Fix billboard particle rendering

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/render/particle/BillboardParticleMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/render/particle/BillboardParticleMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import org.joml.Quaternionf;
+import org.joml.Vector3f;
 import org.lwjgl.system.MemoryStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -34,8 +35,11 @@ public abstract class BillboardParticleMixin extends Particle {
     @Shadow
     protected abstract float getMaxV();
 
-    protected BillboardParticleMixin(ClientWorld world, double x, double y, double z) {
-        super(world, x, y, z);
+    @Unique
+    private Vector3f transferVector = new Vector3f();
+
+    protected BillboardParticleMixin(ClientWorld level, double x, double y, double z) {
+        super(level, x, y, z);
     }
 
     /**
@@ -43,95 +47,45 @@ public abstract class BillboardParticleMixin extends Particle {
      * @author JellySquid
      */
     @Overwrite
-    public void buildGeometry(VertexConsumer vertexConsumer, Camera camera, float tickDelta) {
-        Vec3d vec3d = camera.getPos();
-
-        float x = (float) (MathHelper.lerp(tickDelta, this.prevPosX, this.x) - vec3d.getX());
-        float y = (float) (MathHelper.lerp(tickDelta, this.prevPosY, this.y) - vec3d.getY());
-        float z = (float) (MathHelper.lerp(tickDelta, this.prevPosZ, this.z) - vec3d.getZ());
-
-        Quaternionf quaternion;
-
-        if (this.angle == 0.0F) {
-            quaternion = camera.getRotation();
-        } else {
-            float angle = MathHelper.lerp(tickDelta, this.prevAngle, this.angle);
-
-            quaternion = new Quaternionf(camera.getRotation());
-            quaternion.rotateZ(angle);
-        }
-
+    protected void method_60374(VertexConsumer vertexConsumer, Quaternionf quaternionf, float x, float y, float z, float tickDelta) {
         float size = this.getSize(tickDelta);
-        int light = this.getBrightness(tickDelta);
-
         float minU = this.getMinU();
         float maxU = this.getMaxU();
         float minV = this.getMinV();
         float maxV = this.getMaxV();
-
-        int color = ColorABGR.pack(this.red , this.green, this.blue, this.alpha);
+        int light = this.getBrightness(tickDelta);
 
         var writer = VertexBufferWriter.of(vertexConsumer);
+
+        int color = ColorABGR.pack(this.red, this.green, this.blue, this.alpha);
 
         try (MemoryStack stack = MemoryStack.stackPush()) {
             long buffer = stack.nmalloc(4 * ParticleVertex.STRIDE);
             long ptr = buffer;
 
-            writeVertex(ptr, quaternion,-1.0F, -1.0F, x, y, z, maxU, maxV, color, light, size);
+            this.writeVertex(ptr, quaternionf, x, y, z, 1.0F, -1.0F, size, maxU, maxV, color, light);
             ptr += ParticleVertex.STRIDE;
 
-            writeVertex(ptr, quaternion,-1.0F, 1.0F, x, y, z, maxU, minV, color, light, size);
+            this.writeVertex(ptr, quaternionf, x, y, z, 1.0F, 1.0F, size, maxU, minV, color, light);
             ptr += ParticleVertex.STRIDE;
 
-            writeVertex(ptr, quaternion,1.0F, 1.0F, x, y, z, minU, minV, color, light, size);
+            this.writeVertex(ptr, quaternionf, x, y, z, -1.0F, 1.0F, size, minU, minV, color, light);
             ptr += ParticleVertex.STRIDE;
 
-            writeVertex(ptr, quaternion,1.0F, -1.0F, x, y, z, minU, maxV, color, light, size);
+            this.writeVertex(ptr, quaternionf, x, y, z, -1.0F, -1.0F, size, minU, maxV, color, light);
             ptr += ParticleVertex.STRIDE;
 
             writer.push(stack, buffer, 4, ParticleVertex.FORMAT);
         }
-
     }
 
     @Unique
-    @SuppressWarnings("UnnecessaryLocalVariable")
-    private static void writeVertex(long buffer,
-                                    Quaternionf rotation,
-                                    float posX, float posY,
-                                    float originX, float originY, float originZ,
-                                    float u, float v, int color, int light, float size) {
-        // Quaternion q0 = new Quaternion(rotation);
-        float q0x = rotation.x();
-        float q0y = rotation.y();
-        float q0z = rotation.z();
-        float q0w = rotation.w();
+    private void writeVertex(long ptr, Quaternionf quaternionf, float originX, float originY, float originZ, float posX, float posY, float size, float u, float v, int color, int light) {
+        transferVector.set(posX, posY, 0.0f);
+        transferVector.rotate(quaternionf);
+        transferVector.mul(size);
+        transferVector.add(originX, originY, originZ);
 
-        // q0.hamiltonProduct(x, y, 0.0f, 0.0f)
-        float q1x = (q0w * posX) - (q0z * posY);
-        float q1y = (q0w * posY) + (q0z * posX);
-        float q1w = (q0x * posY) - (q0y * posX);
-        float q1z = -(q0x * posX) - (q0y * posY);
-
-        // Quaternion q2 = new Quaternion(rotation);
-        // q2.conjugate()
-        float q2x = -q0x;
-        float q2y = -q0y;
-        float q2z = -q0z;
-        float q2w = q0w;
-
-        // q2.hamiltonProduct(q1)
-        float q3x = q1z * q2x + q1x * q2w + q1y * q2z - q1w * q2y;
-        float q3y = q1z * q2y - q1x * q2z + q1y * q2w + q1w * q2x;
-        float q3z = q1z * q2z + q1x * q2y - q1y * q2x + q1w * q2w;
-
-        // Vector3f f = new Vector3f(q2.getX(), q2.getY(), q2.getZ())
-        // f.multiply(size)
-        // f.add(pos)
-        float fx = (q3x * size) + originX;
-        float fy = (q3y * size) + originY;
-        float fz = (q3z * size) + originZ;
-
-        ParticleVertex.put(buffer, fx, fy, fz, u, v, color, light);
+        ParticleVertex.put(ptr, transferVector.x(), transferVector.y(), transferVector.z(), u, v, color, light);
     }
 }
