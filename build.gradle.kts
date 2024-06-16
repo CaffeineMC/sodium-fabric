d9b3aa06f8631ae9ef148e6c13741e5d68ea029b
IMS212 <ims@irisshaders.dev>
2024-06-15 18:43:03 -0700

    Make sure build is done with Java 17 on 1.20.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,8 +34,8 @@ loom {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 sourceSets {
