7544573565605ecc7ed0f838e4c3259caa3107fd
mcrcortex <{ID}+{username}@users.noreply.github.com>
2024-07-24 23:45:17 +1000

    Fix for #2586

--- src/main/java/net/caffeinemc/mods/sodium/mixin/core/world/map/ClientLevelMixin.java
+++ src/main/java/net/caffeinemc/mods/sodium/mixin/core/world/map/ClientLevelMixin.java
@@ -1,10 +1,15 @@
 package net.caffeinemc.mods.sodium.mixin.core.world.map;
 
+import net.caffeinemc.mods.sodium.client.render.chunk.map.ChunkStatus;
 import net.caffeinemc.mods.sodium.client.render.chunk.map.ChunkTracker;
 import net.caffeinemc.mods.sodium.client.render.chunk.map.ChunkTrackerHolder;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.world.level.chunk.LevelChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Objects;
 
@@ -17,5 +22,11 @@ public class ClientLevelMixin implements ChunkTrackerHolder {
     @Override
     public ChunkTracker sodium$getTracker() {
         return Objects.requireNonNull(this.chunkTracker);
+    }
+
+    @Inject(method = "unload", at = @At("HEAD"))
+    private void sodium$trackChunkUnload(LevelChunk levelChunk, CallbackInfo ci) {
+        var pos = levelChunk.getPos();
+        this.chunkTracker.onChunkStatusRemoved(pos.x, pos.z, ChunkStatus.FLAG_ALL);
     }
 }
