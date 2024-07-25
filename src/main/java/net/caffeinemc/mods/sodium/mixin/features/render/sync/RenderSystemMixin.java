32fc495d2653151e38a8be669d9bac6a0de69828
theyareonit <kotrigmd@gmail.com>
2024-07-25 02:14:40 +0200

    change sync algorithm

--- src/main/java/net/caffeinemc/mods/sodium/mixin/features/render/sync/RenderSystemMixin.java
+++ src/main/java/net/caffeinemc/mods/sodium/mixin/features/render/sync/RenderSystemMixin.java
@@ -4,14 +4,9 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = RenderSystem.class, remap = false)
 public class RenderSystemMixin {
-
-    @Shadow
-    private static double lastDrawTime = Double.MIN_VALUE;
-
     /**
      * @author theyareonit
      * @reason Improve frame synchronization
@@ -19,17 +14,19 @@ public class RenderSystemMixin {
     @Overwrite
     public static void limitDisplayFPS(int fps) {
         double frametime = 1.0 / fps;
-        double target =  lastDrawTime + frametime;
         double now = GLFW.glfwGetTime();
+        double target = (now - (now % frametime)) + frametime; // subtracting (now % frametime) corrects for desync
 
         for (; now < target; now = GLFW.glfwGetTime()) {
-            double waitTime = (target - now) - 0.002; // -2ms to account for inaccuracy of timeouts on some operating systems
-            if (waitTime >= 0.001) { // pretty sure you can't sleep for less than 1ms without platform-specific code
-                GLFW.glfwWaitEventsTimeout(waitTime); // could be replaced with Thread.sleep(), but i'm not sure if it'd be as precise
+            int waitTime = (int)((target - now) * 1000.0) - 2; // -2ms to account for sleep imprecision in some OSes
+            if (waitTime >= 1) { // cant sleep less than 1ms without platform-specific code
+                try {
+                    Thread.sleep(waitTime); // precision seems fine on both linux and windows
+                }
+                catch (Exception e) {}
             }
         }
 
-        lastDrawTime = now - (now % frametime); // subtracting (now % frametime) keeps frames in sync
         GLFW.glfwPollEvents(); // glfwWaitEventsTimeout won't catch every input if subtracting 2ms from the timeout
     }
 }
