b2ca8a3cc0df5705944b57d8ca86baf85fb2c440
MrMangoHands <mrmangohands@pm.me>
2024-08-17 19:17:17 -0700

    Make iterateVisibleBlockEntities Mixin an Overwrite

--- neoforge/src/main/java/net/caffeinemc/mods/sodium/neoforge/mixin/core/render/world/LevelRendererMixin.java
+++ neoforge/src/main/java/net/caffeinemc/mods/sodium/neoforge/mixin/core/render/world/LevelRendererMixin.java
@@ -4,18 +4,18 @@ import net.caffeinemc.mods.sodium.client.render.SodiumWorldRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
 
 import java.util.function.Consumer;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererMixin {
-    @Inject(method = "iterateVisibleBlockEntities", at = @At("HEAD"), cancellable = true)
-    public void replaceBlockEntityIteration(Consumer<BlockEntity> blockEntityConsumer, CallbackInfo ci) {
-        ci.cancel();
-
+    /**
+     * @author MrMangoHands
+     * @reason Redirect iteration of visible block entities to our renderer
+     */
+    @Overwrite
+    public void iterateVisibleBlockEntities(Consumer<BlockEntity> blockEntityConsumer) {
         SodiumWorldRenderer.instance().iterateVisibleBlockEntities(blockEntityConsumer);
     }
 }
