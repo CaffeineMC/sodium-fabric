c01a6bd47131769d61eb23216db0f2f3e0fd5394
douira <douira100@gmail.com>
2024-09-29 01:15:55 +0200

    fix translucent rendering by correctly decoding vertex segments

--- common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/data/BuiltSectionMeshParts.java
+++ common/src/main/java/net/caffeinemc/mods/sodium/client/render/chunk/data/BuiltSectionMeshParts.java
@@ -24,7 +24,13 @@ public class BuiltSectionMeshParts {
         var vertexCounts = new int[ModelQuadFacing.COUNT];
 
         for (int i : this.vertexSegments) {
-            vertexCounts[SectionRenderDataUnsafe.decodeFacing(i)] = SectionRenderDataUnsafe.decodeVertexCount(i);
+            var count = SectionRenderDataUnsafe.decodeVertexCount(i);
+
+            // it's important to only write non-zero vertex counts since the decoded facing is wrong if the count is zero
+            // (the whole segment is just zero, which decodes to the first facing, but it's not actually that facing, just no vertexes)
+            if (count > 0) {
+                vertexCounts[SectionRenderDataUnsafe.decodeFacing(i)] = count;
+            }
         }
 
         return vertexCounts;
