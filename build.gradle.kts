c5527a6ba40a8427ab041a7153fc286cd075a0d0
IMS212 <ims@irisshaders.dev>
2024-10-18 17:56:09 -0700

    Update to rc1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ plugins {
     id("fabric-loom") version ("1.8.10") apply (false)
 }
 
-val MINECRAFT_VERSION by extra { "1.21.2-pre1" }
-val NEOFORGE_VERSION by extra { "21.2.0-alpha.1.21.2-pre1.20241013.205741" }
-val FABRIC_LOADER_VERSION by extra { "0.16.5" }
-val FABRIC_API_VERSION by extra { "0.105.3+1.21.2" }
+val MINECRAFT_VERSION by extra { "1.21.2-rc1" }
+val NEOFORGE_VERSION by extra { "21.2.0-alpha.1.21.2-rc1.20241017.223717" }
+val FABRIC_LOADER_VERSION by extra { "0.16.7" }
+val FABRIC_API_VERSION by extra { "0.106.0+1.21.2" }
 
 // This value can be set to null to disable Parchment.
 val PARCHMENT_VERSION by extra { null }
